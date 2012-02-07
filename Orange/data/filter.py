c20f8018c895541c022a399f0dfb847aeda1f60d
tomazc <tomaz.curk@fri.uni-lj.si>
2012-02-08 00:09:37 +0100

    Documentation and porting of Orange.data.filter.

---
+++ Orange/data/filter.py
@@ -0,0 +1,17 @@
+from orange import\
+    Filter as Filter,\
+    FilterList as FilterList,\
+    Filter_random as Random,\
+    Filter_isDefined as IsDefined,\
+    Filter_hasClassValue as HasClassValue,\
+    Filter_hasMeta as HasMeta,\
+    Filter_sameValue as SameValue,\
+    Filter_values as Values,\
+    Filter_hasSpecial as HasSpecial,\
+    ValueFilter as ValueFilter,\
+    ValueFilterList as ValueFilterList,\
+    ValueFilter_discrete as ValueFilterDiscrete,\
+    ValueFilter_continuous as ValueFilterContinuous,\
+    ValueFilter_string as ValueFilterString,\
+    Filter_conjunction as Conjunction,\
+    Filter_disjunction as Disjunction
