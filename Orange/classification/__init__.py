527a3dfa643a200e8337d73d294426bd90690ecd
anze <anze.staric@gmail.com>
2012-02-13 11:42:15 +0100

    Fixed some warnings.

--- Orange/classification/__init__.py
+++ Orange/classification/__init__.py
@@ -1,17 +1,15 @@
-from orange import \
-   CostMatrix, \
-   Classifier, \
-   ClassifierList, \
-   Learner, \
-   ClassifierFD, \
-   LearnerFD, \
-   ClassifierFromVarFD, \
-   CartesianClassifier, \
-   ClassifierFromVar, \
-   RandomClassifier, \
-   RandomLearner, \
-   ClassifierFromVar
-   
+from Orange import core
 
-from orange import DefaultClassifier as ConstantClassifier
-
+CostMatrix = core.CostMatrix
+Classifier = core.Classifier
+ClassifierList = core.ClassifierList
+Learner = core.Learner
+ClassifierFD = core.ClassifierFD
+LearnerFD = core.LearnerFD
+ClassifierFromVarFD = core.ClassifierFromVarFD
+CartesianClassifier = core.CartesianClassifier
+ClassifierFromVar = core.ClassifierFromVar
+RandomClassifier = core.RandomClassifier
+RandomLearner = core.RandomLearner
+ClassifierFromVar = core.ClassifierFromVar
+ConstantClassifier = core.DefaultClassifier
