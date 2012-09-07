2ea3866a8bec1e2ba7dd17cb1d60687ba1c57571
janezd <janez.demsar@fri.uni-lj.si>
2012-09-07 16:44:34 +0200

    Added definition of Learner and Classifier, removed the old stuff from classification/__init__.py

--- Orange/classification/__init__.py
+++ Orange/classification/__init__.py
@@ -1,15 +1,9 @@
 from Orange import core
 
-CostMatrix = core.CostMatrix
-Classifier = core.Classifier
-ClassifierList = core.ClassifierList
-Learner = core.Learner
-ClassifierFD = core.ClassifierFD
-LearnerFD = core.LearnerFD
-ClassifierFromVarFD = core.ClassifierFromVarFD
-CartesianClassifier = core.CartesianClassifier
-ClassifierFromVar = core.ClassifierFromVar
-RandomClassifier = core.RandomClassifier
-RandomLearner = core.RandomLearner
-ClassifierFromVar = core.ClassifierFromVar
-ConstantClassifier = core.DefaultClassifier
+class Learner:
+    pass
+
+
+class Classifier:
+    def __init__(self, domain):
+        self.domain = domain
