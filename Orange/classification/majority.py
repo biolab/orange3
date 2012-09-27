aff008d251792362790c6d6773cb73abd083e8c6
janezd <janez.demsar@fri.uni-lj.si>
2012-09-27 23:05:44 +0200

    Further simplified classification.ConstantClassifier

--- Orange/classification/majority.py
+++ Orange/classification/majority.py
@@ -1,38 +1,36 @@
 import numpy as np
 from Orange import classification
 
-class MajorityLearner(classification.Learner):
+class MajorityLearner(classification.Fitter):
     def __call__(self, data):
         if len(data.domain.class_vars) > 1:
             raise NotImplementedError(
                 "Majority learner does not support multiple classes")
 
         y = data.Y
-        w = data.W if data.W.shape[-1] != 0 else None
+        w = data.W if data.has_weights else None
         assert np.issubdtype(y.dtype, int)
 
         n_values = data.domain.class_var.values()
         dist = np.bincount(y, w, minlength=n_values).astype(float)[:n_values]
-        N = sum(dist)
+        N = np.sum(dist)
         if N > 0:
-            dist /= sum(dist)
+            dist /= N
         else:
             dist.fill(1/len(dist))
-        return ConstantClassifier(data.domain, dist=dist)
+        return ConstantClassifier(data.domain, probs=dist)
 
 
-class ConstantClassifier(classification.Classifier):
-    def __init__(self, domain, value=None, dist=None):
+class ConstantClassifier(classification.Model):
+    def __init__(self, domain, value=None, probs=None):
         super().__init__(self, domain)
-        if value is None:
-            self.value = np.argmax(dist)
-        else:
-            self.value = value
-        if self.dist is None:
-            self.dist = np.zeros(len(domain.class_var.values), float)
-            self.dist[self.value] = 1
-        else:
-            self.dist = dist
+        self.value = value
+        self.probs = probs
 
     def predict(self, X):
-        return np.tile(self.value, len(X)), np.tile(self.dist, (len(X), 1))
+        r = []
+        if self.value is not None:
+            r.append(np.tile(self.value, len(X)))
+        if self.probs is not None:
+            r.append(np.tile(self.probs, (len(X), 1)))
+        return r
