a563f8d3927c8657f3943b51f9f0e2df4a887e2d
thocevar <devnull@localhost>
2012-10-26 12:43:11 +0200

    Added Naive Bayes classifier from scikit.

--- Orange/classification/naive_bayes.py
+++ Orange/classification/naive_bayes.py
@@ -6,13 +6,13 @@ class BayesLearner(classification.Fitter):
     def __call__(self, data):
         assert isinstance(data, Table)
         clf = MultinomialNB()
-        clf.fit(data.getX(),data.getY())
+        clf.fit(data.X, data.Y[:,0])
         return BayesClassifier(data.domain, clf)
 
 
 class BayesClassifier(classification.Model):
     def __init__(self, domain, clf):
-        super().__init__(self, domain)
+        super().__init__(domain)
         self.clf = clf
 
     def __call__(self, X):
