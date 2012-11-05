ecb0ceb5c16f6cf691eaf6a8fc7965233adcc6a8
anze <anze.staric@gmail.com>
2012-11-05 14:06:05 +0100

    Fixed domain pickling tests.

--- Orange/tests/test_domain.py
+++ Orange/tests/test_domain.py
@@ -5,9 +5,13 @@ from Orange import testing
 from Orange import data
 
 class PickleDomain(testing.PickleTest):
-    DEBUG = True
     def generate_objects(self):
         yield data.Domain([])
+
+    def assertEqual(self, first, second, msg=None):
+        super().assertEqual(first.attributes, second.attributes)
+        super().assertEqual(first.anonymous, second.anonymous)
+
 
 
 
