43d8b8f9b89fbe29ea64c21bcae616c41a9273b2
thocevar <devnull@localhost>
2012-11-09 11:39:32 +0100

    Added linear regression.

---
+++ Orange/classification/linear_regression.py
@@ -0,0 +1,16 @@
+from Orange import classification
+from sklearn.linear_model import LinearRegression
+
+class LinearRegressionLearner(classification.Fitter):
+    def fit(self, X, Y, W):
+        clf = LinearRegression()
+        return LinearRegressionPredictor(clf.fit(X, Y.reshape(-1)))
+
+
+class LinearRegressionPredictor(classification.Model):
+    def __init__(self, clf):
+        self.clf = clf
+
+    def predict(self, X):
+        value = self.clf.predict(X)
+        return value
--- Orange/tests/test_classification.py
+++ Orange/tests/test_classification.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from Orange import data
 import Orange.classification.naive_bayes as nb
+import Orange.classification.linear_regression as lr
 
 class NaiveBayesTest(unittest.TestCase):
 
@@ -21,7 +22,30 @@ class NaiveBayesTest(unittest.TestCase):
         z = clf(x2)
         self.assertTrue((z.reshape((-1,1))==y2).all())
 
-    def test_class_variable(self):
+
+class LinearRegressionTest(unittest.TestCase):
+
+    def test_LinearRegression(self):
+        nrows = 1000
+        ncols = 3
+        x = np.random.random_integers(-20, 50, (nrows, ncols))
+        c = np.random.rand(ncols, 1)*10-3
+        e = np.random.rand(nrows, 1)-0.5
+        y = np.dot(x,c) + e
+
+        x1, x2 = np.split(x,2);
+        y1, y2 = np.split(y,2);
+        t = data.Table(x1, y1)
+        learn = lr.LinearRegressionLearner()
+        clf = learn(t)
+        z = clf(x2)
+        self.assertTrue((abs(z.reshape(-1,1)-y2)<2.0).all())
+
+
+class MultiClassTest(unittest.TestCase):
+
+    # Naive Bayes implementation doesn't support multiple class variables
+    def test_unsupported(self):
         nrows = 20
         ncols = 10
         x = np.random.random_integers(1, 3, (nrows, ncols))
@@ -39,5 +63,5 @@ class NaiveBayesTest(unittest.TestCase):
         learn = nb.BayesLearner()
         clf = learn(t)
 
-        # TODO
-        # test with a classifier that supports mmultiple class vars
+    def test_supported(self):
+        pass
