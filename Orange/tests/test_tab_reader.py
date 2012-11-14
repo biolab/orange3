0da041beee1a5c5b3c767acae47a99f3fbfadb27
anze <anze.staric@gmail.com>
2012-11-14 19:14:27 +0100

    Fixed a failing test.

--- Orange/tests/test_tab_reader.py
+++ Orange/tests/test_tab_reader.py
@@ -7,7 +7,7 @@ from Orange.data import ContinuousVariable, DiscreteVariable
 from Orange.data.io import TabDelimReader
 
 simplefile = """\
-Feature 1\tFeature 2\tClass 1\tClass 2
+Feature 1\tFeature 2\tClass 1\tClass 42
 c        \tM F      \tc      \td
          \t         \tclass  \tclass
 1.0      \tM        \t5      \trich
@@ -27,7 +27,7 @@ class TestTabReader(unittest.TestCase):
         self.assertIsInstance(c1, ContinuousVariable)
         self.assertEqual(c1.name, "Class 1")
         self.assertIsInstance(c2, DiscreteVariable)
-        self.assertEqual(c2.name, "Class 2")
+        self.assertEqual(c2.name, "Class 42")
 
         print(table.domain.class_vars[1].values)
         np.testing.assert_almost_equal(table.X, np.array([[1, 0],[2, 1]]))
