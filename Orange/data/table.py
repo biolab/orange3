Fixed sorting and a wrong index in Table._compute_contingencies for continuous attributes

Orange/data/table.py
@@ -1174,13 +1174,13 @@ class Table(MutableSequence, Storage):
                             col_data = grp_data[grp_data.indptr[col_i]:
                                                 grp_data.indptr[col_i+1]]
                         else:
-                            col_data = grp_data[:, col_i]
+                            col_data = grp_data[:, arr_i]
                         if W is not None:
                             ranks = np.argsort(col_data)
                             vals = np.vstack((col_data[ranks], grp_W[ranks]))
                             nans = bn.countnans(col_data, grp_W)
                         else:
-                            np.sort(col_data)
+                            col_data.sort()
                             vals = np.ones((2, len(col_data)))
                             vals[0, :] = col_data
                             nans = bn.countnans(col_data)