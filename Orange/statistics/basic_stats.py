a9801cf581b6f7a843988e329eeaf1410e4901fd
janezd <janez.demsar@fri.uni-lj.si>
2013-01-21 16:08:51 +0100

    Added a temporary implementation of statistics.basis_stats

---
+++ Orange/statistics/basic_stats.py
@@ -0,0 +1,25 @@
+from Orange.data import Variable, ContinuousVariable
+
+import bottleneck as bn
+
+
+# TODO: this is all just a quick hack; do it properly -- call data storage etc.
+
+class BasicStats:
+    def __init__(self, variable, data):
+        if (isinstance(variable, Variable) and
+                not isinstance(variable, ContinuousVariable)):
+            raise ValueError("variable '{}' is not continuous".
+                             format(variable.name))
+        col = data.get_column_view(variable)
+        self.min = bn.nanmin(col)
+        self.max = bn.nanmax(col)
+
+# TODO sparse data
+def get_stats(data):
+    return [BasicStats(i, data) for i, var in enumerate(data.domain)
+            if isinstance(var, ContinuousVariable)
+           ] + [
+            BasicStats(-1 - i, data) for i, var in enumerate(data.domain.metas)
+            if isinstance(var, ContinuousVariable)
+           ]
