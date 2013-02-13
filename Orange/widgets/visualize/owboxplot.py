e46d0e86c19b3c976437862e4c312ce827442d50
janezd <janez.demsar@fri.uni-lj.si>
2013-02-13 19:43:27 +0100

    Box plot works and looks nice; not finished yet

--- Orange/widgets/visualize/owboxplot.py
+++ Orange/widgets/visualize/owboxplot.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import textwrap
 import operator
+import math
 from scipy import stats
 import numpy as np
 
@@ -15,75 +15,51 @@ from Orange.widgets import widget, gui
 from Orange.widgets.settings import (Setting, DomainContextHandler,
                                      ContextSetting)
 from Orange.widgets.utils import datacaching
-from Orange.widgets.widget import Multiple, Default
-from Orange.widgets.utils.plot import owplot, owconstants
+from Orange.widgets.widget import Default
+from Orange.widgets.utils.plot import owaxis
 
 #from OWColorPalette import ColorPixmap, ColorPaletteGenerator
 
 class BoxItem(QtGui.QGraphicsItemGroup):
-    def __init__(self, width,
-                 whisker_low, whisker_high, box_low, box_high, mean, median,
-                 label=""):
+    WIDTH = 20
+
+    pen_light = QtGui.QPen(QtGui.QBrush(QtGui.QColor(0xff, 0xff, 0xff)), 2)
+    pen_dark = QtGui.QPen(QtGui.QBrush(QtGui.QColor(0x33, 0x00, 0xff)), 2)
+    for pen in (pen_dark, pen_light):
+        pen.setCosmetic(True)
+        pen.setCapStyle(QtCore.Qt.RoundCap)
+        pen.setJoinStyle(QtCore.Qt.RoundJoin)
+
+    def __init__(self, index, label, a_min, mean, a_max, q25, q50, q75):
         super().__init__()
+        self.index = index
         self.mean = mean
-        self.median = median
+        self.median = q50
         self.label = label
 
         Line = QtGui.QGraphicsLineItem
-        box = QtGui.QGraphicsRectItem(-width//2, box_low,
-                                      width, box_high - box_low)
-        whisker1 = Line(-width//8, whisker_low, width//8, whisker_low)
-        whisker2 = Line(-width//8, whisker_high, width//8, whisker_high)
-        vert_line = Line(0, whisker_low, 0, whisker_high)
-        mean_line = Line(-0.55*width, mean, 0.55*width, mean)
-        median_line = Line(-width//2, median, width//2, median)
-        for it in (box, whisker1, whisker2, vert_line, mean_line, median_line):
-            self.addToGroup(it)
+        width = self.WIDTH
+        whisker1 = Line(-width/8, a_min, width/8, a_min, self)
+        whisker2 = Line(-width/8, a_max, width/8, a_max, self)
+        vert_line = Line(0, a_min, 0, a_max, self)
+        mean_line = Line(-0.7*width, mean, 0.7*width, mean, self)
+        for it in (whisker1, whisker2, vert_line, mean_line):
+            it.setPen(self.pen_dark)
 
+        box = QtGui.QGraphicsRectItem(-width/2, q25, width, q75 - q25, self)
+        box.setBrush(QtGui.QBrush(QtGui.QColor(0x33, 0x88, 0xff, 0xc0)))
+        box.setPen(QtGui.QPen(QtCore.Qt.NoPen))
+        box.setZValue(100)
 
-class BoxPlotArea(owplot.OWPlot):
-    def __init__(self):
-        super().__init__()
-        self.set_axis_title(0, '')
-        self.set_show_axis_title(0, 1)
-        self.axis_margin = 20
-        self.y_axis_extra_margin = 35
-        self.title_margin = 40
-        self.graph_margin = 10
-        self.grid_curve.set_pen(QtGui.QPen(3))
-        self.boxes = []
+        median_line = Line(-width/2, q50, width/2, q50, self)
+        median_line.setPen(self.pen_light)
+        median_line.setZValue(200)
 
-    def setBoxes(self, boxes, y_axis_title):
-        for i in self.boxes:
-            self.remove_item(i)
-        self.boxes = boxes
-        self.set_axis_title(0, y_axis_title)
-        self.set_axis_labels(2, [box.label for box in self.boxes])
-
-    def removeBoxes(self):
-        self.setBoxes([], "")
-
-    def addBoxItem(self, label,
-                   xAxis=owconstants.xBottom, yAxis=owconstants.yLeft,
-                   visible=1):
-        box = BoxItem(label)
-        #tooltip = QToolTip()
-        #tooltip.showText
-        #box.setToolTip(tooltip)
-        box.set_axes(xAxis, yAxis)
-        box.setVisible(visible)
-        return self.add_custom_curve(box, enableLegend=0)
-
-    def sizeHint(self):
-        return QtCore.QSize(100, 100)
-
-    def sort_by(self, criterion, reverse):
-        self.boxes.sort(key=operator.attrgetter(criterion), reverse=reverse)
-        for i, box in enumerate(self.boxes):
-            box.set_xindex(i)
-        self.set_axis_labels(2, [box.label for box in self.boxes])
-        self.replot()
-
+        # not inserted into the group in order to avoid scaling
+        self.label_text = t = QtGui.QGraphicsSimpleTextItem(label)
+        t.setY(a_min)
+        t.setFlags(t.flags() |
+                   QtGui.QGraphicsItem.ItemIgnoresTransformations)
 
 
 class OWBoxPlot(widget.OWWidget):
@@ -94,18 +70,19 @@ class OWBoxPlot(widget.OWWidget):
     #_icon = "icons/Boxplot.svg"
     _priority = 100
     _author = "Amela Rakanović, Janez Demšar"
-    #_author_email = "ales.erjavec(@at@)fri.uni-lj.si"
     inputs = [("Data", Table, "data")]
-    outputs = [("Basic statistic", Table),
+    outputs = [("Basic statistic", Table, Default),
                ("Significant columns", Table),
-               ("Selected Data", Table, Default)]
+               ("Selected Data", Table)]
 
     settingsHandler = DomainContextHandler()
-    sorting_select = Setting("Sort by label")
+    sorting_select = Setting(0)
     grouping_select = ContextSetting([0])
     attributes_select = ContextSetting([0])
     stattest = Setting(0)
     sig_threshold = Setting(0.05)
+
+    _sorting_criteria_attrs = ["index", "label", "median", "mean"]
 
     def __init__(self, parent=None, signalManager=None, settings=None):
         super().__init__(parent, signalManager, settings)
@@ -128,17 +105,17 @@ class OWBoxPlot(widget.OWWidget):
             selectionMode=QtGui.QListWidget.SingleSelection,
             callback=self.process_change)
         self.sorting_combo = gui.comboBox(gb, self,
-            'sorting_select', sendSelectedValue = 1,
-            emptyString="Sort by label", callback=self.sorting_update,
-            items=["Sort by label", "Sort by median", "Sort by average"])
+            'sorting_select', callback=self.sorting_update,
+            items=["Show in original order",
+                   "Sort by label", "Sort by median", "Sort by mean"])
 
-        b = gui.widgetBox(self.controlArea, "Selection of output variables")
+        b = gui.widgetBox(self.controlArea, "Statistics")
         gui.doubleSpin(b, self, "sig_threshold", 0.05, 0.5, step=0.05,
-            label="Significance threshold", controlWidth=75,
+            label="Significance", controlWidth=75,
             alignment=QtCore.Qt.AlignRight)
-        gui.widgetLabel(b, "Statistical test")
+        gui.widgetLabel(b, "Test type")
         gui.radioButtonsInBox(gui.indentedBox(b, sep=10), self, "stattest",
-            ["Parametric (t-test)", "Non-parametric (Mann-Whitney)"],
+            ["Parametric", "Non-parametric"],
             callback = self.run_selected_test)
 
         gui.rubber(self.controlArea)
@@ -147,8 +124,9 @@ class OWBoxPlot(widget.OWWidget):
         result = gui.widgetBox(self.mainArea, addSpace=True)
         self.boxScene = QtGui.QGraphicsScene()
         self.boxView = QtGui.QGraphicsView(self.boxScene)
-        self.graph = BoxPlotArea()
-#        self.mainArea.layout().addWidget(self.graph)
+        self.boxView.setRenderHints(QtGui.QPainter.Antialiasing |
+                                    QtGui.QPainter.TextAntialiasing |
+                                    QtGui.QPainter.SmoothPixmapTransform)
         self.mainArea.layout().addWidget(self.boxView)
         self.no_values = gui.widgetLabel(self.mainArea,
             "<center><big><b>Too many values.</b></big></center>")
@@ -164,9 +142,168 @@ class OWBoxPlot(widget.OWWidget):
         #self.controlArea.setFixedWidth(250)
         self.mainArea.setMinimumWidth(650)
 
+    def data(self, dataset):
+        if dataset is not None and (
+                not len(dataset) or not len(dataset.domain)):
+            dataset = None
+        self.closeContext()
+        self.ddataset = dataset
+        self.grouping_select = []
+        self.attributes_select = []
+        self.attr_list_box.clear()
+        self.attrCombo.clear()
+        self.openContext(self.ddataset)
+        if dataset:
+            self.attrCombo.addItem("(none)")
+            attributes = []
+            grouping = ["None"]
+            for attr in self.ddataset.domain:
+                if isinstance(attr, DiscreteVariable):
+                    grouping.append((attr.name, attr.var_type))
+                else:
+                    attributes.append((attr.name, attr.var_type))
+            self.attributes = attributes
+            self.grouping = grouping
+            self.grouping_select = [0]
+            self.attributes_select = [0]
+            self.process_change()
+        else:
+            self.reset_all_data()
+
+    def reset_all_data(self):
+        self.attr_list_box.clear()
+        self.attrCombo.clear()
+        self.graph.clear_data()
+        self.send("Basic statistic", None)
+        self.send("Significant data", None)
+
+    def process_change(self):
+        self.draw_selected()
+#        self.run_selected_test()
+#        self.basic_stat = stat_basic_full_tab(self.ddataset)
+#        self.send("Basic statistic", self.basic_stat)
+
+    def draw_selected(self):
+        dataset = self.ddataset
+        if dataset is None:
+            return
+
+        def compute_box(dist):
+            N = np.sum(dist[1])
+            if N == 0:
+                return None
+            a_min, a_max = dist[0, 0], dist[0, -1]
+            mean = np.sum(dist[0] * dist[1]) / N
+            box_data = [a_min, mean, a_max]
+            s = 0
+            thresholds = [N/4, N/2, 3*N/4]
+            thresh_i = 0
+            for i, e in enumerate(dist[1]):
+                s += e
+                if s >= thresholds[thresh_i]:
+                    if s == thresholds[thresh_i] and i + 1 < dist.shape[1]:
+                        box_data.append((dist[0, i] + dist[0, i + 1]) / 2)
+                    else:
+                        box_data.append(dist[0, i])
+                    thresh_i += 1
+                    if thresh_i == 3:
+                        break
+            while len(box_data) != 6:
+                box_data.append(box_data[-1])
+            return box_data
+
+        self.warning.hide()
+        attr = self.attributes[self.attributes_select[0]][0]
+        attr_ind = dataset.domain.index(attr)
+        attr_desc = dataset.domain[attr_ind]
+        group_by = self.grouping_select[0]
+        if group_by:
+            group_attr = self.grouping[group_by][0]
+            group_ind = dataset.domain.index(group_attr)
+            self.conts = datacaching.getCached(dataset,
+                contingency.get_contingency,
+                (dataset, attr_ind, group_ind))
+            boxes = [(value, compute_box(self.conts[i]))
+                for i, value in enumerate(dataset.domain[group_ind].values)]
+
+            # TODO: add another box for the entire data set
+            self.sorting_combo.setDisabled(False)
+        else:
+            self.dist = datacaching.getCached(dataset,
+                distribution.get_distribution, (attr_ind, dataset))
+            boxes = [(attr, compute_box(self.dist))]
+            self.sorting_combo.setDisabled(True)
+
+        self.boxScene.clear()
+        a_min = min(box[0] for _, box in boxes if box)
+        a_max = max(box[2] for _, box in boxes if box)
+        self.boxes = []
+        for i, (label, box_data) in enumerate(boxes):
+            if box_data:
+                box = BoxItem(i, label, *tuple(box_data))
+                self.boxScene.addItem(box)
+                self.boxScene.addItem(box.label_text)
+                self.boxes.append(box)
+        self.set_positions()
+
+        first_val, step = owaxis.OWAxis.compute_scale(a_min, a_max)
+        while a_min < first_val:
+            first_val -= step
+        a_min = first_val
+        no_ticks = math.ceil((a_max - first_val) / step) + 1
+        a_max = max(a_max, first_val + (no_ticks - 1) * step)
+
+        r = QtCore.QRectF(-80, a_min, len(boxes)*60+60, a_max - a_min)
+        self.boxScene.setSceneRect(r)
+
+        # This code comes from the implementation of QGraphicsView::fitInView
+        # (http://qt.gitorious.org/qt/qt/blobs/4.8/src/gui/graphicsview/
+        # qgraphicsview.cpp), but modified to scale only vertically
+        d = self.boxView
+        d.resetTransform()
+        unity = d.matrix().mapRect(QtCore.QRectF(0, 0, 1, 1))
+        d.scale(1, 1 / unity.height())
+        viewRect = d.viewport().rect().adjusted(15, 15, -15, -15)
+        yratio = viewRect.height() / (a_max - a_min)
+        d.scale(1, -yratio)
+        d.centerOn((-80 + len(boxes) * 60 + 60) / 2, (a_max - a_min) / 2)
+
+        # This comes last because we need to position the text to the
+        # appropriate transformed coordinates
+        font = QtGui.QFont()
+        font.setPixelSize(12)
+        val = first_val
+        gray_pen = QtGui.QPen(QtCore.Qt.lightGray)
+        while True:
+            self.boxScene.addLine(-65, val, -60, val, gray_pen)
+            t = self.boxScene.addSimpleText(attr_desc.repr_val(val), font)
+            t.setFlags(t.flags() |
+                       QtGui.QGraphicsItem.ItemIgnoresTransformations)
+            r = t.boundingRect()
+            t.setPos(-70 - r.width(), val + r.height() / 2 / yratio)
+            if val >= a_max:
+                break
+            val += step
+        a_max = val
+        self.boxScene.addLine(-60, first_val, -60, a_max, gray_pen)
+
+    def set_positions(self):
+        for i, box in enumerate(self.boxes):
+            box.setX(i * 60)
+            box.label_text.setX(
+                i * 60 - box.label_text.boundingRect().width() / 2)
 
     def sorting_update(self):
-        self.graph.sort_by(self.sorting_select.split()[-1], True)
+        criterion = self._sorting_criteria_attrs[self.sorting_select]
+        self.boxes.sort(key=operator.attrgetter(criterion))
+        self.set_positions()
+
+    def send_to_graph(self, dataset, attr, box_label, y_label):
+        if dataset:
+            np_data = data_to_npcol(dataset, dataset.domain.index(attr))
+            stat_graph1 = stat_graph(np_data)
+            self.graph.append_data(box_label, stat_graph1, y_label)
+
 
     def run_selected_test(self):
         return
@@ -257,108 +394,6 @@ class OWBoxPlot(widget.OWWidget):
         for attr in self.attributes:
             tab.append(attr[0])
         return self.ddataset.select(tab)
-
-    def data(self, dataset):
-        if dataset is not None and (
-                not len(dataset) or not len(dataset.domain)):
-            dataset = None
-        self.closeContext()
-        self.ddataset = dataset
-        self.grouping_select = []
-        self.attributes_select = []
-        self.attr_list_box.clear()
-        self.attrCombo.clear()
-        self.openContext(self.ddataset)
-        if dataset:
-            self.attrCombo.addItem("(none)")
-            attributes = []
-            grouping = ["None"]
-            for attr in self.ddataset.domain:
-                if isinstance(attr, DiscreteVariable):
-                    grouping.append((attr.name, attr.var_type))
-                else:
-                    attributes.append((attr.name, attr.var_type))
-            self.attributes = attributes
-            self.grouping = grouping
-            self.grouping_select = [0]
-            self.attributes_select = [0]
-            self.process_change()
-        else:
-            self.reset_all_data()
-
-    def reset_all_data(self):
-        self.attr_list_box.clear()
-        self.attrCombo.clear()
-        self.graph.clear_data()
-        self.send("Basic statistic", None)
-        self.send("Significant data", None)
-
-    def process_change(self):
-        self.draw_selected()
-#        self.run_selected_test()
-#        self.basic_stat = stat_basic_full_tab(self.ddataset)
-#        self.send("Basic statistic", self.basic_stat)
-
-    def draw_selected(self):
-        dataset = self.ddataset
-        if dataset is None:
-            return
-
-        def box_from_dist(dist, label):
-            N = np.sum(dist[1])
-            if N == 0:
-                return None
-            a_min, a_max = dist[0, 0], dist[0, -1]
-            mean = np.sum(dist[0] * dist[1]) / N
-            s = 0
-            quartiles = []
-            thresholds = [N/4, N/2, 3*N/4]
-            thresh_i = 0
-            for i, e in enumerate(dist[1]):
-                s += e
-                if s >= thresholds[thresh_i]:
-                    if s == thresholds[thresh_i] and i + 1 < dist.shape[1]:
-                        quartiles.append((dist[0, i] + dist[0, i + 1]) / 2)
-                    else:
-                        quartiles.append(dist[0, i])
-                    thresh_i += 1
-                    if thresh_i == 3:
-                        break
-            while len(quartiles) != 3:
-                quartiles.append(quartiles[-1])
-            return BoxItem(30, a_min, a_max, quartiles[0], quartiles[2],
-                           mean, quartiles[1], label)
-
-        self.warning.hide()
-        attr = self.attributes[self.attributes_select[0]][0]
-        attr_ind = dataset.domain.index(attr)
-        group_by = self.grouping_select[0]
-        if group_by:
-            group_attr = self.grouping[group_by][0]
-            group_ind = dataset.domain.index(group_attr)
-            self.conts = datacaching.getCached(dataset,
-                contingency.get_contingency,
-                (dataset, attr_ind, group_ind))
-            boxes = [box_from_dist(self.conts[i], value)
-                for i, value in enumerate(dataset.domain[group_ind].values)]
-        else:
-            self.dist = datacaching.getCached(dataset,
-                distribution.get_distribution, (attr_ind, dataset))
-            boxes = [box_from_dist(self.dist, attr)]
-        self.boxScene.clear()
-        for i, box in enumerate(boxes):
-            if box:
-                box.setX(i * 60)
-                self.boxScene.addItem(box)
-        self.boxView.resetTransform()
-        self.boxView.scale(1, self.boxView.height() / (self.boxScene.height() * 1.1))
-
-
-    def send_to_graph(self, dataset, attr, box_label, y_label):
-        if dataset:
-            np_data = data_to_npcol(dataset, dataset.domain.index(attr))
-            stat_graph1 = stat_graph(np_data)
-            self.graph.append_data(box_label, stat_graph1, y_label)
 
 # ***************************************************************************
 # ***************************************************************************
