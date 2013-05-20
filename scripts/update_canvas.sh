d99824b3847197d7a5bf1d8cfd2073e280d2908f
astaric <anze.staric@gmail.com>
2013-05-20 17:04:20 +0200

    Updated update_canvas script.

--- scripts/update_canvas.sh
+++ scripts/update_canvas.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
-currentdir=`pwd`
-tempdir=`pwd`/`mktemp -d canvas_merge_XXXXXX`
+git diff-index --quiet --cached HEAD || git diff-files --quiet || { echo "Please, commit your changes or stash them before updating canvas."; echo "Aborting"; exit 1; }
 
-cd "$tempdir"
-
-wget https://bitbucket.org/biolab/orange/get/tip.tar.gz
-#cp ../tip.tar.gz tip.tar.gz
+if [ ! -f tip.tar.gz ]; then
+  wget https://bitbucket.org/biolab/orange/get/tip.tar.gzi
+fi
+rm -rf biolab-orange-*
 tar -xzf tip.tar.gz
 mv biolab-orange-*/Orange/OrangeCanvas canvas
 rm canvas/orng*
 mv canvas/main.py canvas/__main__.py
 2to3 -n -W --no-diffs canvas
 echo biolab-orange-* | awk -F"-" '{print $3}' > canvas/source-revision.txt
-echo
-echo "Converted canvas sources are in directory canvas."
+rev=`cat canvas/source-revision.txt`
+rm -rf ../Orange/canvas
+mv canvas ../Orange/
+rm -rf biolab-orange-*
+git checkout -b merge-canvas-$rev || exit 1
+git add ../Orange/canvas
+git commit -am "Updated canvas to $rev"
+
 echo "Please apply the following patches:"
 echo
-for p in ../patches/*; do
+echo cd ../Orange 
+for p in ../scripts/patches/*; do
     echo patch -p3 \< $p
 done
-echo "When all the conflicts have been fixed, type"
-echo "ready"
-
-function ready {
-    rm -rf ../../Orange/canvas
-    mv canvas ../../Orange
-    cd "$currentdir"
-    rm -rf "$tempdir"    
-}
-
