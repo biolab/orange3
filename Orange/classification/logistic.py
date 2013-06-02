3721aada353b474d5b5ce726fa65de4968fb6b5e
Jure Zbontar <jure.zbontar@gmail.com>
2013-06-03 01:31:24 +0200

    Implement logistic regression.

--- Orange/classification/logistic.py
+++ Orange/classification/logistic.py
@@ -1,91 +1,81 @@
-import warnings
+import logging
 
-from scipy.optimize import fmin_l_bfgs_b
 import numpy as np
 import scipy.sparse as sp
+from scipy.optimize import fmin_l_bfgs_b
 
 from Orange import classification
-import Orange.data
-
-
-# helper functions
-def append_ones(X):
-    if sp.issparse(X):
-        return sp.hstack((np.ones((X.shape[0], 1)), X)).tocsr()
-    else:
-        return np.hstack((np.ones((X.shape[0], 1)), X))
-
 
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
 
-
-def cost_grad(theta, X, y, lambda_):
-    sx = sigmoid(X.dot(theta))
-
-    j = -np.log(np.where(y, sx, 1 - sx)).sum()
-    j += lambda_ * theta.dot(theta) / 2.0
-    j /= X.shape[0]
-
-    grad = X.T.dot(sx - y)
-    grad += lambda_ * theta
-    grad /= X.shape[0]
-
-    return j, grad
-
-
-def fit(X, y, lambda_):
-    theta = np.zeros(X.shape[1])
-    theta, _, ret = fmin_l_bfgs_b(cost_grad, theta, args=(X, y, lambda_))
-    if ret['warnflag'] != 0:
-        warnings.warn('L-BFGS failed to converge')
-    return theta
-
-
-def predict(X, theta):
-    return sigmoid(X.dot(theta))
-
-
-# how it should look like
-class LogisticRegressionSimple:
-    def __init__(self, lambda_=1):
+class LogisticRegressionLearner(classification.Fitter):
+    def __init__(self, lambda_, normalize=True, **fmin_args):
         self.lambda_ = lambda_
+        self.fmin_args = fmin_args
+        self.normalize = normalize
 
-    def fit(self, X, y):
-        self.theta = fit(X, y, self.lambda_)
+    def cost_grad(self, theta, X, y):
+        sx = np.clip(sigmoid(X.dot(theta)), 1e-15, 1 - 1e-15)
 
-    def predict(self, X):
-        return predict(X, self.theta)
+        cost = -np.log(np.where(y, sx, 1 - sx)).sum()
+        cost += self.lambda_ * theta.dot(theta)
+        cost /= X.shape[0]
 
+        grad = X.T.dot(sx - y)
+        grad += self.lambda_ * theta
+        grad /= X.shape[0]
 
-# how it would look like in Orange4.0
-class LogisticRegressionLearner(classification.Learner):
-    def __init__(self, lambda_=1):
-        self.lambda_ = lambda_
+        return cost, grad
 
-    def __call__(self, data):
-        theta = fit(data.X, data.Y.ravel(), self.lambda_)
-        return LogisticRegressionClassifier(theta)
+    def fit(self, X, Y, W):
+        if list(np.unique(Y).astype(int)) != [0, 1]:
+            raise ValueError('Logistic regression requires a binary class '
+                'variable')
 
+        if Y.shape[1] > 1:
+            raise ValueError('Logistic regression does not support '
+                'multi-label classification')
 
-class LogisticRegressionClassifier(classification.Classifier):
-    def __init__(self, theta):
+        self.mean = np.mean(X, axis=0)
+        self.std = np.std(X, axis=0)
+        if self.normalize:
+            X = (X - self.mean) / self.std
+        
+        theta = np.zeros(X.shape[1])
+        theta, cost, ret = fmin_l_bfgs_b(
+            self.cost_grad, theta, args=(X, Y.ravel()), **self.fmin_args)
+        if ret['warnflag'] != 0:
+            logging.info('L-BFGS failed to converge')
+
+        return LogisticRegressionClassifier(theta, self.normalize, self.mean, self.std)
+
+class LogisticRegressionClassifier(classification.Model):
+    def __init__(self, theta, normalize, mean, std):
         self.theta = theta
-
-    def __call__(self, X):
-        return predict(X.X, self.theta)
-
+        self.normalize = normalize
+        self.mean = mean
+        self.std = std
+    
+    def predict(self, X):
+        if self.normalize:
+            X = (X - self.mean) / self.std
+        prob = sigmoid(X.dot(self.theta))
+        return np.column_stack((1 - prob, prob))
 
 if __name__ == '__main__':
-    iris = Orange.data.Table('../doc/datasets/iris')
-    iris.Y[iris.Y != 0.0] = 1.0
+    import Orange.data
+    from sklearn.cross_validation import StratifiedKFold
 
-    m = LogisticRegressionSimple(lambda_=2)
-    m.fit(iris.X, iris.Y.ravel())
-    p_simple = m.predict(iris.X)
+    data = Orange.data.Table('../tests/iris')
+    X, Y = data.X, data.Y
+    Y[Y == 0.0] = 1.0
+    Y[Y == 2.0] = 0.0
 
-    c = LogisticRegressionLearner(lambda_=2)
-    l = c(iris)
-    p_orange = l(iris)
-
-    assert np.allclose(p_simple, p_orange)
+    m = LogisticRegressionLearner(lambda_=1.0, normalize=False)
+    scores = []
+    for tr_ind, te_ind in StratifiedKFold(Y.ravel()):
+        s = np.mean(m(data[tr_ind])(data[te_ind]) == data[te_ind].Y.ravel())
+        scores.append(s)
+    print(np.mean(scores))
+        
