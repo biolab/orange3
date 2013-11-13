45e6b904034f3f733b81c80122bbc0797cce94a2
astaric <anze.staric@gmail.com>
2013-11-13 10:15:54 +0100

    Add category icons and colors.
    If you encounter duplicate categories, invalidating widget cache should
    help. This can be achieved by running canvas with --force-discovery
    flag.

--- Orange/widgets/data/__init__.py
+++ Orange/widgets/data/__init__.py
@@ -1,1 +1,17 @@
 NAME = "Data"
+
+ID = "orange.widgets.data"
+
+DESCRIPTION = """Widgets for data manipulation"""
+
+LONG_DESRIPTION = """
+This category contains widgets for data manipulation. This includes
+loading, importing, saving, preprocessing, selection, etc.
+
+"""
+
+ICON = "icons/Category-Data.svg"
+
+BACKGROUND = "#FFD39F"
+
+PRIORITY = 1
---
+++ Orange/widgets/visualize/__init__.py
@@ -0,0 +1,22 @@
+"""
+=========
+Visualize
+=========
+
+Widgets for data visualization.
+
+"""
+
+# Category description for the widget registry
+
+NAME = "Visualize"
+
+ID = "orange.widgets.visualize"
+
+DESCRIPTION = "Widgets for data visualization."
+
+BACKGROUND = "#FFB7B1"
+
+ICON = "icons/Category-Visualize.svg"
+
+PRIORITY = 2
