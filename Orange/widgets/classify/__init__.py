4dd1176d13ec2a4c733a524ed37811c3f6e21d67
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2013-12-20 18:47:27 +0100

    Added Naive Bayes learner widget.

---
+++ Orange/widgets/classify/__init__.py
@@ -0,0 +1,15 @@
+"""
+Widgets for the Classify category.
+
+"""
+
+
+NAME = "Classify"
+
+DESCRIPTION = "Classification algorithms."
+
+BACKGROUND = "#FAC1D9"
+
+ICON = "icons/Category-Classify.svg"
+
+PRIORITY = 3
