ae9e8be3739e3abc3093ebecd0e08358ef3615cf
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2013-12-20 19:20:24 +0100

    Added Predictions widget.

---
+++ Orange/widgets/evaluate/__init__.py
@@ -0,0 +1,13 @@
+"""
+Widgets from Evaluate category
+
+"""
+NAME = "Evaluate"
+
+DESCRIPTION = "Evaluate classification/regression performance."
+
+BACKGROUND = "#C3F3F3"
+
+ICON = "icons/Category-Evaluate.svg"
+
+PRIORITY = 5
