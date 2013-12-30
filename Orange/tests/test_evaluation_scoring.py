17c1bc7078f9fb2de37f6d5e5e342327a3481252
janezd <janez.demsar@fri.uni-lj.si>
2013-12-30 22:59:21 +0100

    Fixed and refactores tests for CA, removed tests for AUC

--- Orange/tests/test_evaluation_scoring.py
+++ Orange/tests/test_evaluation_scoring.py
@@ -6,10 +6,10 @@ from Orange.evaluation import testing, scoring
 from Orange.classification import naive_bayes, majority
 
 
-class ScoringTest(unittest.TestCase):
-    def test_CA(self):
+class Scoring_CA_Test(unittest.TestCase):
+    def test_init(self):
         res = testing.Results(nmethods=2, nrows=100)
-        res.actual = np.zeros(100)
+        res.actual[:50] = 0
         res.actual[50:] = 1
         res.predicted = np.vstack((res.actual, res.actual))
         np.testing.assert_almost_equal(scoring.CA(res), [1, 1])
@@ -20,9 +20,9 @@ class ScoringTest(unittest.TestCase):
         res.predicted[1] = 1 - res.predicted[1]
         np.testing.assert_almost_equal(scoring.CA(res), [0.99, 0])
 
-    def test_CA_call(self):
+    def test_call(self):
         res = testing.Results(nmethods=2, nrows=100)
-        res.actual = np.zeros(100)
+        res.actual[:50] = 0
         res.actual[50:] = 1
         res.predicted = np.vstack((res.actual, res.actual))
         ca = scoring.CA()
@@ -34,37 +34,17 @@ class ScoringTest(unittest.TestCase):
         res.predicted[1] = 1 - res.predicted[1]
         np.testing.assert_almost_equal(ca(res), [0.99, 0])
 
-    def testCA2(self):
-        nrows = 10000
-        ncols = 5
-        x = np.random.random_integers(1, 3, (nrows, ncols))
-        y = np.random.random_integers(1, 2, (nrows, 1))
-        t = data.Table(x, y)
+    def test_bayes(self):
+        x = np.random.random_integers(1, 3, (100, 5))
+        col = np.random.randint(5)
+        y = x[:, col].copy().reshape(100, 1)
+        t = Table(x, y)
 
-        pred = np.random.random_integers(2, 3, nrows)
-        self.assertAlmostEqual(scoring.CA(t,pred), 0.25, delta=0.1)
+        res = testing.TestOnTrainingData(t, [naive_bayes.BayesLearner()])
+        np.testing.assert_almost_equal(scoring.CA(res), [1])
 
-        learn = nb.BayesLearner()
-        clf = learn(t)
-        pred = clf(x)
-        self.assertAlmostEqual(scoring.CA(t,pred), 0.5, delta=0.1)
+        y[-5:] = 1 - y[-5:]
+        res = testing.TestOnTrainingData(t, [naive_bayes.BayesLearner()])
+        self.assertGreaterEqual(scoring.CA(res)[0], 0.75)
+        self.assertLess(scoring.CA(res)[0], 1)
 
-    def testAUC_binary(self):
-        nrows = 1000
-        ncols = 5
-        x = np.random.random_integers(0, 1, (nrows, ncols))
-        col1 = np.random.randint(ncols)
-        col2 = np.random.randint(ncols)
-        e = np.random.random_integers(0,1,nrows)
-        y = (x[:,col1]*x[:,col1]*e).reshape(nrows,1)
-
-        x1, x2 = np.split(x,2);
-        y1, y2 = np.split(y,2);
-        t = data.Table(x1, y1)
-        learn = nb.BayesLearner()
-        clf = learn(t)
-        t = data.Table(x2, y2)
-        prob = clf(x2, ret=classification.Model.Probs)
-        auc = scoring.AUC_binary(t,prob)
-        self.assertLess(auc, 1.0)
-        self.assertGreater(auc, 0.7)
