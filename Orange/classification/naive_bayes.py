15688455310d8b6de97ff1c36157cf198bff0e5c
Lan Zagar <lan.zagar@fri.uni-lj.si>
2014-07-25 15:27:44 +0200

    Improved naive bayes and removed sklearn version.

--- Orange/classification/naive_bayes.py
+++ Orange/classification/naive_bayes.py
@@ -1,28 +1,11 @@
 import numpy as np
-from sklearn.naive_bayes import GaussianNB
 
-from Orange import classification
-from Orange.data import Storage
+from Orange.classification import Fitter, Model
+from Orange.data import Instance, Storage, Table
 from Orange.statistics import contingency
 
 
-class BayesLearner(classification.Fitter):
-    def fit(self, X, Y, W):
-        clf = GaussianNB()
-        return BayesClassifier(clf.fit(X, Y.reshape(-1)))
-
-
-class BayesClassifier(classification.Model):
-    def __init__(self, clf):
-        self.clf = clf
-
-    def predict(self, X):
-        value = self.clf.predict(X)
-        prob = self.clf.predict_proba(X)
-        return value, prob
-
-
-class BayesStorageLearner(classification.Fitter):
+class BayesLearner(Fitter):
     def fit_storage(self, table):
         if not isinstance(table, Storage):
             raise TypeError("Data is not a subclass of Orange.data.Storage.")
@@ -33,29 +16,30 @@ class BayesStorageLearner(classification.Fitter):
         cont = contingency.get_contingencies(table)
         class_freq = np.diag(
             contingency.get_contingency(table, table.domain.class_var))
-        return BayesStorageClassifier(cont, class_freq, table.domain)
+        return BayesClassifier(cont, class_freq, table.domain)
 
 
-class BayesStorageClassifier(classification.Model):
+class BayesClassifier(Model):
     def __init__(self, cont, class_freq, domain):
         super().__init__(domain)
         self.cont = cont
         self.class_freq = class_freq
 
     def predict_storage(self, data):
-        if isinstance(data, Storage):
-            return np.array([self.predict_storage(ins) for ins in data])
+        if isinstance(data, Instance):
+            data = [data]
         ncv = len(self.domain.class_var.values)
-        max_log_prob = None
-        value = None
-        for c in range(ncv):
-            py = (1 + self.class_freq[c]) / (ncv + sum(self.class_freq))
-            log_prob = np.log(py)
-            for i, a in enumerate(self.domain.attributes):
-                relevant = 1 + self.cont[i][c][a.to_val(data[a])]
-                total = len(a.values) + self.class_freq[c]
-                log_prob += np.log(relevant / total)
-            if max_log_prob is None or log_prob > max_log_prob:
-                max_log_prob = log_prob
-                value = c
-        return value
+        probs = np.zeros((len(data), ncv))
+        for i, ins in enumerate(data):
+            for c in range(ncv):
+                py = (1 + self.class_freq[c]) / (ncv + sum(self.class_freq))
+                log_prob = np.log(py)
+                for ai, a in enumerate(self.domain.attributes):
+                    relevant = 1 + self.cont[ai][c][a.to_val(ins[a])]
+                    total = len(a.values) + self.class_freq[c]
+                    log_prob += np.log(relevant / total)
+                probs[i, c] = log_prob
+        np.exp(probs, out=probs)
+        probs /= probs.sum(axis=1)[:, None]
+        values = probs.argmax(axis=1)
+        return values, probs
--- Orange/tests/test_naive_bayes.py
+++ Orange/tests/test_naive_bayes.py
@@ -1,37 +1,39 @@
 import unittest
-import numpy as np
 
-import Orange
+import Orange.data
 import Orange.classification.naive_bayes as nb
 from Orange.evaluation import scoring, testing
 
 
 class NaiveBayesTest(unittest.TestCase):
     def test_NaiveBayes(self):
-        nrows = 1000
-        ncols = 10
-        x = np.random.random_integers(1, 3, (nrows, ncols))
-        col = np.random.randint(ncols)
-        y = x[:nrows, col].reshape(nrows, 1) + 100
-
-        x1, x2 = np.split(x, 2)
-        y1, y2 = np.split(y, 2)
-        t = Orange.data.Table(x1, y1)
-        learn = nb.BayesLearner()
-        clf = learn(t)
-        z = clf(x2)
-        self.assertTrue((z.reshape((-1, 1)) == y2).all())
-
-    def test_BayesStorage(self):
-        nrows = 200
-        ncols = 10
-        x = np.random.random_integers(0, 3, (nrows, ncols))
-        x[:, 0] = 3
-        y = x[:, ncols // 2].reshape(nrows, 1)
-        continuous_table = Orange.data.Table(x, y)
-        table = Orange.data.discretization.DiscretizeTable(
-            continuous_table, clean=False)
-        bayes = nb.BayesStorageLearner()
-        results = testing.CrossValidation(table, [bayes], k=10)
+        table = Orange.data.Table('titanic')
+        bayes = nb.BayesLearner()
+        results = testing.CrossValidation(table[::20], [bayes], k=10)
         ca = scoring.CA(results)
-        self.assertGreater(ca, 0.95)
+        self.assertGreater(ca, 0.7)
+        self.assertLess(ca, 0.9)
+
+    def test_predict_single_instance(self):
+        table = Orange.data.Table('titanic')
+        bayes = nb.BayesLearner()
+        c = bayes(table)
+        for ins in table[::20]:
+            c(ins)
+            val, prob = c(ins, c.ValueProbs)
+
+    def test_predict_table(self):
+        table = Orange.data.Table('titanic')
+        bayes = nb.BayesLearner()
+        c = bayes(table)
+        table = table[::20]
+        c(table)
+        vals, probs = c(table, c.ValueProbs)
+
+    def test_predict_numpy(self):
+        table = Orange.data.Table('titanic')
+        bayes = nb.BayesLearner()
+        c = bayes(table)
+        X = table.X[::20]
+        c(X)
+        vals, probs = c(X, c.ValueProbs)
