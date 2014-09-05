4efe8bc69ae872fd5774ba2fdf018cfc0a8fae36
astaric <anze.staric@gmail.com>
2014-09-05 16:43:26 +0200

    Travis: install only .whl requirements.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -qq libblas-dev liblapack-dev
 pip install -U setuptools pip wheel
 git clone https://github.com/astaric/orange3-requirements wheelhouse
-pip install wheelhouse/*
+pip install wheelhouse/*.whl
 python setup.py build_ext -i
