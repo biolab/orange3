115820f020293fc4d3b5cb9447d3a4fe34e87e1b
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2014-09-09 14:40:42 +0200

    Fixed an error in owdatasampler.py
    Fixes #91

--- Orange/widgets/data/owdatasampler.py
+++ Orange/widgets/data/owdatasampler.py
@@ -319,7 +319,7 @@ def sample_random_n(table, n, stratified=False, replace=False,
             random_state=random_state
         )
     else:
-        train_size = max(len(table.domain.class_var.values), n)
+        train_size = n
         test_size = max(len(table) - train_size, 0)
         ind = cross_validation.ShuffleSplit(
             len(table), n_iter=1, test_size=test_size,
