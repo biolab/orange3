754e72559175a27a7ce5175b358f0e521032f68a
Marko Toplak <marko.toplak@gmail.com>
2014-09-26 17:51:31 +0200

    Class value indexes should not be hardcoded as variables can be recycled.

--- Orange/tests/test_contingency.py
+++ Orange/tests/test_contingency.py
@@ -39,7 +39,7 @@ class Discrete_Test(unittest.TestCase):
 
         correct = [[2.2, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.6, 3.8],
                    [1, 4, 2, 4, 8, 2, 12, 4, 5, 3, 2, 1, 2]]
-        np.testing.assert_almost_equal(cont[2], correct)
+        np.testing.assert_almost_equal(cont[d.domain.class_var.values.index("Iris-virginica")], correct)
 
 
     @staticmethod
