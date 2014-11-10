da1c3d237fcaf2b4fa220f5e8a081c6a551a9d57
astaric <anze.staric@gmail.com>
2014-11-10 14:51:45 +0100

    Remove irrelevant widgets.

--- Orange/widgets/__init__.py
+++ Orange/widgets/__init__.py
@@ -9,10 +9,6 @@ def widget_discovery(discovery):
     #from . import data
     dist = pkg_resources.get_distribution("Orange")
     pkgs = ["Orange.widgets.data",
-            "Orange.widgets.visualize",
-            "Orange.widgets.classify",
-            "Orange.widgets.regression",
-            "Orange.widgets.evaluate",
-            "Orange.widgets.unsupervised"]
+            "Orange.widgets.visualize",]
     for pkg in pkgs:
         discovery.process_category_package(pkg, distribution=dist)
