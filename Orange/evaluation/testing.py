008fc455b2c89b16929d28b8b4cd4746ae5e7a35
Tomaz Curk <tomaz.curk@fri.uni-lj.si>
2014-11-14 10:40:49 +0100

    Minor.

--- Orange/evaluation/testing.py
+++ Orange/evaluation/testing.py
@@ -213,8 +213,9 @@ class CrossValidation(Testing):
 
     def __call__(self, data, fitters):
         n = len(data)
+        Y = data.Y.copy().flatten()
         indices = cross_validation.StratifiedKFold(
-            n, self.k, shuffle=True, random_state=self.random_state
+            Y, self.k, shuffle=True, random_state=self.random_state
         )
         results = Results(data, len(fitters), store_data=self.store_data)
 
