6ee3e7c5602c18bf1069b69b40aaf0030e265f2e
Tomaz Curk <tomaz.curk@fri.uni-lj.si>
2014-11-14 09:29:03 +0100

    Changes to answer comments.

--- Orange/tests/test_majority.py
+++ Orange/tests/test_majority.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 
 from Orange import data
-import Orange.classification.majority as maj
+import Orange.classification.majority as majority_
 
 
 class MajorityTest(unittest.TestCase):
@@ -13,7 +13,7 @@ class MajorityTest(unittest.TestCase):
         y = np.random.random_integers(1, 3, (nrows, 1)) // 2
         y[0] = 4
         t = data.Table(x, y)
-        learn = maj.MajorityFitter()
+        learn = majority_.MajorityFitter()
         clf = learn(t)
 
         x2 = np.random.random_integers(1, 3, (nrows, ncols))
@@ -28,7 +28,7 @@ class MajorityTest(unittest.TestCase):
         heavy = 3
         w = (y == heavy) * 123 + 1
         t = data.Table(x, y, W=w)
-        learn = maj.MajorityFitter()
+        learn = majority_.MajorityFitter()
         clf = learn(t)
 
         x2 = np.random.random_integers(1, 3, (nrows, ncols))
@@ -37,14 +37,14 @@ class MajorityTest(unittest.TestCase):
 
     def test_empty(self):
         iris = data.Table('iris')
-        learn = maj.MajorityFitter()
+        learn = majority_.MajorityFitter()
         clf = learn(iris[:0])
         y = clf(iris[0], clf.Probs)
         self.assertTrue(np.allclose(y, y.sum() / y.size))
 
     def test_missing(self):
         iris = data.Table('iris')
-        learn = maj.MajorityFitter()
+        learn = majority_.MajorityFitter()
         for e in iris[:len(iris)/2:2]:
             e.set_class("?")
         clf = learn(iris)
@@ -52,7 +52,7 @@ class MajorityTest(unittest.TestCase):
         self.assertTrue((y == 2).all())
 
         iris = data.Table('iris')
-        learn = maj.MajorityFitter()
+        learn = majority_.MajorityFitter()
         for e in iris:
             e.set_class("?")
         clf = learn(iris)
@@ -61,5 +61,5 @@ class MajorityTest(unittest.TestCase):
 
     def test_continuous(self):
         autompg = data.Table('auto-mpg')
-        learn = maj.MajorityFitter()
+        learn = majority_.MajorityFitter()
         self.assertRaises(ValueError, learn, autompg)
--- Orange/tests/test_mean.py
+++ Orange/tests/test_mean.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 
 from Orange import data
-import Orange.regression.mean as mea
+import Orange.regression.mean as mean_
 
 
 class MeanTest(unittest.TestCase):
@@ -12,7 +12,7 @@ class MeanTest(unittest.TestCase):
         x = np.random.random_integers(1, 3, (nrows, ncols))
         y = np.random.random_integers(0, 4, (nrows, 1)) / 3.0
         t = data.Table(x, y)
-        learn = mea.MeanFitter()
+        learn = mean_.MeanFitter()
         clf = learn(t)
 
         true_mean = np.average(y)
@@ -28,22 +28,22 @@ class MeanTest(unittest.TestCase):
         heavy = 1
         w = ((y == heavy) * 123 + 1.0 ) / 124.0
         t = data.Table(x, y, W=w)
-        learn = mea.MeanFitter()
+        learn = mean_.MeanFitter()
         clf = learn(t)
 
-        true_mean = np.average(y, weights=w)
+        expected_mean = np.average(y, weights=w)
         x2 = np.random.random_integers(1, 3, (nrows, ncols))
         y2 = clf(x2)
-        self.assertTrue(np.allclose(y2, true_mean))
+        self.assertTrue(np.allclose(y2, expected_mean))
 
     def test_empty(self):
         autompg = data.Table('auto-mpg')
-        learn = mea.MeanFitter()
+        learn = mean_.MeanFitter()
         clf = learn(autompg[:0])
         y = clf(autompg[0])
         self.assertTrue(y == 0)
 
     def test_discrete(self):
         iris = data.Table('iris')
-        learn = mea.MeanFitter()
+        learn = mean_.MeanFitter()
         self.assertRaises(ValueError, learn, iris)
