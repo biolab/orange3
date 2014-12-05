1db4e1478cfb620db36f692533d108bee4aea0ca
Marinka Zitnik <marinka@zitnik.si>
2014-12-05 12:04:43 +0100

    Add cosine, manhattan and jaccard distance.

--- Orange/distance/__init__.py
+++ Orange/distance/__init__.py
@@ -9,12 +9,68 @@ from Orange.misc import DistMatrix
 class Euclidean():
     """Euclidean distance."""
     def __call__(self, e1, e2=None, axis=1):
+        x1 = e1.x if isinstance(e1, data.RowInstance) else e1.X
+        x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X if e2 is not None else None
         if axis == 0:
-            if isinstance(e1, data.Table):
-                e1 = e1.X.T
-            if e2 is not None and isinstance(e2, data.Table):
-                e2 = e2.X.T
-        dist = metrics.pairwise.pairwise_distances(e1, e2, metric='euclidean')
+            x1 = x1.T
+            if x2 is not None:
+                x2 = x2.T
+        dist = metrics.pairwise.pairwise_distances(x1, x2, metric='euclidean')
+        if dist.size == 1:
+            dist = dist[0, 0]
+        else:
+            dist = DistMatrix(dist)
+        return dist
+
+
+class Manhattan():
+    """Manhattan distance."""
+    def __call__(self, e1, e2=None, axis=1):
+        x1 = e1.x if isinstance(e1, data.RowInstance) else e1.X
+        x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X if e2 is not None else None
+        if axis == 0:
+            x1 = x1.T
+            if x2 is not None:
+                x2 = x2.T
+        dist = metrics.pairwise.pairwise_distances(x1, x2, metric='manhattan')
+        if dist.size == 1:
+            dist = dist[0, 0]
+        else:
+            dist = DistMatrix(dist)
+        return dist
+
+
+class Cosine():
+    """Cosine distance."""
+    def __call__(self, e1, e2=None, axis=1):
+        x1 = e1.x if isinstance(e1, data.RowInstance) else e1.X
+        x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X if e2 is not None else None
+        if axis == 0:
+            x1 = x1.T
+            if x2 is not None:
+                x2 = x2.T
+        dist = metrics.pairwise.pairwise_distances(x1, x2, metric='cosine')
+        if dist.size == 1:
+            dist = dist[0, 0]
+        else:
+            dist = DistMatrix(dist)
+        return dist
+
+
+class Jaccard():
+    """Jaccard distance"""
+    def __call__(self, e1, e2=None, axis=1):
+        x1 = e1.x if isinstance(e1, data.RowInstance) else e1.X
+        x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X if e2 is not None else None
+        if axis == 0:
+            x1 = x1.T
+            if x2 is not None:
+                x2 = x2.T
+        if isinstance(e1, data.RowInstance):
+            x1 = x1.reshape(1, len(x1))
+        if isinstance(e2, data.RowInstance):
+            x2 = x2.reshape(1, len(x2))
+        dist = metrics.pairwise.pairwise_distances(x1, x2, metric='jaccard')
         if dist.size == 1:
             dist = dist[0, 0]
         else:
@@ -25,12 +81,17 @@ class Euclidean():
 class Mahalanobis():
     """ Mahalanobis distance."""
     def __call__(self, e1, e2=None, VI=None, axis=1):
+        x1 = e1.x if isinstance(e1, data.RowInstance) else e1.X
+        x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X if e2 is not None else None
         if axis == 0:
-            if isinstance(e1, data.Table):
-                e1 = e1.X.T
-            if e2 is not None and isinstance(e2, data.Table):
-                e2 = e2.X.T
-        dist = metrics.pairwise.pairwise_distances(e1, e2, metric='mahalanobis', VI=VI)
+            x1 = x1.T
+            if x2 is not None:
+                x2 = x2.T
+        if isinstance(e1, data.RowInstance):
+            x1 = x1.reshape(1, len(x1))
+        if isinstance(e2, data.RowInstance):
+            x2 = x2.reshape(1, len(x2))
+        dist = metrics.pairwise.pairwise_distances(x1, x2, metric='mahalanobis', VI=VI)
         if dist.size == 1:
             dist = dist[0, 0]
         else:
@@ -53,19 +114,25 @@ class SpearmanR():
         where r is Spearman's rank coefficient.
         """
         x1 = e1.x if isinstance(e1, data.RowInstance) else e1.X
-        if e2 is not None:
-            x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X
-        else:
-            x2 = x1
+        x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X if e2 is not None else x1
         if x1.ndim == 1 or x2.ndim == 1:
             axis = 0
-            slc = len(e1) if x1.ndim > 1 else 1
+            slc = len(x1) if x1.ndim > 1 else 1
         else:
-            slc = len(e1) if axis == 1 else len(e1.domain.attributes)
+            slc = len(x1) if axis == 1 else len(e1.domain.attributes)
+        # stats.spearmanr does not work when e1=Table and e2=RowInstance
+        # so we replace e1 and e2 and then transpose the result
+        transpose = False
+        if isinstance(e1, data.Table) and isinstance(e2, data.RowInstance):
+            x1, x2 = x2, x1
+            slc = len(e1) if x1.ndim > 1 else 1
+            transpose = True
         rho, _ = stats.spearmanr(x1, x2, axis=axis)
         dist = (1. - rho) / 2.
         if isinstance(dist, np.ndarray):
             dist = dist[:slc, slc:]
+            if transpose:
+                dist = dist.T
             dist = DistMatrix(dist)
         return dist
 
@@ -82,19 +149,25 @@ class SpearmanRAbsolute():
         where r is Spearman's correlation coefficient.
         """
         x1 = e1.x if isinstance(e1, data.RowInstance) else e1.X
-        if e2 is not None:
-            x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X
-        else:
-            x2 = x1
+        x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X if e2 is not None else x1
         if x1.ndim == 1 or x2.ndim == 1:
             axis = 0
-            slc = len(e1) if x1.ndim > 1 else 1
+            slc = len(x1) if x1.ndim > 1 else 1
         else:
             slc = len(e1) if axis == 1 else len(e1.domain.attributes)
+        # stats.spearmanr does not work when e1=Table and e2=RowInstance
+        # so we replace e1 and e2 and then transpose the result
+        transpose = False
+        if isinstance(e1, data.Table) and isinstance(e2, data.RowInstance):
+            x1, x2 = x2, x1
+            slc = len(e1) if x1.ndim > 1 else 1
+            transpose = True
         rho, _ = stats.spearmanr(x1, x2, axis=axis)
         dist = (1. - np.abs(rho)) / 2.
         if isinstance(dist, np.ndarray):
             dist = dist[:slc, slc:]
+            if transpose:
+                dist = dist.T
             dist = DistMatrix(dist)
         return dist
 
@@ -113,25 +186,17 @@ class PearsonR():
 
         where r is Pearson's rank coefficient.
         """
-        # get data from Orange.data.Table to numpy.array
         x1 = e1.x if isinstance(e1, data.RowInstance) else e1.X
-        if e2 is not None:
-            x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X
-        else:
-            x2 = x1
-        # transform
+        x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X if e2 is not None else x1
         if axis == 0:
             x1 = x1.T
             x2 = x2.T
-        # prepare so, that can be used for iterator
         if x1.ndim == 1:
             x1 = list([x1])
         if x2.ndim == 1:
             x2 = list([x2])
-        # TODO: implement pearson that works on full matrices
         rho = np.array([[stats.pearsonr(i, j)[0] for j in x2] for i in x1])
         dist = (1. - rho) / 2.
-        # if only one, return float, else matrix
         if dist.size == 1:
             dist = dist[0][0]
         else:
@@ -153,25 +218,17 @@ class PearsonRAbsolute():
 
         where r is Pearson's rank coefficient.
         """
-        # get data from Orange.data.Table to numpy.array
         x1 = e1.x if isinstance(e1, data.RowInstance) else e1.X
-        if e2 is not None:
-            x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X
-        else:
-            x2 = x1
-        # transform
+        x2 = e2.x if isinstance(e2, data.RowInstance) else e2.X if e2 is not None else x1
         if axis == 0:
             x1 = x1.T
             x2 = x2.T
-        # prepare so, that can be used for iterator
         if x1.ndim == 1:
             x1 = list([x1])
         if x2.ndim == 1:
             x2 = list([x2])
-        # TODO: implement pearson that works on full matrices
         rho = np.array([[stats.pearsonr(i, j)[0] for j in x2] for i in x1])
         dist = (1. - np.abs(rho)) / 2.
-        # if only one, return float, else matrix
         if dist.size == 1:
             dist = dist[0][0]
         else:
