e96d11af3d794712b6ebc8e541917185a4f6f3df
janezd <janez.demsar@fri.uni-lj.si>
2015-01-30 12:30:59 +0100

    Clean up Orange.__init__

--- Orange/__init__.py
+++ Orange/__init__.py
@@ -1,6 +1,3 @@
-from __future__ import absolute_import
-from importlib import import_module
-
 try:
     from .import version
     # Always use short_version here (see PEP 386)
@@ -12,25 +9,27 @@ except ImportError:
 
 ADDONS_ENTRY_POINT = 'orange.addons'
 
-import warnings
-import pkg_resources
 
-alreadyWarned = False
-disabledMsg = "Some features will be disabled due to failing modules\n"
+already_warned = False
+disabled_msg = "Some features will be disabled due to failing modules\n"
 
 
 def _import(name):
-    global alreadyWarned
+    from importlib import import_module
+    import warnings
+    global already_warned
     try:
         import_module(name, package='Orange')
     except ImportError as err:
         warnings.warn("%sImporting '%s' failed: %s" %
-                      (disabledMsg if not alreadyWarned else "", name, err),
+                      (disabled_msg if not already_warned else "", name, err),
                       UserWarning, 2)
-        alreadyWarned = True
+        already_warned = True
 
 
+# noinspection PyUnresolvedReferences
 def import_all():
+    from importlib import import_module
     import Orange
     for name in ["classification", "clustering", "data", "distance",
                  "evaluation", "feature", "misc", "regression", "statistics"]:
@@ -46,5 +45,5 @@ def import_all():
 _import(".data")
 
 del _import
-del alreadyWarned
-del disabledMsg
+del already_warned
+del disabled_msg
