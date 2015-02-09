e9b1ac6df9cae757a2014ae810f3919dc2f899d8
janezd <janez.demsar@fri.uni-lj.si>
2015-02-09 15:21:53 +0100

    Add Excel format to File widget

--- Orange/widgets/data/owfile.py
+++ Orange/widgets/data/owfile.py
@@ -43,12 +43,13 @@ class OWFile(widget.OWWidget):
     registered_file_types = []
     dlgFormats = (
         "Tab-delimited files (*.tab)\n"
+        "Excel files (*.xlsx)\n"
         "Text file (*.txt)\n"
         "Basket files (*.basket)\n" +
         "".join("{0} ({1})\n".format(*ft) for ft in registered_file_types) +
         "All files (*)")
-    formats = {".tab": "Tab-delimited file", ".txt": "Text file",
-               ".basket": "Basket file"}
+    formats = {".tab": "Tab-delimited file", ".xlsx": "Excel file",
+               ".txt": "Text file", ".basket": "Basket file"}
     formats.update(dict((ft[1][2:], ft[0]) for ft in registered_file_types))
 
     def __init__(self):
