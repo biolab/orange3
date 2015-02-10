1a856680adce15b9425811181d897bab22e9f756
Lan Zagar <lan.zagar@fri.uni-lj.si>
2015-02-10 11:01:04 +0100

    Move Orange.feature.scoring to Orange.preprocess.score

--- Orange/preprocess/__init__.py
+++ Orange/preprocess/__init__.py
@@ -2,4 +2,5 @@ from .continuize import *
 from .discretize import *
 from .impute import *
 from .preprocess import *
+from .score import *
 from .transformation import *
--- Orange/tests/test_feature_scoring.py
+++ Orange/tests/test_feature_scoring.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 
 from Orange.data import Table, Domain
-from Orange.feature import scoring
+from Orange.preprocess import score
 from Orange import preprocess
 
 
@@ -14,19 +14,19 @@ class FeatureScoringTest(unittest.TestCase):
         self.housing = Table("housing")  # cont. features, cont. class
 
     def test_info_gain(self):
-        scorer = scoring.InfoGain()
+        scorer = score.InfoGain()
         correct = [0.79067, 0.71795, 0.83014, 0.97432, 0.46970]
         np.testing.assert_almost_equal([scorer(a, self.zoo) for a in range(5)],
                                        correct, decimal=5)
 
     def test_gain_ratio(self):
-        scorer = scoring.GainRatio()
+        scorer = score.GainRatio()
         correct = [0.80351, 1.00000, 0.84754, 1.00000, 0.59376]
         np.testing.assert_almost_equal([scorer(a, self.zoo) for a in range(5)],
                                        correct, decimal=5)
 
     def test_gini(self):
-        scorer = scoring.Gini()
+        scorer = score.Gini()
         correct = [0.11893, 0.10427, 0.13117, 0.14650, 0.05973]
         np.testing.assert_almost_equal([scorer(a, self.zoo) for a in range(5)],
                                        correct, decimal=5)
@@ -34,29 +34,29 @@ class FeatureScoringTest(unittest.TestCase):
     def test_classless(self):
         classless = Table(Domain(self.zoo.domain.attributes),
                           self.zoo[:, 0:-1])
-        scorers = [scoring.Gini(), scoring.InfoGain(), scoring.GainRatio()]
+        scorers = [score.Gini(), score.InfoGain(), score.GainRatio()]
         for scorer in scorers:
             with self.assertRaises(ValueError):
                 scorer(0, classless)
 
     def test_wrong_class_type(self):
-        scorers = [scoring.Gini(), scoring.InfoGain(), scoring.GainRatio()]
+        scorers = [score.Gini(), score.InfoGain(), score.GainRatio()]
         for scorer in scorers:
             with self.assertRaises(ValueError):
                 scorer(0, self.housing)
 
         with self.assertRaises(ValueError):
-            scoring.Chi2(2, self.housing)
+            score.Chi2(2, self.housing)
         with self.assertRaises(ValueError):
-            scoring.ANOVA(2, self.housing)
-        scoring.UnivariateLinearRegression(2, self.housing)
+            score.ANOVA(2, self.housing)
+        score.UnivariateLinearRegression(2, self.housing)
 
     def test_chi2(self):
         nrows, ncols = 500, 5
         X = np.random.randint(4, size=(nrows, ncols))
         y = 10 + (-3*X[:, 1] + X[:, 3]) // 2
         data = preprocess.DiscretizeTable(Table(X, y))
-        scorer = scoring.Chi2()
+        scorer = score.Chi2()
         sc = [scorer(a, data) for a in range(ncols)]
         self.assertTrue(np.argmax(sc) == 1)
 
@@ -65,7 +65,7 @@ class FeatureScoringTest(unittest.TestCase):
         X = np.random.rand(nrows, ncols)
         y = 4 + (-3*X[:, 1] + X[:, 3]) // 2
         data = Table(X, y)
-        scorer = scoring.ANOVA()
+        scorer = score.ANOVA()
         sc = [scorer(a, data) for a in range(ncols)]
         self.assertTrue(np.argmax(sc) == 1)
 
@@ -74,6 +74,6 @@ class FeatureScoringTest(unittest.TestCase):
         X = np.random.rand(nrows, ncols)
         y = (-3*X[:, 1] + X[:, 3]) / 2
         data = Table(X, y)
-        scorer = scoring.UnivariateLinearRegression()
+        scorer = score.UnivariateLinearRegression()
         sc = [scorer(a, data) for a in range(ncols)]
         self.assertTrue(np.argmax(sc) == 1)
