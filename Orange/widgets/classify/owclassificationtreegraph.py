97fef4eacb736adeb620128ef26234773ec5f751
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2015-02-18 15:32:41 +0100

    Fix Classification Tree Graph due to simplified TreeClassifier.

--- Orange/widgets/classify/owclassificationtreegraph.py
+++ Orange/widgets/classify/owclassificationtreegraph.py
@@ -1,7 +1,8 @@
 import sys
 
 import numpy
-from numpy import argmax, zeros
+
+from sklearn.tree._tree import TREE_LEAF
 
 from Orange.widgets.classify.owtreeviewer2d import *
 
@@ -132,13 +133,13 @@ class OWClassificationTreeGraph(OWTreeViewer2D):
         palette = self.scene.colorPalette
         for node in self.scene.nodes():
             distr = node.get_distribution()
-            total = sum(distr)
+            total = numpy.sum(distr)
             if self.target_class_index:
                 p = distr[self.target_class_index - 1] / total
                 color = palette[self.target_class_index].light(200 - 100 * p)
             else:
                 modus = node.majority()
-                p = distr[modus] / total
+                p = distr[modus] / (total or 1)
                 color = palette[int(modus)].light(400 - 300 * p)
             node.backgroundBrush = QBrush(color)
         self.scene.update()
@@ -166,31 +167,32 @@ class OWClassificationTreeGraph(OWTreeViewer2D):
             self.target_combo.addItems(self.domain.class_vars[0].values)
             self.target_class_index = 0
             self.openContext(self.domain.class_var)
-            self.root_node = self.walkcreate(self.tree, None, distr=clf.distr)
+            self.root_node = self.walkcreate(self.tree, 0, None)
             self.info.setText(
                 '{} nodes, {} leaves'.
-                format(self.root_node.num_nodes(),
-                       self.root_node.num_leaves()))
+                format(self.tree.node_count,
+                       numpy.count_nonzero(
+                            self.tree.children_left == TREE_LEAF)))
+
             self.scene.fix_pos(self.root_node, self._HSPACING, self._VSPACING)
             self.activate_loaded_settings()
             self.scene_view.centerOn(self.root_node.x(), self.root_node.y())
             self.update_node_tooltips()
         self.scene.update()
 
-    def walkcreate(self, tree, parent=None, level=0, i=0, distr=None):
+    def walkcreate(self, tree, node_id, parent=None):
         node = ClassificationTreeNode(tree, self.domain, parent, None,
-                                      self.scene, i=i, distr=distr[i])
+                                      self.scene, i=node_id)
         if parent:
             parent.graph_add_edge(
                 GraphicsEdge(None, self.scene, node1=parent, node2=node))
-        left_child_index = tree.children_left[i]
-        right_child_index = tree.children_right[i]
-        if left_child_index >= 0:
-            self.walkcreate(tree, parent=node, level=level+1,
-                            i=left_child_index, distr=distr)
-        if right_child_index >= 0:
-            self.walkcreate(tree, parent=node, level=level+1,
-                            i=right_child_index, distr=distr)
+        left_child_index = tree.children_left[node_id]
+        right_child_index = tree.children_right[node_id]
+
+        if left_child_index != TREE_LEAF:
+            self.walkcreate(tree, node_id=left_child_index, parent=node)
+        if right_child_index != TREE_LEAF:
+            self.walkcreate(tree, node_id=right_child_index, parent=node)
         return node
 
     def node_tooltip(self, node):
@@ -256,6 +258,30 @@ class PieChart(QGraphicsRectItem):
         painter.drawEllipse(-self.r, -self.r, 2 * self.r, 2 * self.r)
 
 
+def _subnode_range(tree, node_id):
+    right = left = node_id
+    if tree.children_left[left] != TREE_LEAF:
+        left = tree.children_left[left]
+    else:
+        assert tree.children_right[node_id] == TREE_LEAF
+
+    while tree.children_right[right] != TREE_LEAF:
+        right = tree.children_right[right]
+
+    return left, right
+
+
+def _leaf_indices(tree, node_id):
+    left, right = _subnode_range(tree, node_id)
+    if left == right:
+        # leaf
+        return numpy.array([node_id], dtype=int)
+    else:
+        isleaf = tree.children_left[left: right + 1] == TREE_LEAF
+        assert numpy.flatnonzero(isleaf).size > 0
+        return left + numpy.flatnonzero(isleaf)
+
+
 class ClassificationTreeNode(GraphicsNode):
     def __init__(self, tree, domain, parent=None, parent_item=None,
                  scene=None, i=0, distr=None):
@@ -264,6 +290,7 @@ class ClassificationTreeNode(GraphicsNode):
         self.tree = tree
         self.domain = domain
         self.i = i
+        self.node_id = i
         self.parent = parent
         self.pie = PieChart(self.get_distribution(), 8, self, scene)
         fm = QFontMetrics(self.document().defaultFont())
@@ -277,54 +304,25 @@ class ClassificationTreeNode(GraphicsNode):
         """
         :return: Distribution of class values.
         """
-        d = zeros((self.tree.value.shape[2], ), dtype="float")
-        for k, v in self.distribution.items():
-            d[k] = v
-        return list(d / d.sum())
+        if self.is_leaf():
+            counts = self.tree.value[self.node_id]
+        else:
+            leaf_ind = _leaf_indices(self.tree, self.node_id)
+            values = self.tree.value[leaf_ind]
+            counts = numpy.sum(values, axis=0)
 
-    def num_nodes(self):
-        """
-        :return: Number of nodes below particular node.
-        """
-        return self.num_nodesw(self.i)
-
-    def num_nodesw(self, i=0):
-        """
-        :param i: index of current node.
-        :return: Number of nodes below particular node.
-        """
-        s = 1
-        if self.tree.children_left[i] > 0:
-            s += self.num_nodesw(i=self.tree.children_left[i])
-        if self.tree.children_right[i] > 0:
-            s += self.num_nodesw(i=self.tree.children_right[i])
-        return s
+        assert counts.shape[0] == 1, "n_outputs > 1 "
+        counts = counts[0]
+        counts_sum = numpy.sum(counts)
+        if counts_sum > 0:
+            counts /= counts_sum
+        return counts
 
     def num_instances(self):
         """
         :return: Number of instances in a particular node.
         """
         return self.tree.n_node_samples[self.i]
-
-    def num_leaves(self, i=0):
-        """
-        :return: Number of leaves below a particular node.
-        """
-        return self.num_leavesw(i=self.i)
-
-    def num_leavesw(self, i=0):
-        """
-        :param i: index of current node.
-        :return: Number of leaves below particular node.
-        """
-        s = 0
-        if self.tree.children_left[i] < 0 and self.tree.children_right[i] < 0:
-            return 1
-        if self.tree.children_left[i] > 0:
-            s += self.num_leavesw(i=self.tree.children_left[i])
-        if self.tree.children_right[i] > 0:
-            s += self.num_leavesw(i=self.tree.children_right[i])
-        return s
 
     def split_condition(self):
         """
@@ -368,28 +366,21 @@ class ClassificationTreeNode(GraphicsNode):
         """
         :return: Node is leaf
         """
-        return self.tree.children_left[self.i] < 0 and \
-            self.tree.children_right[self.i] < 0
+        return self.tree.children_left[self.node_id] < 0 and \
+            self.tree.children_right[self.node_id] < 0
 
     def attribute(self):
         """
         :return: Node attribute index.
         """
-        return self.attributew(i=self.i)
-
-    def attributew(self, i=0):
-        """
-        :return:
-            Attribute at node to split on.
-        """
-        return self.tree.feature[i]
+        return self.tree.feature[self.node_id]
 
     def majority(self):
         """
         :return:
             Majority class at node.
         """
-        return argmax(self.get_distribution())
+        return numpy.argmax(self.get_distribution())
 
     def update_contents(self):
         self.prepareGeometryChange()
@@ -450,11 +441,13 @@ class ClassificationTreeNode(GraphicsNode):
         painter.setClipRect(rect)
         return QGraphicsTextItem.paint(self, painter, option, widget)
 
+
 if __name__ == "__main__":
     from Orange.classification.tree import TreeLearner
     a = QApplication(sys.argv)
     ow = OWClassificationTreeGraph()
     ow.ctree(TreeLearner(max_depth=3)(Table('iris')))
     ow.show()
+    ow.raise_()
     a.exec_()
     ow.saveSettings()
