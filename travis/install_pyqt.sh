cc42d524faefd12412896a74733194a9631b7c84
astaric <anze.staric@gmail.com>
2015-02-20 13:57:06 +0100

    Fix travis build.
    PyQt make was run as a different user resulting in a different path.

--- travis/install_pyqt.sh
+++ travis/install_pyqt.sh
@@ -1,4 +1,5 @@
 # PyQt requirements
+ln -s $TRAVIS_BUILD_DIR/../../biolab $TRAVIS_BUILD_DIR/../../astaric
 cd $TRAVIS_BUILD_DIR/wheelhouse/pyqt/sip-4.16.5
 sudo make install
 cd $TRAVIS_BUILD_DIR/wheelhouse/pyqt/PyQt-x11-gpl-4.11.3
