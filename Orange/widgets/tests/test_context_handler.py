d496b26971573f81a67f6093d1dc0457a7ea102c
astaric <anze.staric@gmail.com>
2015-03-20 09:48:18 +0100

    Add tests for ContextHandler.initialize.

---
+++ Orange/widgets/tests/test_context_handler.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+from unittest.mock import Mock
+from Orange.widgets.settings import ContextHandler, Setting
+
+__author__ = 'anze'
+
+
+class SimpleWidget:
+    name = "Simple Widget"
+
+    setting = Setting(42)
+
+
+class ContextHandlerTestCase(TestCase):
+    def test_initialize(self):
+        handler = ContextHandler()
+        handler.provider = Mock()
+
+        # Context settings from data
+        widget = SimpleWidget()
+        handler.initialize(widget, {'context_settings': 5})
+        self.assertTrue(hasattr(widget, 'context_settings'))
+        self.assertEqual(widget.context_settings, 5)
+
+        # Default (global) context settings
+        widget = SimpleWidget()
+        handler.initialize(widget)
+        self.assertTrue(hasattr(widget, 'context_settings'))
+        self.assertEqual(widget.context_settings, handler.global_contexts)
