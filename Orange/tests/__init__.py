6a21792d6a5e2c7baca030fd3a40371f1883047b
astaric <anze.staric@gmail.com>
2015-04-07 15:00:11 +0200

    Run settings handler tests in python setup.py test.

--- Orange/tests/__init__.py
+++ Orange/tests/__init__.py
@@ -2,11 +2,16 @@ import os
 import unittest
 
 try:
-  from Orange.widgets.tests import test_settings, test_setting_provider
-  from Orange.widgets.data.tests import test_owselectcolumns
-  run_widget_tests = True
+    from Orange.widgets.tests import test_setting_provider, \
+        test_settings_handler, test_context_handler, \
+        test_class_values_context_handler, test_domain_context_handler, \
+        test_widget
+    from Orange.widgets.data.tests import test_owselectcolumns
+
+    run_widget_tests = True
 except ImportError:
-  run_widget_tests = False
+    run_widget_tests = False
+
 
 def suite():
     test_dir = os.path.dirname(__file__)
@@ -14,16 +19,22 @@ def suite():
         unittest.TestLoader().discover(test_dir),
     ]
     if run_widget_tests:
-      all_tests.extend([
-        unittest.TestLoader().loadTestsFromModule(test_settings),
-        unittest.TestLoader().loadTestsFromModule(test_setting_provider),
-        unittest.TestLoader().loadTestsFromModule(test_owselectcolumns),
-      ])
+        load = unittest.TestLoader().loadTestsFromModule
+        all_tests.extend([
+            load(test_setting_provider),
+            load(test_settings_handler),
+            load(test_context_handler),
+
+            load(test_class_values_context_handler),
+            load(test_domain_context_handler),
+            load(test_owselectcolumns),
+
+            load(test_widget)
+        ])
     return unittest.TestSuite(all_tests)
 
 
 test_suite = suite()
-
 
 if __name__ == '__main__':
     unittest.main(defaultTest='suite')
