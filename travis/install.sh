a70895d5c48ae47f3830707d79cbcb1d892c2605
astaric <anze.staric@gmail.com>
2015-07-18 15:06:47 +0200

    Use custom PostgreSQL on Travis.
    Building our own PostgreSQL allows us to install custom extensions and
    use prerelease versions.

--- travis/install.sh
+++ travis/install.sh
@@ -1,4 +1,4 @@
-pip install -U setuptools pip wheel pgxnclient
+pip install -U setuptools pip wheel
 if [ ! "$(ls wheelhouse)" ]; then
     git clone -b pyqt --depth=1 https://github.com/astaric/orange3-requirements wheelhouse
 else
@@ -9,15 +9,3 @@ pip install wheelhouse/*.whl
 pip install -r requirements.txt
 
 python setup.py build_ext -i
-
-# Sql requirements
-#~ pip install wheelhouse/sql/*.whl
-#~ psql -c 'create database test;' -U postgres
-#~ cd wheelhouse/sql/quantile-1.1.3
-#~ sudo make install
-#~ cd ../../..
-#~ cd wheelhouse/sql/binning
-#~ sudo make install
-#~ cd ../../..
-#~ psql test -c 'CREATE EXTENSION quantile;' -U postgres
-#~ psql test -c 'CREATE EXTENSION binning;' -U postgres
