ddf02098b57c1ede7da04b6bf6ff62e52393b32e
Lan Zagar <lan.zagar@fri.uni-lj.si>
2015-07-21 11:52:10 +0200

    Removed deprecated parameter (random_state).
    See http://scikit-learn.org/stable/modules/generated/sklearn.cluster.DBSCAN.html#sklearn.cluster.DBSCAN

--- Orange/tests/test_clustering_dbscan.py
+++ Orange/tests/test_clustering_dbscan.py
@@ -6,12 +6,10 @@ from Orange.clustering.dbscan import DBSCAN
 
 class DBSCANTest(unittest.TestCase):
 
-
     def test_dbscan_parameters(self):
         table = Orange.data.Table('iris')
         dbscan = DBSCAN(eps=0.1, min_samples=7, metric='euclidean',
-                        algorithm='auto', leaf_size=12, p=None,
-                        random_state=42)
+                        algorithm='auto', leaf_size=12, p=None)
         c = dbscan(table)
 
     def test_predict_table(self):
@@ -27,4 +25,3 @@ class DBSCANTest(unittest.TestCase):
         c = dbscan(table)
         X = table.X[::20]
         p = c(X)
-
