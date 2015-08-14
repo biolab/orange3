213777b4911f092b6476ac47170e1333055b69bb
acopar <andrej.copar@gmail.com>
2015-08-14 13:59:15 +0000

    owunivariateregression: Create Univariate regression widget
    owunivariateregression widget,
    linear: add PolynomialFeatures and PolynomialModel

--- Orange/regression/linear.py
+++ Orange/regression/linear.py
@@ -2,12 +2,13 @@ import sklearn.linear_model as skl_linear_model
 import sklearn.pipeline as skl_pipeline
 import sklearn.preprocessing as skl_preprocessing
 
-from Orange.regression import Model, SklLearner
+from Orange.regression import Learner, Model, SklLearner
 
 
 __all__ = ["LinearRegressionLearner", "RidgeRegressionLearner",
            "LassoRegressionLearner", "SGDRegressionLearner",
-           "ElasticNetLearner", "ElasticNetCVLearner"]
+           "ElasticNetLearner", "ElasticNetCVLearner",
+           "PolynomialLearner"]
 
 
 class LinearRegressionLearner(SklLearner):
@@ -82,21 +83,40 @@ class SGDRegressionLearner(SklLearner):
         self.params = vars()
 
     def fit(self, X, Y, W):
-        sk = skl_linear_model.SGDRegressor(**self.params)
+        sk = self.__wraps__(**self.params)
         clf = skl_pipeline.Pipeline(
             [('scaler', skl_preprocessing.StandardScaler()), ('sgd', sk)])
         clf.fit(X, Y.ravel())
         return LinearModel(clf)
 
 
+class PolynomialLearner(Learner):
+    name = 'poly learner'
+
+    def __init__(self, learner, degree=1, preprocessors=None):
+        super().__init__(preprocessors=preprocessors)
+        self.degree = degree
+        self.learner = learner
+    
+    def fit(self, X, Y, W):
+        polyfeatures = skl_preprocessing.PolynomialFeatures(self.degree)
+        X = polyfeatures.fit_transform(X)
+        clf = self.learner
+        if W is None or not self.supports_weights:
+            model = clf.fit(X, Y, None)
+        else:
+            model = clf.fit(X, Y, sample_weight=W.reshape(-1))
+        return PolynomialModel(model, polyfeatures)
+
+
 class LinearModel(Model):
     supports_multiclass = True
 
-    def __init__(self, skmodel):
-        self.skmodel = skmodel
+    def __init__(self, model):
+        self.model = model
 
-    def predict(self, table):
-        vals = self.skmodel.predict(table)
+    def predict(self, X):
+        vals = self.model.predict(X)
         if len(vals.shape) == 1:
             # Prevent IndexError for 1D array
             return vals
@@ -106,4 +126,18 @@ class LinearModel(Model):
             return vals
 
     def __str__(self):
-        return 'LinearModel {}'.format(self.skmodel)
+        return 'LinearModel {}'.format(self.model)
+
+class PolynomialModel(Model):
+    supports_multiclass = True
+
+    def __init__(self, model, polyfeatures):
+        self.model = model
+        self.polyfeatures = polyfeatures
+
+    def predict(self, X):
+        X = self.polyfeatures.fit_transform(X)
+        return self.model.predict(X)
+
+    def __str__(self):
+        return 'PolynomialModel {}'.format(self.model)
---
+++ Orange/widgets/regression/owunivariateregression.py
@@ -0,0 +1,232 @@
+from PyQt4.QtGui import QLayout
+from PyQt4 import QtCore
+from PyQt4 import QtGui
+from PyQt4.QtGui import QColor, QPen
+from PyQt4.QtCore import QRectF
+
+import pyqtgraph as pg
+import numpy as np
+
+from Orange.data import Table, Domain
+from Orange.regression.linear import (RidgeRegressionLearner, LinearModel,
+                                      LinearRegressionLearner, PolynomialLearner)
+from Orange.regression import Learner
+from Orange.preprocess.preprocess import Preprocess
+from Orange.widgets import widget, settings, gui
+from Orange.widgets.utils import itemmodels
+
+
+class OWUnivariateRegression(widget.OWWidget):
+    name = "Univariate Regression"
+    description = "Univariate regression with polynomial expansion."
+    icon = "icons/UnivariateRegression.svg"
+
+    inputs = [("Data", Table, "set_data", widget.Default),
+              ("Preprocessor", Preprocess, "set_preprocessor"),
+              ("Learner", Learner, "set_learner")]
+    outputs = [("Learner", Learner),
+               ("Predictor", LinearModel)]
+    
+    learner_name = settings.Setting("Univariate Regression")
+    
+    polynomialexpansion = settings.Setting(1)
+    
+    x_var_index = settings.ContextSetting(0)
+    y_var_index = settings.ContextSetting(1)
+    
+    want_main_area = True
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.data = None
+        self.preprocessors = None
+        self.learner = None
+        self.scatterplot_item = None
+        self.plot_item = None
+        
+        self.x_label = 'x'
+        self.y_label = 'y'
+        
+        box = gui.widgetBox(self.controlArea, "Learner/Predictor Name")
+        gui.lineEdit(box, self, "learner_name")
+
+        box = gui.widgetBox(self.controlArea, "Input")
+
+        self.x_var_model = itemmodels.VariableListModel()
+        self.comboBoxAttributesX = gui.comboBox(
+            box, self, value='x_var_index',
+            callback=self.apply)
+        self.comboBoxAttributesX.setModel(self.x_var_model)
+
+        gui.doubleSpin(box, self, "polynomialexpansion", 0, 10, 
+            label="Polynomial expansion:", callback=self.apply)
+
+        self.y_var_model = itemmodels.VariableListModel()
+        self.comboBoxAttributesY = gui.comboBox(
+            self.controlArea, self, value='y_var_index', box='Target',
+            callback=self.apply)
+        self.comboBoxAttributesY.setModel(self.y_var_model)
+        
+        gui.rubber(self.controlArea)
+        
+        # main area GUI
+        self.plotview = pg.PlotWidget(background="w")
+        self.plot = self.plotview.getPlotItem()
+
+        axis_color = self.palette().color(QtGui.QPalette.Text)
+        axis_pen = QtGui.QPen(axis_color)
+
+        tickfont = QtGui.QFont(self.font())
+        tickfont.setPixelSize(max(int(tickfont.pixelSize() * 2 // 3), 11))
+        
+        axis = self.plot.getAxis("bottom")
+        axis.setLabel(self.x_label)
+        axis.setPen(axis_pen)
+        axis.setTickFont(tickfont)
+
+        axis = self.plot.getAxis("left")
+        axis.setLabel(self.y_label)
+        axis.setPen(axis_pen)
+        axis.setTickFont(tickfont)
+
+        self.plot.setRange(xRange=(0.0, 1.0), yRange=(0.0, 1.0),
+                           disableAutoRange=True)
+        
+        self.mainArea.layout().addWidget(self.plotview)
+
+        self.apply()
+
+    def clear(self):
+        self.data = None
+        self.clear_plot()
+        
+    def clear_plot(self):
+        if self.plot_item is not None:
+            self.plot_item.setParentItem(None)
+            self.plotview.removeItem(self.plot_item)
+            self.plot_item = None
+            
+        if self.scatterplot_item is not None:
+            self.scatterplot_item.setParentItem(None)
+            self.plotview.removeItem(self.scatterplot_item)
+            self.scatterplot_item = None
+        
+        self.plotview.clear()
+    
+    def set_data(self, data):
+        self.clear()
+        self.data = data
+        if data is not None:
+            cvars = [var for var in data.domain.variables if var.is_continuous]
+            class_cvars = [var for var in data.domain.class_vars if var.is_continuous]
+            
+            self.x_var_model[:] = cvars
+            self.y_var_model[:] = cvars
+            
+            nvars = len(cvars)
+            nclass = len(class_cvars)
+            self.x_var_index = min(max(0, self.x_var_index), nvars - 1)
+            if nclass > 0:
+                self.y_var_index = min(max(0, nvars-nclass), nvars - 1)
+            else:
+                self.y_var_index = min(max(0, nvars-1), nvars - 1)
+
+    def set_preprocessor(self, preproc):
+        if preproc is None:
+            self.preprocessors = None
+        else:
+            self.preprocessors = (preproc,)
+    
+    def set_learner(self, learner):
+        self.learner = learner
+
+    def handleNewSignals(self):
+        self.apply()
+
+    def plot_scatter_points(self, x_data, y_data):
+        if self.scatterplot_item:
+            self.plotview.removeItem(self.scatterplot_item)
+        self.n_points = len(x_data)
+        self.scatterplot_item = pg.ScatterPlotItem(
+            x=x_data, y=y_data, data=np.arange(self.n_points),
+        )
+        self.plotview.addItem(self.scatterplot_item)
+        self.plotview.replot()
+    
+    def set_range(self, x_data, y_data):
+        min_x, max_x = np.nanmin(x_data), np.nanmax(x_data)
+        min_y, max_y = np.nanmin(y_data), np.nanmax(y_data)
+        self.plotview.setRange(
+            QRectF(min_x, min_y, max_x - min_x, max_y - min_y),
+            padding=0.025)
+        self.plotview.replot()
+    
+    def plot_regression_line(self, x_data, y_data):
+        if self.plot_item:
+            self.plotview.removeItem(self.plot_item)
+        self.plot_item = pg.PlotCurveItem(
+            x=x_data, y=y_data, 
+            pen=pg.mkPen(QColor(255, 0, 0), width=2), 
+            antialias=True
+        )
+        self.plotview.addItem(self.plot_item)
+        self.plotview.replot()
+    
+    def apply(self):
+        learner = self.learner
+        predictor = None
+
+        if self.data is not None:
+            if self.learner is None:
+                learner = LinearRegressionLearner(preprocessors=self.preprocessors)
+
+            attributes = self.x_var_model[self.x_var_index]
+            class_var = self.y_var_model[self.y_var_index]
+            data_table = Table(Domain([attributes], class_vars=[class_var]), self.data)
+
+            degree = int(self.polynomialexpansion)
+
+            learner = PolynomialLearner(learner, degree=degree)
+            learner.name = self.learner_name
+            predictor = learner(data_table)
+
+            x = data_table.X.ravel()
+            y = data_table.Y.ravel()
+            linspace = np.linspace(min(x), max(x), 1000).reshape(-1,1)
+            values = predictor(linspace, predictor.Value)
+
+            self.plot_scatter_points(x, y)
+
+            self.plot_regression_line(linspace.ravel(), values.ravel())
+
+            x_label = self.x_var_model[self.x_var_index]
+            axis = self.plot.getAxis("bottom")
+            axis.setLabel(x_label)
+            
+            y_label = self.y_var_model[self.y_var_index]
+            axis = self.plot.getAxis("left")
+            axis.setLabel(y_label)
+
+            self.set_range(x, y)
+
+        self.send("Learner", learner)
+        self.send("Predictor", predictor)
+
+
+if __name__ == "__main__":
+    import sys
+    from PyQt4.QtGui import QApplication
+
+    a = QApplication(sys.argv)
+    ow = OWUnivariateRegression()
+    learner = RidgeRegressionLearner(alpha=1.0)
+    polylearner = PolynomialLearner(learner, degree=2)
+    d = Table('iris')
+    ow.set_data(d)
+    ow.set_learner(learner)
+    ow.show()
+    a.exec_()
+    ow.saveSettings()
+    
+    
