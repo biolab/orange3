ba9e566f2b268359b86accf2531435561f17abd3
astaric <anze.staric@gmail.com>
2015-08-21 10:36:32 +0200

    travis: Use rsync to upload docs

--- travis/upload_doc.sh
+++ travis/upload_doc.sh
@@ -4,4 +4,5 @@ cd $TRAVIS_BUILD_DIR
 #scp -i travis/key.private -P 5722 -r doc/build/html/ travis@butler.fri.uni-lj.si:/home/travis/html
 ln -s `pwd`/doc/build/html doc/build/orange3doc
 echo -e "Host orange.biolab.si\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-echo "put -r doc/build/orange3doc/ /" | sftp -i travis/key.private download@orange.biolab.si
+echo -e "\tUser uploaddocs\n\tIdentityFile $TRAVIS_BUILD_DIR/travis/key.private\n" >> ~/.ssh/config
+rsync -a --delete doc/build/orange3doc/ orange.biolab.si:/orange3doc/
