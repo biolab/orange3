4a674b7c99982a1a5b963733ceb54bdc8c80af9e
astaric <anze.staric@gmail.com>
2015-09-08 17:39:53 +0200

    settings: Simplify ClassValuesContextHandler
    Pull methods that can be used in multiple context handlers to
    ContextHandler base class.

--- Orange/widgets/settings.py
+++ Orange/widgets/settings.py
@@ -683,17 +683,30 @@ class ContextHandler(SettingsHandler):
         self.settings_from_widget(widget)
         widget.current_context = None
 
-    # TODO this method has misleading name (method 'initialize' does what
-    #      this method's name would indicate.
     def settings_to_widget(self, widget):
-        """Restore settings from current context to the given widget instance"""
+        """Apply context settings stored in currently opened context
+        to the widget.
+        """
         widget.retrieveSpecificSettings()
 
-    # TODO similar to settings_to_widget; update_class_defaults does this for
-    #      context independent settings
+        context = widget.current_context
+        if context is None:
+            return
+
+        for setting, data, instance in \
+                self.provider.traverse_settings(data=context.values, instance=widget):
+            if not isinstance(setting, ContextSetting) or setting.name not in data:
+                continue
+
+            value = self.decode_setting(setting, data[setting.name])
+            setattr(instance, setting.name, value)
+            if hasattr(setting, "selected") and setting.selected in data:
+                setattr(instance, setting.selected, data[setting.selected])
+
     def settings_from_widget(self, widget):
-        """Store values of context settings from widget
-        to the current context"""
+        """Update the current context with the setting values from the widget.
+        """
+
         context = widget.current_context
         if context is None:
             return
@@ -708,6 +721,30 @@ class ContextHandler(SettingsHandler):
                     yield setting.selected, list(getattr(instance, setting.selected))
 
         context.values = self.provider.pack(widget, packer=packer)
+
+    def fast_save(self, widget, name, value):
+        """Update value of `name` setting in the current context to `value`
+        """
+
+        super().fast_save(widget, name, value)
+
+        context = widget.current_context
+        if context is None:
+            return
+
+        if name in self.known_settings:
+            setting = self.known_settings[name]
+            value = self.encode_setting(context, setting, value)
+            self.update_packed_data(context.values, name, value)
+
+    @staticmethod
+    def update_packed_data(data, name, value):
+        """Updates setting value stored in data dict"""
+
+        *prefixes, name = name.split('.')
+        for prefix in prefixes:
+            data = data.setdefault(prefix, {})
+        data[name] = value
 
     def encode_setting(self, context, setting, value):
         """Encode value to be stored in settings dict"""
@@ -841,9 +878,6 @@ class DomainContextHandler(ContextHandler):
                 continue
 
             value = self.decode_setting(setting, data[setting.name])
-            setattr(instance, setting.name, value)
-            if hasattr(setting, "selected") and setting.selected in data:
-                setattr(instance, setting.selected, data[setting.selected])
 
             if isinstance(value, list):
                 excluded |= set(value)
@@ -860,6 +894,8 @@ class DomainContextHandler(ContextHandler):
             setattr(widget, self.reservoir, ll)
 
     def fast_save(self, widget, name, value):
+        super().fast_save(widget, name, value)
+
         context = widget.current_context
         if not context:
             return
@@ -873,15 +909,6 @@ class DomainContextHandler(ContextHandler):
                 value = list(value)
 
             self.update_packed_data(context.values, name, value)
-
-    @staticmethod
-    def update_packed_data(data, name, value):
-        """Updates setting value stored in data dict"""
-
-        *prefixes, name = name.split('.')
-        for prefix in prefixes:
-            data = data.setdefault(prefix, {})
-        data[name] = value
 
     def encode_setting(self, context, setting, value):
         value = copy.copy(value)
@@ -1021,18 +1048,6 @@ class ClassValuesContextHandler(ContextHandler):
             return context.classes is None and 2
         else:
             return context.classes == classes and 2
-
-    def settings_to_widget(self, widget):
-        super().settings_to_widget(widget)
-        context = widget.current_context
-        self.provider.unpack(widget, context.values)
-
-    def fast_save(self, widget, name, value):
-        if widget.current_context is None:
-            return
-
-        if name in self.known_settings:
-            self.update_packed_data(widget.current_context.values, name, copy.copy(value))
 
 
 ### Requires the same the same attributes in the same order
