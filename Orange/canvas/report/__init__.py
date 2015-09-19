b26e563a6fb35d2bfd33281e8ae83a1604d56ab9
janezd <janez.demsar@fri.uni-lj.si>
2015-09-19 23:45:27 +0200

    report: Add/Move utility functions to Orange.canvas.report

--- Orange/canvas/report/__init__.py
+++ Orange/canvas/report/__init__.py
@@ -1,1 +1,28 @@
+def plural(s, number):
+    return s.format(number=number, s="s" if number % 100 != 1 else "")
+
+
+def plural_w(s, number, capitalize=False):
+    numbers = ("zero", "one", "two", "three", "four", "five", "six", "seven",
+               "nine", "ten")
+    number_str = numbers[number] if number < len(numbers) else str(number)
+    if capitalize:
+        number_str = number_str.capitalize()
+    return s.format(number=number_str, s="s" if number % 100 != 1 else "")
+
+
+def clip_string(s, limit=1000, sep=None):
+    if len(s) < limit:
+        return s
+    s = s[:limit - 3]
+    if sep is None:
+        return s
+    sep_pos = s.rfind(sep)
+    if sep_pos == -1:
+        return s
+    return s[:sep_pos + len(sep)] + "..."
+
+
+def clipped_list(s, limit=1000):
+    return clip_string(", ".join(s), limit, ", ")
 
