a66e2957fdd0ca0980004dcc74c2af01b217c479
Vesna Tanko <tankovesna@hotmail.com>
2015-09-21 15:33:28 +0200

    OWReport: Load scheme

--- Orange/canvas/scheme/readwrite.py
+++ Orange/canvas/scheme/readwrite.py
@@ -6,7 +6,8 @@ import base64
 import sys
 import warnings
 
-from xml.etree.ElementTree import TreeBuilder, Element, ElementTree, parse
+from xml.etree.ElementTree import (TreeBuilder, Element, ElementTree, parse,
+                                   fromstring)
 
 from collections import defaultdict, namedtuple
 from itertools import chain, count
@@ -565,7 +566,10 @@ def parse_ows_etree_v_1_0(tree):
 
 
 def parse_ows_stream(stream):
-    doc = parse(stream)
+    if isinstance(stream, str):
+        doc = ElementTree(fromstring(stream))
+    else:
+        doc = parse(stream)
     scheme_el = doc.getroot()
     version = scheme_el.get("version", None)
     if version is None:
