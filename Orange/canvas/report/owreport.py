2dcfbc69466198929f81fcdb4c5ca06ace7b1d2e
Vesna Tanko <tankovesna@hotmail.com>
2015-09-24 11:24:26 +0200

    OWReport: replace list with table

--- Orange/canvas/report/owreport.py
+++ Orange/canvas/report/owreport.py
@@ -1,12 +1,51 @@
 import os
 import pkg_resources
-from PyQt4.QtCore import Qt
-from PyQt4.QtGui import (QApplication, QDialog, QPrinter, QIcon,
-                         QPrintDialog, QFileDialog, QMenu)
+from PyQt4.QtCore import Qt, pyqtSlot
+from PyQt4.QtGui import (QApplication, QDialog, QPrinter, QIcon, QFont,
+                         QPrintDialog, QFileDialog, QTableView, QCursor,
+                         QStandardItemModel, QStandardItem, QHeaderView)
 from Orange.widgets import gui
 from Orange.widgets.widget import OWWidget
 from Orange.widgets.settings import Setting
 from Orange.canvas.application.canvasmain import CanvasMainWindow
+
+
+class ReportItem(QStandardItem):
+    def __init__(self, icon, text, html, scheme):
+        super().__init__(icon, text)
+        self.id = id(icon)
+        self.html = html
+        self.scheme = scheme
+        font = QFont()
+        font.setPointSize(12)
+        self.setFont(font)
+
+
+class ReportItemModel(QStandardItemModel):
+    def __init__(self, rows, columns, parent=None):
+        super().__init__(rows, columns, parent)
+
+    def add_item(self, item):
+        row = self.rowCount()
+        self.setItem(row, 0, item)
+        self.setItem(row, 1, self._icon_item("Remove", "icons/delete.png"))
+        self.setItem(row, 2, self._icon_item("Scheme", "icons/scheme.png"))
+
+    def get_item_by_id(self, item_id):
+        for i in range(self.rowCount()):
+            item = self.item(i)
+            if str(item.id) == item_id:
+                return item
+        return None
+
+    @staticmethod
+    def _icon_item(tooltip, path):
+        path = pkg_resources.resource_filename(__name__, path)
+        item = QStandardItem()
+        item.setIcon(QIcon(path))
+        item.setEditable(False)
+        item.setToolTip(tooltip)
+        return item
 
 
 class OWReport(OWWidget):
@@ -15,114 +54,112 @@ class OWReport(OWWidget):
 
     def __init__(self):
         super().__init__()
-        # TODO - ko kliknes na webview, oznaci item
-        self.widget_list_items = []
-        self.widget_list = gui.listBox(
-            self.controlArea, self,
-            labels="widget_list_items", callback=self._reload,
-            enableDragDrop=True, dragDropCallback=self._reload
-        )
-        self.widget_list.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.widget_list.customContextMenuRequested.connect(self._show_menu)
+        self.table_model = ReportItemModel(0, 3)
+        self.table = QTableView(self.controlArea)
+        self.table.setModel(self.table_model)
+        self.table.setShowGrid(False)
+        self.table.setSelectionBehavior(QTableView.SelectRows)
+        self.table.setSelectionMode(QTableView.SingleSelection)
+        self.table.setWordWrap(False)
+        self.table.setMouseTracking(True)
+        self.table.verticalHeader().setResizeMode(QHeaderView.Fixed)
+        self.table.verticalHeader().setDefaultSectionSize(20)
+        self.table.verticalHeader().setVisible(False)
+        self.table.horizontalHeader().setVisible(False)
+        self.table.setFixedWidth(252)
+        self.table.setColumnWidth(0, 200)
+        self.table.setColumnWidth(1, 25)
+        self.table.setColumnWidth(2, 25)
+        self.table.clicked.connect(self._table_clicked)
+        self.table.entered.connect(self._table_entered)
+        self.controlArea.layout().addWidget(self.table)
 
         self.save_button = gui.button(
-            self.controlArea, self, "Save",
-            callback=self._save_report, default=True
+            self.controlArea, self, "Save", callback=self._save_report
         )
         self.print_button = gui.button(
             self.controlArea, self, "Print", callback=self._print_report
         )
 
-        self.widget_list_items_schemas = {}
-
-        self.report_view_items = {}
-        self.report_view = gui.WebviewWidget(self.mainArea)
-        frame = self.report_view.page().mainFrame()
-        frame.setScrollBarPolicy(Qt.Vertical, Qt.ScrollBarAsNeeded)
-        self.javascript = frame.evaluateJavaScript
-
+        self.report_view = gui.WebviewWidget(self.mainArea, bridge=self)
         index_file = pkg_resources.resource_filename(__name__, "index.html")
         self.report_html_template = open(index_file, "r").read()
 
-    def _reload(self):
-        self._build_html()
+    def _table_clicked(self, index):
+        if index.column() == 0:
+            item = self.table_model.item(index.row())
+            self._scroll_to_item(item)
+            self._change_selected_item(item)
+        if index.column() == 1:
+            self._remove_item(index.row())
+        if index.column() == 2:
+            self._show_scheme(index.row())
 
-    def _show_menu(self, pos):
-        widget_list_menu = QMenu(self)
-        widget_list_menu.addAction("Show scheme", self._show_scheme)
-        widget_list_menu.addAction("Remove", self._remove_widget_item)
-        widget_list_menu.addAction("Clear", self._clear)
-        widget_list_menu.popup(self.mapToGlobal(pos))
+    def _table_entered(self, index):
+        if index.column() in (1, 2):
+            self.table.setCursor(QCursor(Qt.PointingHandCursor))
+        else:
+            self.table.setCursor(QCursor(Qt.ArrowCursor))
 
-    def _show_scheme(self):
-        selected_row = self.widget_list.currentRow()
-        if selected_row >= 0:
-            selected_item = self.widget_list_items[selected_row]
-            scheme = self.widget_list_items_schemas[selected_item]
-            canvas = self.get_canvas_instance()
-            if canvas:
-                canvas.load_scheme_xml(scheme)
+    def _show_scheme(self, row):
+        scheme = self.table_model.item(row).scheme
+        canvas = self.get_canvas_instance()
+        if canvas:
+            canvas.load_scheme_xml(scheme)
 
     def _get_scheme(self):
         canvas = self.get_canvas_instance()
         return canvas.get_scheme_xml() if canvas else None
 
-    def _clear(self):
-        self.widget_list_items = []
-        self.report_view_items = {}
+    def _remove_item(self, row):
+        self.table_model.removeRow(row)
         self._build_html()
+        indexes = self.table.selectionModel().selectedIndexes()
+        if indexes:
+            item = self.table_model.item(indexes[0].row())
+            self._scroll_to_item(item)
+            self._change_selected_item(item)
 
-    def _remove_widget_item(self):
-        selected_row = self.widget_list.currentRow()
-        if selected_row >= 0:
-            items = self.widget_list_items
-            selected_item = items.pop(selected_row)
-            self.widget_list_items = items
-            del self.report_view_items[selected_item]
-            if selected_row < len(self.report_view_items):
-                self.widget_list.setCurrentRow(selected_row)
-            self._build_html()
-
-    def _add_widget_item(self, widget):
-        items = self.widget_list_items
+    def _add_item(self, widget, name=None):
         path = pkg_resources.resource_filename(widget.__module__, widget.icon)
-        icon = QIcon(path)
-        items.append((widget.name, icon))
-        self.report_view_items[(widget.name, icon)] = widget.report_html
-        self.widget_list_items = items
-        self.widget_list_items_schemas[(widget.name,
-                                        icon)] = self._get_scheme()
+        item = ReportItem(QIcon(path), name if name else widget.name,
+                          widget.report_html, self._get_scheme())
+        self.table_model.add_item(item)
+        return item
 
     def _build_html(self):
-        n_widgets = len(self.widget_list_items)
-        if not n_widgets:
-            self.report_view.setHtml("")
-            return
-
-        selected_row = self.widget_list.currentRow()
-        if selected_row < 0 and n_widgets:
-            selected_row = n_widgets - 1
-            self.widget_list.setCurrentRow(selected_row)
-
         html = self.report_html_template
         html += "<body>"
-        for i, (item_name, item_icon) in enumerate(self.widget_list_items):
-            html += "<div id='%s' class='%s'>%s</div>" % (
-                id(item_icon),
-                "selected" if i == selected_row else "normal",
-                self.report_view_items[(item_name, item_icon)]
-            )
+        for i in range(self.table_model.rowCount()):
+            item = self.table_model.item(i)
+            html += "<div id='%s' class='normal' " \
+                    "onClick='pybridge._select_item(this.id)'>%s</div>" % \
+                    (item.id, item.html)
         html += "</body></html>"
         self.report_view.setHtml(html)
 
-        if selected_row < len(self.widget_list_items):
-            self.javascript(
-                "document.getElementById('%s').scrollIntoView();"
-                % id(self.widget_list_items[selected_row][1]))
+    def _scroll_to_item(self, item):
+        self.report_view.evalJS("document.getElementById('%s')."
+                                "scrollIntoView();" % item.id)
 
-    def make_report(self, widget):
-        self._add_widget_item(widget)
+    def _change_selected_item(self, item):
+        self.report_view.evalJS("document.getElementsByClassName('selected')"
+                                "[0].className = 'normal';")
+        self.report_view.evalJS("document.getElementById('%s')."
+                                "className = 'selected';" % item.id)
+
+    @pyqtSlot(str)
+    def _select_item(self, item_id):
+        item = self.table_model.get_item_by_id(item_id)
+        self.table.selectRow(self.table_model.indexFromItem(item).row())
+        self._change_selected_item(item)
+
+    def make_report(self, widget, name=None):
+        item = self._add_item(widget, name)
         self._build_html()
+        self._scroll_to_item(item)
+        self._change_selected_item(item)
+        self.table.selectRow(self.table_model.rowCount() - 1)
 
     def _save_report(self):
         filename = QFileDialog.getSaveFileName(self, "Save Report",
@@ -196,9 +233,6 @@ if __name__ == "__main__":
     disc.set_data(zoo)
     disc.create_report_html()
     main.make_report(disc)
-    file = OWFile()
-    file.create_report_html()
-    main.make_report(file)
 
     rf = OWRandomForest()
     rf.set_data(iris)
@@ -207,6 +241,6 @@ if __name__ == "__main__":
 
     main.show()
     main.saveSettings()
-    assert len(main.widget_list_items) == 5
+    assert main.table_model.rowCount() == 4
 
     sys.exit(app.exec_())
