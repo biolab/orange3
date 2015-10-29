881149548fc549764b1518a231939e9eaf38ab4b
Lan Zagar <lan.zagar@fri.uni-lj.si>
2015-10-29 11:59:42 +0100

    owscattermap: Remove 0.1s sampling option

--- Orange/widgets/visualize/owscattermap.py
+++ Orange/widgets/visualize/owscattermap.py
@@ -470,8 +470,8 @@ class OWScatterMap(widget.OWWidget):
 
     sample_percentages = []
     sample_percentages_captions = []
-    sample_times = [0.1, 0.5, 3, 5, 20, 40, 80]
-    sample_times_captions = ['0.1s', '1s', '5s', '10s', '30s', '1min', '2min']
+    sample_times = [0.5, 3, 5, 20, 40, 80]
+    sample_times_captions = ['1s', '5s', '10s', '30s', '1min', '2min']
 
     use_cache = settings.Setting(True)
 
