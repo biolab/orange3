5383bf9f8819a172bbd4c0db284d7796af57a1dd
nikicc <niko.colneric@gmail.com>
2015-11-20 11:31:58 +0100

    distances: autocommit button, normalization -1,1, typos

--- Orange/widgets/unsupervised/owdistancetransformation.py
+++ Orange/widgets/unsupervised/owdistancetransformation.py
@@ -14,21 +14,24 @@ class OWDistanceTransformation(widget.OWWidget):
     outputs = [("Distances", DistMatrix)]
 
     want_main_area = False
+    resizing_enabled = False
 
     normalization_method = settings.Setting(0)
     inversion_method = settings.Setting(0)
+    autocommit = settings.Setting(False)
 
     normalization_options = (
         ("No normalization", lambda x: x),
         ("To interval [0, 1]", lambda x: scale(x, min=0, max=1)),
-        ("Sigmoid function: 1/(1+exp(-x))", lambda x: 1/(1+np.exp(-x))),
+        ("To interval [-1, 1]", lambda x: scale(x, min=-1, max=1)),
+        ("Sigmoid function: 1/(1+exp(-X))", lambda x: 1/(1+np.exp(-x))),
     )
 
     inversion_options = (
         ("No inversion", lambda x: x),
         ("-X", lambda x: -x),
         ("1 - X", lambda x: 1-x),
-        ("Max - X", lambda x: np.max(x) - x),
+        ("max(X) - X", lambda x: np.max(x) - x),
         ("1/X", lambda x: 1/x),
     )
 
@@ -40,16 +43,19 @@ class OWDistanceTransformation(widget.OWWidget):
         box = gui.widgetBox(self.controlArea, "Normalization")
         gui.radioButtons(box, self, "normalization_method",
                          btnLabels=[x[0] for x in self.normalization_options],
-                         callback=self.commit)
+                         callback=self._invalidate)
 
         box = gui.widgetBox(self.controlArea, "Inversion")
         gui.radioButtons(box, self, "inversion_method",
                          btnLabels=[x[0] for x in self.inversion_options],
-                         callback=self.commit)
+                         callback=self._invalidate)
+
+        gui.auto_commit(self.controlArea, self, "autocommit", "Apply",
+                        checkbox_label="Apply on any change")
 
     def set_data(self, data):
         self.data = data
-        self.commit()
+        self.unconditional_commit()
 
     def commit(self):
         distances = self.data
@@ -62,3 +68,6 @@ class OWDistanceTransformation(widget.OWWidget):
             inv = self.inversion_options[self.inversion_method][1]
             distances = inv(distances)
         self.send("Distances", distances)
+
+    def _invalidate(self):
+        self.commit()
