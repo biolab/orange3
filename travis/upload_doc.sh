f14dbefa2b8f0136d1f23649963bc4b0f2a9bc6d
Marko Toplak <marko.toplak@gmail.com>
2015-11-27 14:59:06 +0100

    Update documentation build scripts on travis.

--- travis/upload_doc.sh
+++ travis/upload_doc.sh
@@ -2,7 +2,10 @@ cd $TRAVIS_BUILD_DIR
 
 #echo -e "Host butler.fri.uni-lj.si\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 #scp -i travis/key.private -P 5722 -r doc/build/html/ travis@butler.fri.uni-lj.si:/home/travis/html
-ln -s `pwd`/doc/build/html doc/build/orange3doc
+mkdir doc/build/orange3doc
+ln -s `pwd`/doc/data-mining-library/build/html doc/build/orange3doc/data-mining-library
+ln -s `pwd`/doc/development/build/html doc/build/orange3doc/development
+ln -s `pwd`/doc/visual-programming/build/html doc/build/orange3doc/visual-programming
 echo -e "Host orange.biolab.si\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 echo -e "\tUser uploaddocs\n\tIdentityFile $TRAVIS_BUILD_DIR/travis/key.private\n" >> ~/.ssh/config
 rsync -a --delete doc/build/orange3doc/ orange.biolab.si:/orange3doc/
