af0d680865eece3e2127c8b634e02e8fc5d0a81e
Marko Toplak <marko.toplak@gmail.com>
2015-11-27 14:49:25 +0100

    Moved development documentation.

--- doc/development/make.bat
+++ doc/development/make.bat
@@ -127,9 +127,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\scripting-tutorial.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\OrangeDevelopment.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\scripting-tutorial.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\OrangeDevelopment.ghc
 	goto end
 )
 
---
+++ doc/development/source/index.rst
@@ -0,0 +1,13 @@
+Widget Development
+==================
+
+.. toctree::
+    :maxdepth: 2
+
+    tutorial
+    tutorial-cont
+    tutorial-settings
+    tutorial-channels
+    tutorial-progressbar
+    widget
+    gui
