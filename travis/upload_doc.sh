c7a54009c598bd761210c9ebb367fade958c61f1
Marko Toplak <marko.toplak@gmail.com>
2015-11-27 15:35:38 +0100

    documentation upload: upload symlinked folders too

--- travis/upload_doc.sh
+++ travis/upload_doc.sh
@@ -8,4 +8,4 @@ ln -s `pwd`/doc/development/build/html doc/orange3doc/development
 ln -s `pwd`/doc/visual-programming/build/html doc/orange3doc/visual-programming
 echo -e "Host orange.biolab.si\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 echo -e "\tUser uploaddocs\n\tIdentityFile $TRAVIS_BUILD_DIR/travis/key.private\n" >> ~/.ssh/config
-rsync -a --delete doc/orange3doc/ orange.biolab.si:/orange3doc/
+rsync -a --delete -K doc/orange3doc/ orange.biolab.si:/orange3doc/
