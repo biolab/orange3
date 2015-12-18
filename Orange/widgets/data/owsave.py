fda92662412758456da810115cc7fa8c160690c0
Tomaz Curk <tomaz.curk@fri.uni-lj.si>
2015-12-18 13:44:19 +0100

    OWSave: display name of file

--- Orange/widgets/data/owsave.py
+++ Orange/widgets/data/owsave.py
@@ -22,6 +22,7 @@ class OWSave(widget.OWWidget):
     resizing_enabled = False
 
     last_dir = Setting("")
+    auto_save = Setting(True)
 
     def __init__(self, data=None, file_formats=None):
         super().__init__()
@@ -32,14 +33,21 @@ class OWSave(widget.OWWidget):
         self.formats = [(f.DESCRIPTION, f.EXTENSIONS)
                         for f in sorted(set(self.file_formats.values()),
                                         key=lambda f: f.OWSAVE_PRIORITY)]
+
+        self.info = gui.widgetLabel(self.controlArea, 'Output filename not '
+                                                      'specified.')
         self.comboBoxFormat = gui.comboBox(
             self.controlArea, self, value='format_index',
             items=['{} (*{})'.format(x[0], ' *'.join(x[1]))
                    for x in self.formats],
             box='File Format')
+
         box = gui.widgetBox(self.controlArea)
-        self.save = gui.button(box, self, "Save", callback=self.save_file,
-                               default=True, disabled=True)
+        self.save = gui.auto_commit(self.controlArea, self, "auto_save",
+                                    "Save",
+                                    commit=self.save_file)
+#        self.save = gui.button(box, self, "Save", callback=self.save_file,
+#                               default=True, disabled=True)
         gui.separator(box)
         self.saveAs = gui.button(box, self, "Save as ...",
                                  callback=self.save_file_as, disabled=True)
@@ -52,6 +60,7 @@ class OWSave(widget.OWWidget):
         self.data = data
         self.save.setDisabled(data is None)
         self.saveAs.setDisabled(data is None)
+        self.save_file()
 
     def save_file_as(self):
         format_name, format_extensions = self.formats[self.format_index]
@@ -69,7 +78,8 @@ class OWSave(widget.OWWidget):
             filename += format_extensions[0]
         self.filename = filename
         self.last_dir, file_name = os.path.split(self.filename)
-        self.save.setText("Save as '%s'" % file_name)
+        self.info.setText("Save as '%s'" %  file_name)
+        self.save.button.setText("Save as '%s'" % file_name)
         self.save.setDisabled(False)
         self.save_file()
 
@@ -85,7 +95,6 @@ class OWSave(widget.OWWidget):
                 self.error()
             except Exception as errValue:
                 self.error(str(errValue))
-
 
 if __name__ == "__main__":
     import sys
