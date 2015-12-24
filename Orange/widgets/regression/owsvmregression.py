53632961989d58d69e362bcd9d18b2390527c059
janezd <janez.demsar@fri.uni-lj.si>
2015-12-24 10:09:40 +0100

    SVM classification and regression: Improve widget layout

--- Orange/widgets/regression/owsvmregression.py
+++ Orange/widgets/regression/owsvmregression.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 from collections import OrderedDict
 
 from PyQt4 import QtGui
-from PyQt4.QtGui import QGridLayout, QLabel
+from PyQt4.QtGui import QLabel
 from PyQt4.QtCore import Qt
 
 from Orange.data import Table
@@ -10,9 +9,10 @@ from Orange.regression import SVRLearner, NuSVRLearner, SklModel
 from Orange.widgets import widget, settings, gui
 from Orange.widgets.utils.owlearnerwidget import OWProvidesLearner
 from Orange.widgets.utils.sql import check_sql_input
+from Orange.widgets.classify.owsvmclassification import SVMBaseMixin
 
 
-class OWSVMRegression(OWProvidesLearner, widget.OWWidget):
+class OWSVMRegression(SVMBaseMixin, widget.OWWidget):
     name = "SVM Regression"
     description = "Support vector machine regression algorithm."
     icon = "icons/SVMRegression.svg"
@@ -39,112 +39,46 @@ class OWSVMRegression(OWProvidesLearner, widget.OWWidget):
     #: Nu pareter for Nu SVR
     nu = settings.Setting(0.5)
 
-    #: Kernel types
-    Linear, Poly, RBF, Sigmoid = 0, 1, 2, 3
-    #: Selected kernel type
-    kernel_type = settings.Setting(RBF)
-    #: kernel degree
-    degree = settings.Setting(3)
-    #: gamma
-    gamma = settings.Setting(0.0)
-    #: coef0 (adative constant)
-    coef0 = settings.Setting(0.0)
-
-    #: numerical tolerance
-    tol = settings.Setting(0.001)
-
-    want_main_area = False
-    resizing_enabled = False
-
     def __init__(self):
         super().__init__()
-
         self.data = None
         self.preprocessors = None
 
-        box = gui.widgetBox(self.controlArea, self.tr("Name"))
-        gui.lineEdit(box, self, "learner_name")
+        self._setup_layout()
+        self._on_kernel_changed()
+        self.apply()
 
-        form = QGridLayout()
-        typebox = gui.radioButtonsInBox(
-            self.controlArea, self, "svrtype", [],
-            box=self.tr("SVM Regression Type"),
-            orientation=form,
-        )
+    def _add_type_box(self):
+        form = QtGui.QGridLayout()
+        self.type_box = box = gui.radioButtonsInBox(
+                self.controlArea, self, "svrtype", [], box="SVR Type",
+                orientation=form)
 
-        eps_svr = gui.appendRadioButton(typebox, "ε-SVR", addToLayout=False)
-        form.addWidget(eps_svr, 0, 0, Qt.AlignLeft)
+        form.addWidget(gui.appendRadioButton(box, "ε-SVR", addToLayout=False),
+                       0, 0, Qt.AlignLeft)
+        form.addWidget(QtGui.QLabel("Cost (C)"),
+                       0, 1, Qt.AlignRight)
+        form.addWidget(gui.doubleSpin(box, self, "epsilon_C", 0.1, 512.0, 0.1,
+                                      decimals=2, addToLayout=False),
+                       0, 2)
+        form.addWidget(QLabel("Loss Epsilon (ε)"),
+                       1, 1, Qt.AlignRight)
+        form.addWidget(gui.doubleSpin(box, self, "epsilon", 0.1, 512.0, 0.1,
+                                      decimals=2, addToLayout=False),
+                       1, 2)
 
-        form.addWidget(QtGui.QLabel(self.tr("Cost (C)")), 0, 1, Qt.AlignRight)
-        c_spin = gui.doubleSpin(
-            typebox, self, "epsilon_C", 0.1, 512.0, 0.1,
-            decimals=2, addToLayout=False
-        )
-        form.addWidget(c_spin, 0, 2)
-
-        form.addWidget(QLabel("Loss Epsilon (ε)"), 1, 1, Qt.AlignRight)
-        eps_spin = gui.doubleSpin(
-            typebox, self, "epsilon",  0.1, 512.0, 0.1,
-            decimals=2, addToLayout=False
-        )
-        form.addWidget(eps_spin, 1, 2)
-
-        nu_svr = gui.appendRadioButton(typebox, "ν-SVR", addToLayout=False)
-        form.addWidget(nu_svr, 2, 0, Qt.AlignLeft)
-
-        form.addWidget(QLabel(self.tr("Cost (C)")), 2, 1, Qt.AlignRight)
-        c_spin = gui.doubleSpin(
-            typebox, self, "nu_C", 0.1, 512.0, 0.1,
-            decimals=2, addToLayout=False
-        )
-        form.addWidget(c_spin, 2, 2)
-
+        form.addWidget(gui.appendRadioButton(box, "ν-SVR", addToLayout=False),
+                       2, 0, Qt.AlignLeft)
+        form.addWidget(QLabel("Cost (C)"),
+                       2, 1, Qt.AlignRight)
+        form.addWidget(gui.doubleSpin(box, self, "nu_C", 0.1, 512.0, 0.1,
+                                      decimals=2, addToLayout=False),
+                       2, 2)
         form.addWidget(QLabel("Complexity bound (ν)"),
                        3, 1, Qt.AlignRight)
-        nu_spin = gui.doubleSpin(
-            typebox, self, "nu", 0.05, 1.0, 0.05,
-            decimals=2, addToLayout=False
-        )
-        form.addWidget(nu_spin, 3, 2)
-
-        # Kernel control
-        box = gui.widgetBox(self.controlArea, self.tr("Kernel"))
-        buttonbox = gui.radioButtonsInBox(
-            box, self, "kernel_type",
-            btnLabels=["Linear,   x∙y",
-                       "Polynomial,   (g x∙y + c)^d",
-                       "RBF,   exp(-g|x-y|²)",
-                       "Sigmoid,   tanh(g x∙y + c)"],
-            callback=self._on_kernel_changed
-        )
-        parambox = gui.widgetBox(box, orientation="horizontal")
-        gamma = gui.doubleSpin(
-            parambox, self, "gamma", 0.0, 10.0, 0.0001,
-            label=" g: ", orientation="horizontal",
-            alignment=Qt.AlignRight
-        )
-        coef0 = gui.doubleSpin(
-            parambox, self, "coef0", 0.0, 10.0, 0.0001,
-            label=" c: ", orientation="horizontal",
-            alignment=Qt.AlignRight
-        )
-        degree = gui.doubleSpin(
-            parambox, self, "degree", 0.0, 10.0, 0.5,
-            label=" d: ", orientation="horizontal",
-            alignment=Qt.AlignRight
-        )
-        self._kernel_params = [gamma, coef0, degree]
-
-        # Numerical tolerance control
-        box = gui.widgetBox(self.controlArea, "Numerical Tolerance")
-        gui.doubleSpin(box, self, "tol", 1e-7, 1e-3, 5e-7)
-
-        gui.button(self.controlArea, self, "&Apply",
-                   callback=self.apply, default=True)
-
-        self._on_kernel_changed()
-
-        self.apply()
+        form.addWidget(gui.doubleSpin(box, self, "nu", 0.05, 1.0, 0.05,
+                                      decimals=2, addToLayout=False),
+                       3, 2)
 
     @check_sql_input
     def set_data(self, data):
@@ -186,16 +120,6 @@ class OWSVMRegression(OWProvidesLearner, widget.OWWidget):
         self.send("Predictor", predictor)
         self.send("Support vectors", sv)
 
-    def _on_kernel_changed(self):
-        enabled = [[False, False, False],  # linear
-                   [True, True, True],     # poly
-                   [True, False, False],   # rbf
-                   [True, True, False]]    # sigmoid
-
-        mask = enabled[self.kernel_type]
-        for spin, enabled in zip(self._kernel_params, mask):
-            spin.setEnabled(enabled)
-
     def send_report(self):
         self.report_items((("Name", self.learner_name),))
 
@@ -205,16 +129,7 @@ class OWSVMRegression(OWProvidesLearner, widget.OWWidget):
                 "ε-SVR, C={}, ε={}".format(self.epsilon_C, self.epsilon)
         else:
             items["SVM type"] = "ν-SVR, C={}, ν={}".format(self.nu_C, self.nu)
-        if self.kernel_type == 0:
-            items["Kernel"] = "Linear"
-        elif self.kernel_type == 1:
-            items["Kernel"] = "Polynomial, ({g:.4} x∙y + {c:.4})^{d}".format(
-                g=self.gamma, c=self.coef0, d=self.degree)
-        elif self.kernel_type == 2:
-            items["Kernel"] = "RBF, exp(-{:.4}|x-y|²)".format(self.gamma)
-        else:
-            items["Kernel"] = "Sigmoid, tanh({g:.4} x∙y + {c:.4})".format(
-                g=self.gamma, c=self.coef0)
+        self._report_kernel_parameters(items)
         items["Numerical tolerance"] = "{:.6}".format(self.tol)
         self.report_items("Model parameters", items)
 
