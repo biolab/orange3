5ea21de25ad6d20f373fac4d925c1b7c54a04996
Bogdan Vatulya <bogdan.vatulya@organicresponse.com.au>
2016-02-04 11:07:19 +1100

    reverted back __loader__ hack. Should remove it in a  separate PR

--- Orange/canvas/__init__.py
+++ Orange/canvas/__init__.py
@@ -3,9 +3,4 @@
 # default resource loader, but it python3 it does not. As a result,
 # pkg_resources is unable to select a resource loader and load resources.
 # By settings __loader__ to None, we workaround the pkg_resources bug.
-
-# A hack seems not to work for python 3.5 and orange-canvas fails to start (throws) when pkg_resources.__init__.py
-# calls resource_exists with orange.qss
-# when loader is commented out it all works
-# __loader__ = None
-
+__loader__ = None
