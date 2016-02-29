2e4c56ece52d2c0d823d6e2341d55f0c8863d2c1
Kernc <kerncece@gmail.com>
2016-02-29 18:28:46 +0100

    Move directory: travis -> .travis

--- .travis/upload_doc.sh
+++ .travis/upload_doc.sh
@@ -7,5 +7,5 @@ cp -r `pwd`/doc/data-mining-library/build/html doc/orange3doc/data-mining-librar
 cp -r `pwd`/doc/development/build/html doc/orange3doc/development
 cp -r `pwd`/doc/visual-programming/build/html doc/orange3doc/visual-programming
 echo -e "Host orange.biolab.si\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-echo -e "\tUser uploaddocs\n\tIdentityFile $TRAVIS_BUILD_DIR/travis/key.private\n" >> ~/.ssh/config
+echo -e "\tUser uploaddocs\n\tIdentityFile $TRAVIS_BUILD_DIR/.travis/key.private\n" >> ~/.ssh/config
 rsync -a --delete doc/orange3doc/ orange.biolab.si:/orange3doc/
