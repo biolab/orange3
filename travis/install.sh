4f510f1a42213ea6171c22b3aba066d253988c32
Kernc <kerncece@gmail.com>
2016-02-29 15:11:10 +0100

    travis: Drop wheelhouse; install/build recent packages from pip

--- travis/install.sh
+++ travis/install.sh
@@ -1,11 +1,4 @@
 pip install -U setuptools pip wheel
-if [ ! "$(ls wheelhouse)" ]; then
-    git clone --depth=1 https://github.com/astaric/orange3-requirements wheelhouse
-else
-    echo 'Using cached wheelhouse.';
-fi
-
-pip install wheelhouse/*.whl
 pip install -r requirements.txt
 
 python setup.py build_ext -i
