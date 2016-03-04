0e74015b7aaaa75f672b6932db15e4fd1455cc74
Kernc <kerncece@gmail.com>
2016-03-04 16:49:45 +0100

    travis: export and use ORANGE_DIR == sdist extracted dir

--- .travis/upload_doc.sh
+++ .travis/upload_doc.sh
@@ -1,9 +1,10 @@
+cd $ORANGE_DIR
+
 # Decrypt private key
 openssl aes-256-cbc -K $encrypted_3fc26dee5a84_key -iv $encrypted_3fc26dee5a84_iv -in .travis/upload_doc_id -out .travis/key.private -d
 chmod 700 .travis/key.private
 
 # Upload the docs
-cd $TRAVIS_BUILD_DIR
 mkdir doc/orange3doc
 cp -r doc/data-mining-library/build/html doc/orange3doc/data-mining-library
 cp -r doc/development/build/html doc/orange3doc/development
@@ -12,6 +13,6 @@ cp -r doc/visual-programming/build/html doc/orange3doc/visual-programming
 Host orange.biolab.si
     StrictHostKeyChecking no
     User uploaddocs
-    IdentityFile $TRAVIS_BUILD_DIR/.travis/key.private
+    IdentityFile $ORANGE_DIR/.travis/key.private
 "
 rsync -a --delete doc/orange3doc/ orange.biolab.si:/orange3doc/
