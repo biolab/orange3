4a3aac38816766a188abe4e3c6024f2a1fc69107
Kernc <kerncece@gmail.com>
2016-03-04 12:49:08 +0100

    travis: fix failures introduced by previous commit

--- .travis/decrypt_upload_doc_id.sh
+++
@@ -1,2 +0,0 @@
-openssl aes-256-cbc -K $encrypted_3fc26dee5a84_key -iv $encrypted_3fc26dee5a84_iv -in .travis/upload_doc_id -out .travis/key.private -d
-chmod 700 .travis/key.private
--- .travis/upload_doc.sh
+++ .travis/upload_doc.sh
@@ -1,11 +1,17 @@
-cd $TRAVIS_BUILD_DIR
+# Decrypt private key
+openssl aes-256-cbc -K $encrypted_3fc26dee5a84_key -iv $encrypted_3fc26dee5a84_iv -in .travis/upload_doc_id -out .travis/key.private -d
+chmod 700 .travis/key.private
 
-#echo -e "Host butler.fri.uni-lj.si\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-#scp -i travis/key.private -P 5722 -r doc/build/html/ travis@butler.fri.uni-lj.si:/home/travis/html
+# Upload the docs
+cd $TRAVIS_BUILD_DIR
 mkdir doc/orange3doc
-cp -r `pwd`/doc/data-mining-library/build/html doc/orange3doc/data-mining-library
-cp -r `pwd`/doc/development/build/html doc/orange3doc/development
-cp -r `pwd`/doc/visual-programming/build/html doc/orange3doc/visual-programming
-echo -e "Host orange.biolab.si\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-echo -e "\tUser uploaddocs\n\tIdentityFile $TRAVIS_BUILD_DIR/.travis/key.private\n" >> ~/.ssh/config
+cp -r doc/data-mining-library/build/html doc/orange3doc/data-mining-library
+cp -r doc/development/build/html doc/orange3doc/development
+cp -r doc/visual-programming/build/html doc/orange3doc/visual-programming
+> ~/.ssh/config echo "
+Host orange.biolab.si
+    StrictHostKeyChecking no
+    User uploaddocs
+    IdentityFile $TRAVIS_BUILD_DIR/.travis/key.private
+"
 rsync -a --delete doc/orange3doc/ orange.biolab.si:/orange3doc/
