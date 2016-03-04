e5dd16db720e4fcfea24ae450f3b62b73fcb99ba
rahulroxx <rahulyupz@gmail.com>
2016-03-03 23:35:14 -0800

    fix multiple excel selection

--- Orange/widgets/data/owfile.py
+++ Orange/widgets/data/owfile.py
@@ -345,12 +345,12 @@ class OWFile(widget.OWWidget):
             else:
                 start_file = os.path.expanduser("~/")
 
-        filename = QtGui.QFileDialog.getOpenFileName(
+        filename = QtGui.QFileDialog.getOpenFileNames(
             self, 'Open Orange Data File', start_file, self.dlg_formats)
         if not filename:
             return
-
-        self._add_path(filename)
+        for files in filename:
+            self._add_path(files)
         self.set_file_list()
         self.source = self.LOCAL_FILE
         self.load_data()
