biolab/orange3

commit 5854989266e407a30a2a833b7aa4bdfcae8d8dcf

TimeVariable: fix microsecond time overflow on some platforms
Might be related to http://bugs.python.org/issue14180
Either case, it didn't work, as-was, in Python 3.5.

Orange/data/variable.py
@@ -860,10 +860,13 @@ class TimeVariable(ContinuousVariable):
         return s[:-6] if s.endswith(('+00:00', '-00:00')) else _subtz(r'\1\2\3', s)
 
     def repr_val(self, val):
+        seconds = round(val)
+        microseconds = round((val - seconds) * 1e6)
         if val < 0:
-            date = str(datetime.fromtimestamp(0, tz=self.timezone) + timedelta(seconds=val))
+            date = datetime.fromtimestamp(0, tz=self.timezone) + timedelta(seconds=seconds)
         else:
-            date = str(datetime.fromtimestamp(val, tz=self.timezone))
+            date = datetime.fromtimestamp(seconds, tz=self.timezone)
+        date = str(date.replace(microsecond=microseconds))
         if self.have_date and not self.have_time:
             date = date.split()[0]
         elif not self.have_date and self.have_time:

Orange/tests/test_variable.py
@@ -249,8 +249,8 @@ class StringVariableTest(VariableTest):
 class TimeVariableTest(VariableTest):
     TESTS = [
         # in str, UTC timestamp, out str (in UTC)
-        ('2015-10-12 14:13:11.01+0200', 1444651991.01, '2015-10-12 14:13:11.009999+0200'),
-        ('2015-10-12T14:13:11.01+0200', 1444651991.01, '2015-10-12 14:13:11.009999+0200'),
+        ('2015-10-12 14:13:11.01+0200', 1444651991.01, '2015-10-12 14:13:11.010000+0200'),
+        ('2015-10-12T14:13:11.01+0200', 1444651991.01, '2015-10-12 14:13:11.010000+0200'),
         ('2015-10-12 14:13:11+0200', 1444651991, '2015-10-12 14:13:11+0200'),
         ('2015-10-12T14:13:11+0200', 1444651991, '2015-10-12 14:13:11+0200'),
         ('20151012T141311+0200',     1444651991, '2015-10-12 14:13:11+0200'),

commit 70d83af7e74a7bd07d4e39c99bbcd89a11030bef

TimeVariable: fix repr_val issue introduced in 5854989266e407a30a2a833b7aa4bdfcae8d8dcf

Orange/data/variable.py
@@ -860,8 +860,8 @@ class TimeVariable(ContinuousVariable):
         return s[:-6] if s.endswith(('+00:00', '-00:00')) else _subtz(r'\1\2\3', s)
 
     def repr_val(self, val):
-        seconds = round(val)
-        microseconds = round((val - seconds) * 1e6)
+        seconds = int(round(val))
+        microseconds = int(round((val - seconds) * 1e6))
         if val < 0:
             date = datetime.fromtimestamp(0, tz=self.timezone) + timedelta(seconds=seconds)
         else: