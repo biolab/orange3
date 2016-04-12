cbccfb78df6347527820cb30df68b5e7ab1b54a5
rekonder <andrej.jugovic7@gmail.com>
2016-04-12 16:41:56 +0200

    Cleaning in test_reprocess_pca.py

--- Orange/tests/test_preprocess_pca.py
+++ Orange/tests/test_preprocess_pca.py
@@ -8,22 +8,13 @@ from Orange.preprocess import ProjectPCA
 
 
 class TestPCAProjector(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.ionosphere = Table("ionosphere")
 
     def test_project_pca_default(self):
-        data = self.ionosphere
-        projector = ProjectPCA()
-        data_pc = projector(data)
-        self.assertEqual(data_pc.X.shape[1], data.X.shape[1])
-        self.assertTrue((data.metas == data_pc.metas).all())
-        self.assertTrue((data.Y == data_pc.Y).any())
-
-    def test_project_pca(self):
-        data = self.ionosphere
-        projector = ProjectPCA(n_components=5)
-        data_pc = projector(data)
-        self.assertEqual(data_pc.X.shape[1], 5)
-        self.assertTrue((data.metas == data_pc.metas).all())
-        self.assertTrue((data.Y == data_pc.Y).any())
+        data = Table("ionosphere")
+        for kwargs, value in (({}, data.X.shape[1]),
+                              ({'n_components': 5}, 5)):
+            projector = ProjectPCA(**kwargs)
+            data_pc = projector(data)
+            self.assertEqual(data_pc.X.shape[1], value)
+            self.assertTrue((data.metas == data_pc.metas).all())
+            self.assertTrue((data.Y == data_pc.Y).any())
