68fce9577ece65fe190c3dfbaa684ac2964e400f
rekonder <andrej.jugovic7@gmail.com>
2016-04-12 17:01:06 +0200

    Cleaning test_random_forest.py

--- Orange/tests/test_random_forest.py
+++ Orange/tests/test_random_forest.py
@@ -14,81 +14,63 @@ class RandomForestTest(unittest.TestCase):
     def setUpClass(cls):
         cls.iris = Table('iris')
         cls.house = Table('housing')
+        cls.forestClassification = RandomForestLearner()
+        cls.regressionForest = RandomForestRegressionLearner()
+        cls.modelClassification = cls.forestClassification(cls.iris)
+        cls.modelRegression = cls.regressionForest(cls.house)
 
     def test_RandomForest(self):
-        forest = RandomForestLearner()
-        results = CrossValidation(self.iris, [forest], k=10)
+        results = CrossValidation(self.iris, [self.forestClassification], k=10)
         ca = CA(results)
         self.assertGreater(ca, 0.9)
         self.assertLess(ca, 0.99)
 
     def test_predict_single_instance(self):
-        forest = RandomForestLearner()
-        c = forest(self.iris)
         for ins in self.iris:
-            c(ins)
-            val, prob = c(ins, c.ValueProbs)
+            self.modelClassification(ins)
+            _, _ = self.modelClassification(ins, self.modelClassification.ValueProbs)
 
-    def test_predict_table(self):
-        forest = RandomForestLearner()
-        c = forest(self.iris)
-        c(self.iris)
-        vals, probs = c(self.iris, c.ValueProbs)
-
-    def test_predict_numpy(self):
-        forest = RandomForestLearner()
-        c = forest(self.iris)
-        c(self.iris.X)
-        vals, probs = c(self.iris.X, c.ValueProbs)
-
-    def test_RandomForestRegression(self):
-        forest = RandomForestRegressionLearner()
-        results = CrossValidation(self.house, [forest], k=10)
-        _ = RMSE(results)
-
-    def test_predict_single_instance_reg(self):
-        forest = RandomForestRegressionLearner()
-        model = forest(self.house)
-        for ins in self.house:
-            pred = model(ins)
-            self.assertGreater(pred, 0)
-
-    def test_predict_table_reg(self):
-        forest = RandomForestRegressionLearner()
-        model = forest(self.house)
-        pred = model(self.house)
-        self.assertEqual(len(self.house), len(pred))
-        self.assertGreater(all(pred), 0)
-
-    def test_predict_numpy_reg(self):
-        forest = RandomForestRegressionLearner()
-        model = forest(self.house)
-        pred = model(self.house.X)
-        self.assertEqual(len(self.house), len(pred))
-        self.assertGreater(all(pred), 0)
+    def test_predict(self):
+        for data in (self.iris,  # table
+                     self.iris.X):  # numpy
+            self.modelClassification(data)
+            _, _ = self.modelClassification(data, self.modelClassification.ValueProbs)
 
     def test_classification_scorer(self):
-        learner = RandomForestLearner()
-        scores = learner.score_data(self.iris)
+        scores = self.forestClassification.score_data(self.iris)
         self.assertEqual(len(scores), len(self.iris.domain.attributes))
         self.assertNotEqual(sum(scores), 0)
         self.assertEqual(['petal length', 'petal width'],
                          sorted([self.iris.domain.attributes[i].name
                                  for i in np.argsort(scores)[-2:]]))
 
-    def test_regression_scorer(self):
-        learner = RandomForestRegressionLearner()
-        scores = learner.score_data(self.house)
-        self.assertEqual(['LSTAT', 'RM'],
-                         sorted([self.house.domain.attributes[i].name
-                                 for i in np.argsort(scores)[-2:]]))
-
     def test_scorer_feature(self):
         np.random.seed(42)
         data = Table('test4.tab')
-        learner = RandomForestLearner()
-        scores = learner.score_data(data)
+        scores = self.forestClassification.score_data(data)
         for i, attr in enumerate(data.domain.attributes):
             np.random.seed(42)
-            score = learner.score_data(data, attr)
+            score = self.forestClassification.score_data(data, attr)
             self.assertEqual(score, scores[i])
+
+    def test_RandomForestRegression(self):
+        results = CrossValidation(self.house, [self.regressionForest], k=10)
+        _ = RMSE(results)
+
+    def test_predict_single_instance_reg(self):
+        for ins in self.house:
+            pred = self.modelRegression(ins)
+            self.assertGreater(pred, 0)
+
+    def test_predict_reg(self):
+        for data in (self.house,  # table
+                     self.house.X):  # numpy
+            pred = self.modelRegression(data)
+            self.assertEqual(len(data), len(pred))
+            self.assertGreater(all(pred), 0)
+
+    def test_regression_scorer(self):
+        scores = self.regressionForest.score_data(self.house)
+        self.assertEqual(['LSTAT', 'RM'],
+                         sorted([self.house.domain.attributes[i].name
+                                 for i in np.argsort(scores)[-2:]]))
