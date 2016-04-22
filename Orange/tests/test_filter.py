1bb4ef0061464263574a65928b11b7970ede4353
mstrazar <martin.strazar@gmail.com>
2016-04-22 17:23:18 +0200

    Added SameValue filter tests with Mock and disc, cont, meta variable types.

--- Orange/tests/test_filter.py
+++ Orange/tests/test_filter.py
@@ -3,12 +3,14 @@
 
 import unittest
 from unittest.mock import MagicMock, patch
+import itertools
 
 import numpy as np
 
 from Orange.data import Table, Domain, ContinuousVariable
 from Orange.data.filter import \
-    FilterContinuous, FilterDiscrete, FilterString, Values, HasClass, IsDefined
+    FilterContinuous, FilterDiscrete, FilterString, Values, HasClass, \
+    IsDefined, SameValue
 
 NIMOCK = MagicMock(side_effect=NotImplementedError())
 
@@ -326,3 +328,69 @@ class TestFilterString(unittest.TestCase):
         self.inst["name"] = ""
         flt = FilterString("name", FilterString.IsDefined)
         self.assertFalse(flt(self.inst))
+
+
+class TestSameValueFilter(unittest.TestCase):
+    def setUp(self):
+        self.table = Table('zoo')
+
+        self.attr_disc  = self.table.domain["type"]
+        self.attr_cont  = self.table.domain["legs"]
+        self.attr_meta  = self.table.domain["name"]
+
+        self.value_cont = 4
+        self.value_disc = self.attr_disc.to_val("mammal")
+        self.value_meta = self.attr_meta.to_val("girl")
+
+    def test_same_value_filter_table(self):
+
+        test_pairs = ((self.attr_cont, 4, self.value_cont),
+                      (self.attr_disc, "mammal", self.value_disc),
+                      (self.attr_meta, "girl", self.value_meta),)
+
+        for var_index, value, num_value in test_pairs:
+            filter_ = SameValue(var_index, value)(self.table)
+            self.assertTrue(all(inst[var_index] == num_value for inst in filter_))
+
+            filter_inverse = SameValue(var_index, value, negate=True)(self.table)
+            self.assertTrue(all(inst[var_index] != num_value for inst in filter_inverse))
+
+            self.assertEqual(len(filter_) + len(filter_inverse), len(self.table))
+
+
+        for t1, t2 in itertools.combinations(test_pairs, 2):
+            pos1, val1, r1 = t1
+            pos2, val2, r2 = t2
+
+            filter_1 = SameValue(pos1, val1)(self.table)
+            filter_2 = SameValue(pos2, val2)(self.table)
+
+            filter_12 = SameValue(pos2, val2)(SameValue(pos1, val1)(self.table))
+            filter_21 = SameValue(pos1, val1)(SameValue(pos2, val2)(self.table))
+
+            self.assertEqual(len(filter_21), len(filter_12))
+
+            self.assertTrue(len(filter_1) >= len(filter_12))
+            self.assertTrue(len(filter_2) >= len(filter_12))
+
+            self.assertTrue(all(inst[pos1] == r1 and
+                                inst[pos2] == r2 and
+                                inst in filter_21
+                                for inst in filter_12))
+            self.assertTrue(all(inst[pos1] == r1 and
+                                inst[pos2] == r2 and
+                                inst in filter_12
+                                for inst in filter_21))
+
+    def test_same_value_filter_instance(self):
+        inst = self.table[0]
+
+        filter_ = SameValue(self.attr_disc, self.value_disc)(inst)
+        self.assertEqual(filter_, inst[self.attr_disc] == self.value_disc)
+
+        filter_n = SameValue(self.attr_disc, self.value_disc, negate=True)(inst)
+        self.assertEqual(filter_n, inst[self.attr_disc] != self.value_disc)
+
+    @patch('Orange.data.Table._filter_same_value', NIMOCK)
+    def test_has_class_filter_not_implemented(self):
+        self.test_same_value_filter_table()
