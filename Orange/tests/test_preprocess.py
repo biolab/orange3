f133356acfaa9064b348f91eb99e1e110a033d9b
rekonder <andrej.jugovic7@gmail.com>
2016-05-03 16:14:09 +0200

    Cleaning test_preprocess.py

--- Orange/tests/test_preprocess.py
+++ Orange/tests/test_preprocess.py
@@ -2,25 +2,25 @@
 # pylint: disable=missing-docstring
 
 import unittest
-from unittest.mock import Mock, MagicMock, patch
+from unittest.mock import Mock
 import numpy as np
 
-import Orange
 from Orange.data import Domain, Table, DiscreteVariable
-from Orange.preprocess import RemoveNaNClasses
+from Orange.preprocess import RemoveNaNClasses, preprocess
 
 
 class TestPreprocess(unittest.TestCase):
     def test_read_data_calls_reader(self):
-        class MockPreprocessor(Orange.preprocess.preprocess.Preprocess):
+        class MockPreprocessor(preprocess.Preprocess):
             __init__ = Mock(return_value=None)
             __call__ = Mock()
+
             @classmethod
             def reset(cls):
                 cls.__init__.reset_mock()
                 cls.__call__.reset_mock()
 
-        table = Mock(Orange.data.Table)
+        table = Mock(Table)
         MockPreprocessor(table, 1, 2, a=3)
         MockPreprocessor.__init__.assert_called_with(1, 2, a=3)
         MockPreprocessor.__call__.assert_called_with(table)
@@ -42,20 +42,20 @@ class TestPreprocess(unittest.TestCase):
 class RemoveConstant(unittest.TestCase):
     def test_remove_columns(self):
         X = np.random.rand(6, 4)
-        X[:, (1,3)] = 5
+        X[:, (1, 3)] = 5
         X[3, 1] = np.nan
         X[1, 1] = np.nan
-        data = Orange.data.Table(X)
-        d = Orange.preprocess.preprocess.RemoveConstant(data)
+        data = Table(X)
+        d = preprocess.RemoveConstant(data)
         self.assertEqual(len(d.domain.attributes), 2)
 
-        pp_rc = Orange.preprocess.preprocess.RemoveConstant()
+        pp_rc = preprocess.RemoveConstant()
         d = pp_rc(data)
         self.assertEqual(len(d.domain.attributes), 2)
 
     def test_nothing_to_remove(self):
-        data = Orange.data.Table("iris")
-        d = Orange.preprocess.preprocess.RemoveConstant(data)
+        data = Table("iris")
+        d = preprocess.RemoveConstant(data)
         self.assertEqual(len(d.domain.attributes), 4)
 
 
@@ -69,7 +69,7 @@ class TestRemoveNanClass(unittest.TestCase):
     def test_remove_nan_classes_multiclass(self):
         domain = Domain([DiscreteVariable("a", values="01")],
                         [DiscreteVariable("b", values="01"),
-                        DiscreteVariable("c", values="01")])
+                         DiscreteVariable("c", values="01")])
         table = Table(domain, [[0, 1, np.nan],
                                [1, np.nan, 0],
                                [1, 0, 1],
