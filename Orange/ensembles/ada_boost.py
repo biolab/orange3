842a22fe381a1e956f08f96ff9c702a96acf3165
s-alexey <suharevichalexey@gmail.com>
2016-05-05 12:31:31 +0300

    Update ensemble module

--- Orange/ensembles/ada_boost.py
+++ Orange/ensembles/ada_boost.py
@@ -4,6 +4,7 @@ from Orange.classification.base_classification import (SklLearnerClassification,
                                                        SklModelClassification)
 from Orange.regression.base_regression import (SklLearnerRegression,
                                                SklModelRegression)
+from Orange import options
 
 __all__ = ["SklAdaBoostLearner", "SklAdaBoostRegressionLearner"]
 
@@ -12,17 +13,30 @@ class SklAdaBoostClassifier(SklModelClassification):
     pass
 
 
+class BaseAdaBoostOptions:
+    base_estimator = options.ObjectOption('base_estimator')
+    options = [
+        options.IntegerOption('n_estimators', default=50, range=(1, 1000), step=10),
+        options.FloatOption('learning_rate', default=1., range=(0.05, 1.), step=.05),
+        options.IntegerOption('random_state', default=0, range=(0, 100)),
+    ]
+
+
 class SklAdaBoostLearner(SklLearnerClassification):
     __wraps__ = skl_ensemble.AdaBoostClassifier
     __returns__ = SklAdaBoostClassifier
     name = 'skl adaBoost'
+    verbose_name = 'Ada Boost'
 
-    def __init__(self, base_estimator=None, n_estimators=50, learning_rate=1.,
-                 algorithm='SAMME.R', random_state=None, preprocessors=None):
+    options = [
+        BaseAdaBoostOptions.base_estimator,
+        options.ChoiceOption('algorithm', choices=('SAMME', 'SAMME.R')),
+    ] + BaseAdaBoostOptions.options
+
+    def __init__(self, base_estimator=None, **kwargs):
         if isinstance(base_estimator, SklLearner):
-            base_estimator = base_estimator.__wraps__(**base_estimator.params)
-        super().__init__(preprocessors=preprocessors)
-        self.params = vars()
+            base_estimator = base_estimator.instance
+        super().__init__(base_estimator=base_estimator, **kwargs)
 
 
 class SklAdaBoostRegressor(SklModelRegression):
@@ -33,10 +47,14 @@ class SklAdaBoostRegressionLearner(SklLearnerRegression):
     __wraps__ = skl_ensemble.AdaBoostRegressor
     __returns__ = SklAdaBoostRegressor
     name = 'skl adaBoost regression'
+    verbose_name = 'Ada Boost Regression'
 
-    def __init__(self, base_estimator=None, n_estimators=50, learning_rate=1.,
-                 loss='linear', random_state=None, preprocessors=None):
+    options = [
+        BaseAdaBoostOptions.base_estimator,
+        options.ChoiceOption('loss', choices=('linear', 'square', 'exponential'))
+    ] + BaseAdaBoostOptions.options
+
+    def __init__(self, base_estimator=None, **kwargs):
         if isinstance(base_estimator, SklLearner):
-            base_estimator = base_estimator.__wraps__(**base_estimator.params)
-        super().__init__(preprocessors=preprocessors)
-        self.params = vars()
+            base_estimator = base_estimator.instance
+        super().__init__(base_estimator=base_estimator, **kwargs)
