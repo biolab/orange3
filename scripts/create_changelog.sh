ddab1ca7d34cd4a12b6af5a0f0b6bfb7a9a32535
astaric <anze.staric@gmail.com>
2016-06-24 14:21:53 +0200

    Scripts: Add create_changelog

---
+++ scripts/create_changelog.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION=$(grep '^VERSION' setup.py | sed -E "s/.*'([^']*)'/\1/")
+RELEASE_DATE=$(date "+%Y-%m-%d")
+
+echo "[$VERSION] - $RELEASE_DATE"
+echo "--------------------"
+echo '##### Enhancements'
+git log stable..master --first-parent --format='%s %b' |
+    sed -E 's/.*(#[0-9]+).*\[ENH\] *(.*)/\* \2 (\1)/' |
+    grep -E '^\*'
+echo
+echo "##### Bugfixes"
+git log stable..master --first-parent --format='%s %b' |
+    sed -E 's/.*(#[0-9]+).*\[FIX\] *(.*)/\* \2 (\1)/' |
+    grep -E '^\*'
