ba9165a667ad198b41b227abd6dcacf5e3c780e0
Kernc <kerncece@gmail.com>
2016-07-04 15:25:47 +0200

    Move Orange.util.scale() -> Orange.data.util.scale()

--- Orange/data/util.py
+++ Orange/data/util.py
@@ -2,6 +2,7 @@
 Data-manipulation utilities.
 """
 import numpy as np
+import bottleneck as bn
 
 
 def one_hot(values, dtype=float):
@@ -18,3 +19,12 @@ def one_hot(values, dtype=float):
         2d array with ones in respective indicator columns.
     """
     return np.eye(np.max(values) + 1, dtype=dtype)[np.asanyarray(values, dtype=int)]
+
+
+def scale(values, min=0, max=1):
+    """Return values scaled to [min, max]"""
+    minval = np.float_(bn.nanmin(values))
+    ptp = bn.nanmax(values) - minval
+    if ptp == 0:
+        return np.clip(values, min, max)
+    return (-minval + values) / ptp * (max - min) + min
--- Orange/tests/test_util.py
+++ Orange/tests/test_util.py
@@ -2,18 +2,13 @@ import unittest
 
 import numpy as np
 
-from Orange.util import scale, export_globals, flatten, deprecated, try_
+from Orange.util import export_globals, flatten, deprecated, try_
 
 
 SOMETHING = 0xf00babe
 
 
 class TestUtil(unittest.TestCase):
-    def test_scale(self):
-        self.assertTrue(np.all(scale([0, 1, 2], -1, 1) == [-1, 0, 1]))
-        self.assertTrue(np.all(scale([3, 3, 3]) == [1, 1, 1]))
-        self.assertTrue(not np.all(np.isnan(scale([.5, np.nan]))))
-
     def test_export_globals(self):
         self.assertEqual(sorted(export_globals(globals(), __name__)),
                          ['SOMETHING', 'TestUtil'])
