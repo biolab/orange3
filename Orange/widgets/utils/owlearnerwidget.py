78585cf9a4a46d3315330434d7e1c1dda5416ed0
Vesna Tanko <tankovesna@hotmail.com>
2016-07-19 14:06:48 +0200

    OWBaseLearner: Auto apply changed setting only when checked

--- Orange/widgets/utils/owlearnerwidget.py
+++ Orange/widgets/utils/owlearnerwidget.py
@@ -198,7 +198,8 @@ class OWBaseLearner(OWWidget, metaclass=OWBaseLearnerMeta):
         self.outdated_settings = True
         self.warning(self.OUTDATED_LEARNER_WARNING_ID,
                      None if self.auto_apply else "Press Apply to submit changes.")
-        self.apply()
+        if self.auto_apply:
+            self.apply()
 
     def send_report(self):
         self.report_items((("Name", self.learner_name),))
