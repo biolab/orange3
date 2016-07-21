e74cfe8cf23dbf98dd29f71cc8707d93e74c509b
Vesna Tanko <tankovesna@hotmail.com>
2016-07-21 15:10:41 +0200

    TestWidgetTree: Cls and reg tree tests upgrade

--- Orange/widgets/regression/tests/test_owregressiontree.py
+++ Orange/widgets/regression/tests/test_owregressiontree.py
@@ -1,7 +1,8 @@
 # Test methods with long descriptive names can omit docstrings
 # pylint: disable=missing-docstring
 from Orange.widgets.regression.owregressiontree import OWRegressionTree
-from Orange.widgets.tests.base import WidgetTest, WidgetLearnerTestMixin
+from Orange.widgets.tests.base import (WidgetTest, WidgetLearnerTestMixin,
+                                       GuiToParam)
 
 
 class TestOWRegressionTree(WidgetTest, WidgetLearnerTestMixin):
@@ -9,3 +10,39 @@ class TestOWRegressionTree(WidgetTest, WidgetLearnerTestMixin):
         self.widget = self.create_widget(OWRegressionTree,
                                          stored_settings={"auto_apply": False})
         self.init()
+
+        def combo_set_value(i, x):
+            x.activated.emit(i)
+            x.setCurrentIndex(i)
+
+        scores = [score[1] for score in self.widget.scores]
+        md_spin = self.widget.max_depth_spin[1]
+        mi_spin = self.widget.min_internal_spin[1]
+        ml_spin = self.widget.min_leaf_spin[1]
+        md_min_max = [md_spin.minimum(), md_spin.maximum()]
+        mi_min_max = [mi_spin.minimum(), mi_spin.maximum()]
+        ml_min_max = [ml_spin.minimum(), ml_spin.maximum()]
+        self.gui_to_params = [
+            GuiToParam('criterion', self.widget.score_combo,
+                       lambda x: scores[x.currentIndex()],
+                       combo_set_value, scores, list(range(len(scores)))),
+            GuiToParam('max_depth', md_spin, lambda x: x.value(),
+                       lambda i, x: x.setValue(i), md_min_max, md_min_max),
+            GuiToParam('min_samples_split', mi_spin, lambda x: x.value(),
+                       lambda i, x: x.setValue(i), mi_min_max, mi_min_max),
+            GuiToParam('min_samples_leaf', ml_spin, lambda x: x.value(),
+                       lambda i, x: x.setValue(i), ml_min_max, ml_min_max)]
+
+    def test_parameters_unchecked(self):
+        """Check learner and model for various values of all parameters
+        when pruning parameters are not checked
+        """
+        self.widget.max_depth_spin[0].setCheckState(False)
+        self.widget.min_internal_spin[0].setCheckState(False)
+        self.widget.min_leaf_spin[0].setCheckState(False)
+        for i, val in ((1, None), (2, 2), (3, 1)):
+            el = self.gui_to_params[i]
+            self.gui_to_params[i] = GuiToParam(
+                el.name, el.gui_el, lambda x, value=val: value,
+                el.set, [val], el.set_values[0:1])
+        self.test_parameters()
