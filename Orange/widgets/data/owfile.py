a3ae2a2e081c6f9afd5263b1f5934bc7e706a5b9
Lan Zagar <lan.zagar@fri.uni-lj.si>
2016-07-22 16:38:34 +0200

    owfile: Fix typo in error message

--- Orange/widgets/data/owfile.py
+++ Orange/widgets/data/owfile.py
@@ -256,7 +256,7 @@ class OWFile(widget.OWWidget, RecentPathsWComboMixin):
             try:
                 data = self.reader.read()
             except Exception as ex:
-                errors.append("An error occured:")
+                errors.append("An error occurred:")
                 errors.append(str(ex))
                 data = None
                 self.editor_model.reset()
