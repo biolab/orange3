020550cfa248e1650c3d4ca189014d536ae62616
Kernc <kerncece@gmail.com>
2016-07-29 10:30:11 +0200

    OWVennDiagram: visually indicate that titles are editable
    Refs: https://github.com/biolab/orange3/issues/1083

--- Orange/widgets/visualize/owvenndiagram.py
+++ Orange/widgets/visualize/owvenndiagram.py
@@ -1063,6 +1063,7 @@ class GraphicsTextEdit(QGraphicsTextItem):
 
     def __init__(self, *args, **kwargs):
         super(GraphicsTextEdit, self).__init__(*args, **kwargs)
+        self.setCursor(Qt.IBeamCursor)
         self.setTabChangesFocus(True)
         self._edittrigger = GraphicsTextEdit.DoubleClicked
         self._editing = False
