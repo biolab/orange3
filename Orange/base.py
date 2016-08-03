1d7f935576c33490a8ccc441b90680024e26ac36
Casey Primozic <me@ameo.link>
2016-08-03 10:29:25 -0500

    Re-enabled preprocessor attribute for learners, allowing for None Revert "Removed volatile preprocessor code for `SklLearner` __repr__"
    This reverts commit b1febe26bb35492b172eb0f9cf6f998d0d3f6406.
    
    Conflicts:
    	Orange/base.py

--- Orange/base.py
+++ Orange/base.py
@@ -210,7 +210,7 @@ class SklLearner(Learner, metaclass=WrapperMeta):
     _params = {}
 
     name = 'skl learner'
-    preprocessors = [
+    preprocessors = default_preprocessors = [
                     RemoveNaNClasses(),
                     Continuize(),
                     RemoveNaNColumns(),
@@ -263,9 +263,11 @@ class SklLearner(Learner, metaclass=WrapperMeta):
         return '{} {}'.format(self.name, self.params)
 
     def __repr__(self):
-        return '{}({})'.format(type(self).__name__,
+        return '{}({}{})'.format(type(self).__name__,
                                ", ".join("{}={}".format(k, repr(v))
-                                         for k, v in self.params.items()))
+                                         for k, v in self.params.items()),
+                                ", preprocessors={}".format(repr(self.preprocessors)) if \
+                                        self.preprocessors != self.default_preprocessors else "")
 
     @property
     def supports_weights(self):
