b22e0d8d05cd055cc505dcc95725be697bd327f2
Sašo Stanovnik <sstanovnik@gmail.com>
2016-08-19 10:02:36 +0200

    Add a pandas git build to travis.

--- .travis/stage_install.sh
+++ .travis/stage_install.sh
@@ -1,6 +1,12 @@
 
 [ "$RUN_PYLINT" ] && return 0   # Nothing to do
 
+# pandas requires cython to install from source, cython recommends --no-cython-compile for CI builds
+# also modify the pandas dependency in requirements-core.txt dependency to the git version
+[ "$PANDAS_GIT" ] && \
+    pip install Cython --install-option="--no-cython-compile" && \
+    sed -Eibak "s,^pandas.*,git+git://github.com/pydata/pandas.git," "$TRAVIS_BUILD_DIR/requirements-core.txt"
+
 for script in \
     install_orange.sh    \
     install_postgres.sh  \
