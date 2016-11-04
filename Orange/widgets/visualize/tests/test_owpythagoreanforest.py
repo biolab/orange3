8f9ae15b1a1773dd2932fbca53bd865468632f44
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-11-04 12:21:19 +0100

    Pythagorean forest (tests): Add tests for depth slider

--- Orange/widgets/visualize/tests/test_owpythagoreanforest.py
+++ Orange/widgets/visualize/tests/test_owpythagoreanforest.py
@@ -1,7 +1,9 @@
+from unittest.mock import Mock
+
 from Orange.classification.random_forest import RandomForestLearner
 from Orange.data import Table
 from Orange.regression.random_forest import RandomForestRegressionLearner
-from Orange.widgets.tests.base import WidgetTest, WidgetOutputsTestMixin
+from Orange.widgets.tests.base import WidgetTest
 from Orange.widgets.visualize.owpythagoreanforest import OWPythagoreanForest
 from Orange.widgets.visualize.pythagorastreeviewer import PythagorasTreeViewer
 
@@ -10,7 +12,6 @@ class TestOWPythagoreanForest(WidgetTest):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        WidgetOutputsTestMixin.init(cls)
 
         # Set up for widget tests
         titanic_data = Table('titanic')[::50]
@@ -25,28 +26,37 @@ class TestOWPythagoreanForest(WidgetTest):
     def setUp(self):
         self.widget = self.create_widget(OWPythagoreanForest)
 
-    def get_tree_widgtes(self):
-        return [x for x in self.widget.scene.items() \
+    def get_tree_widgets(self):
+        return [x for x in self.widget.scene.items()
                 if isinstance(x, PythagorasTreeViewer)]
+
+    @staticmethod
+    def set_combo_option(combo_box, text):
+        """Set a given combo box value to some text (given that it exists)."""
+        index = combo_box.findText(text)
+        # This only changes the selection, need to emit signal to call callback
+        combo_box.setCurrentIndex(index)
+        # Apparently `currentIndexChanged` just isn't good enough...
+        combo_box.activated.emit(index)
 
     def test_sending_rf_draws_trees(self):
         # No trees by default
-        self.assertEqual(len(self.get_tree_widgtes()), 0,
+        self.assertEqual(len(self.get_tree_widgets()), 0,
                          'No trees should be drawn when no forest on input')
 
         # Draw trees for classification rf
         self.send_signal('Random forest', self.titanic)
-        self.assertEqual(len(self.get_tree_widgtes()), 3,
+        self.assertEqual(len(self.get_tree_widgets()), 3,
                          'Incorrect number of trees when forest on input')
 
         # Clear trees when None
         self.send_signal('Random forest', None)
-        self.assertEqual(len(self.get_tree_widgtes()), 0,
+        self.assertEqual(len(self.get_tree_widgets()), 0,
                          'Trees are cleared when forest is disconnected')
 
         # Draw trees for regression rf
         self.send_signal('Random forest', self.housing)
-        self.assertEqual(len(self.get_tree_widgtes()), 3,
+        self.assertEqual(len(self.get_tree_widgets()), 3,
                          'Incorrect number of trees when forest on input')
 
     def test_info_label(self):
@@ -62,3 +72,14 @@ class TestOWPythagoreanForest(WidgetTest):
         self.send_signal('Random forest', None)
         self.assertNotRegex(self.widget.ui_info.text(), regex,
                             'Removing RF does not clear info box')
+
+    def test_depth_slider(self):
+        self.send_signal('Random forest', self.titanic)
+
+        trees = self.get_tree_widgets()
+        for tree in trees:
+            tree.set_depth_limit = Mock()
+
+        self.widget.ui_depth_slider.setValue(0)
+        for tree in trees:
+            tree.set_depth_limit.assert_called_once_with(0)
