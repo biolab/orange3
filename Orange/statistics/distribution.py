Fix a FutureWarning from numpy.average (numpy 1.12b)
Passing a ndarray subclass to numpy.average triggers a:

    FutureWarning: np.average currently does not preserve subclasses, but
    will do so in the future to match the behavior of most other numpy
    functions such as np.mean. In particular, this means calls which
    returned a scalar may return a 0-d subclass object instead.

In this case this is not what we want.

Orange/statistics/distribution.py
@@ -268,7 +268,7 @@ class Continuous(np.ndarray):
                 return x
 
     def mean(self):
-        return np.average(self[0], weights=np.asarray(self[1]))
+        return np.average(np.asarray(self[0]), weights=np.asarray(self[1]))
 
     def variance(self):
         avg = self.mean()