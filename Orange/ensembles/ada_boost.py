d41946ea786d45c7f020f87836c6395f72c0ea16
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-11-30 13:25:27 +0100

    OWAdaBoost: Merge, set default to RF

--- Orange/ensembles/ada_boost.py
+++ Orange/ensembles/ada_boost.py
@@ -1,9 +1,13 @@
 import sklearn.ensemble as skl_ensemble
-from Orange.base import SklLearner
-from Orange.classification.base_classification import (SklLearnerClassification,
-                                                       SklModelClassification)
-from Orange.regression.base_regression import (SklLearnerRegression,
-                                               SklModelRegression)
+
+from Orange.base import SklLearner, SklModel
+from Orange.classification.base_classification import (
+    SklLearnerClassification, SklModelClassification
+)
+from Orange.modelling import Fitter
+from Orange.regression.base_regression import (
+    SklLearnerRegression, SklModelRegression
+)
 
 __all__ = ["SklAdaBoostLearner", "SklAdaBoostRegressionLearner"]
 
@@ -12,13 +16,18 @@ class SklAdaBoostClassifier(SklModelClassification):
     pass
 
 
-class SklAdaBoostLearner(SklLearnerClassification):
+class SklAdaBoosClassificationtLearner(SklLearnerClassification):
     __wraps__ = skl_ensemble.AdaBoostClassifier
     __returns__ = SklAdaBoostClassifier
-    name = 'skl adaBoost'
+    name = 'skl adaBoost classification'
 
     def __init__(self, base_estimator=None, n_estimators=50, learning_rate=1.,
                  algorithm='SAMME.R', random_state=None, preprocessors=None):
+        # If fitter, get the appropriate Learner instance
+        if isinstance(base_estimator, Fitter):
+            base_estimator = base_estimator.get_learner(
+                base_estimator.CLASSIFICATION)
+        # If sklearn learner, get the underlying sklearn representation
         if isinstance(base_estimator, SklLearner):
             base_estimator = base_estimator.__wraps__(**base_estimator.params)
         super().__init__(preprocessors=preprocessors)
@@ -36,7 +45,21 @@ class SklAdaBoostRegressionLearner(SklLearnerRegression):
 
     def __init__(self, base_estimator=None, n_estimators=50, learning_rate=1.,
                  loss='linear', random_state=None, preprocessors=None):
+        # If fitter, get the appropriate Learner instance
+        if isinstance(base_estimator, Fitter):
+            base_estimator = base_estimator.get_learner(
+                base_estimator.REGRESSION)
+        # If sklearn learner, get the underlying sklearn representation
         if isinstance(base_estimator, SklLearner):
             base_estimator = base_estimator.__wraps__(**base_estimator.params)
         super().__init__(preprocessors=preprocessors)
         self.params = vars()
+
+
+class SklAdaBoostLearner(Fitter):
+    name = 'skl adaboost'
+
+    __fits__ = {'classification': SklAdaBoosClassificationtLearner,
+                'regression': SklAdaBoostRegressionLearner}
+
+    __returns__ = SklModel
---
+++ Orange/widgets/model/owadaboost.py
@@ -0,0 +1,82 @@
+from AnyQt.QtCore import Qt
+
+from Orange.base import Learner
+from Orange.data import Table
+from Orange.ensembles import SklAdaBoostLearner
+from Orange.modelling import RandomForestLearner
+from Orange.widgets import gui
+from Orange.widgets.settings import Setting
+from Orange.widgets.utils.owlearnerwidget import OWBaseLearner
+
+
+class OWAdaBoost(OWBaseLearner):
+    name = "AdaBoost"
+    description = "An ensemble meta-algorithm that combines weak learners " \
+                  "and adapts to the 'hardness' of each training sample. "
+    icon = "icons/AdaBoost.svg"
+    priority = 80
+
+    LEARNER = SklAdaBoostLearner
+
+    inputs = [("Learner", Learner, "set_base_learner")]
+
+    losses = ["SAMME", "SAMME.R"]
+
+    n_estimators = Setting(50)
+    learning_rate = Setting(1.)
+    algorithm = Setting(0)
+
+    DEFAULT_BASE_ESTIMATOR = RandomForestLearner()
+
+    def add_main_layout(self):
+        box = gui.widgetBox(self.controlArea, "Parameters")
+        self.base_estimator = self.DEFAULT_BASE_ESTIMATOR
+        self.base_label = gui.label(
+            box, self, "Base estimator: " + self.base_estimator.name)
+
+        self.n_estimators_spin = gui.spin(
+            box, self, "n_estimators", 1, 100, label="Number of estimators:",
+            alignment=Qt.AlignRight, callback=self.settings_changed)
+        self.learning_rate_spin = gui.doubleSpin(
+            box, self, "learning_rate", 1e-5, 1.0, 1e-5, label="Learning rate:",
+            decimals=5, alignment=Qt.AlignRight, controlWidth=90,
+            callback=self.settings_changed)
+        self.add_specific_parameters(box)
+
+    def add_specific_parameters(self, box):
+        self.algorithm_combo = gui.comboBox(
+            box, self, "algorithm", label="Algorithm:", items=self.losses,
+            orientation=Qt.Horizontal, callback=self.settings_changed)
+
+    def create_learner(self):
+        return self.LEARNER(
+            base_estimator=self.base_estimator,
+            n_estimators=self.n_estimators,
+            learning_rate=self.learning_rate,
+            preprocessors=self.preprocessors,
+            algorithm=self.losses[self.algorithm]
+        )
+
+    def set_base_learner(self, learner):
+        self.base_estimator = learner if learner \
+            else self.DEFAULT_BASE_ESTIMATOR
+        self.base_label.setText("Base estimator: " + self.base_estimator.name)
+        if self.auto_apply:
+            self.apply()
+
+    def get_learner_parameters(self):
+        return (("Base estimator", self.base_estimator),
+                ("Number of estimators", self.n_estimators),
+                ("Algorithm", self.losses[self.algorithm].capitalize()))
+
+
+if __name__ == "__main__":
+    import sys
+    from AnyQt.QtWidgets import QApplication
+
+    a = QApplication(sys.argv)
+    ow = OWAdaBoost()
+    ow.set_data(Table(sys.argv[1] if len(sys.argv) > 1 else 'iris'))
+    ow.show()
+    a.exec_()
+    ow.saveSettings()
