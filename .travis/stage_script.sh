98a2e3b54b9fdf3f6f3a99cab23d94756fadd267
Kernc <kerncece@gmail.com>
2016-12-01 17:03:03 +0100

    Travis: run tests also on Trusty, Python 3.5, PyQt5

--- .travis/stage_script.sh
+++ .travis/stage_script.sh
@@ -11,7 +11,12 @@ cd "$ORANGE_DIR"
 python -c "from Orange.tests import *"
 cp "$TRAVIS_BUILD_DIR"/.coveragerc ./  # for covereage and codecov
 export PYTHONPATH="$ORANGE_DIR" PYTHONUNBUFFERED=x
-catchsegv xvfb-run coverage run --source=Orange \
-                                -m unittest -v Orange.tests \
-                                               Orange.widgets.tests \
-                                               Orange.canvas.report.tests
+
+# Screen must be 24bpp lest pyqt5 crashes, see pytest-dev/pytest-qt/35
+XVFBARGS="-screen 0 1280x1024x24"
+
+catchsegv xvfb-run -a -s "$XVFBARGS" \
+    coverage run -m unittest -v \
+        Orange.tests \
+        Orange.widgets.tests \
+        Orange.canvas.report.tests
