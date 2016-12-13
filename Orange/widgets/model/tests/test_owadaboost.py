7b398cc89de3a96ff98a76adab48aa5dc0c98787
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-12-13 22:33:08 +0100

    OWAdaBoost: Update model tests, add test for random state

--- Orange/widgets/model/tests/test_owadaboost.py
+++ Orange/widgets/model/tests/test_owadaboost.py
@@ -13,24 +13,38 @@ class TestOWAdaBoost(WidgetTest, WidgetLearnerTestMixin):
         self.widget = self.create_widget(
             OWAdaBoost, stored_settings={"auto_apply": False})
         self.init()
-        losses = self.widget.losses
         self.parameters = [
-            ParameterMapping('algorithm', self.widget.algorithm_combo, losses),
+            ParameterMapping('algorithm', self.widget.cls_algorithm_combo,
+                             self.widget.algorithms),
+            ParameterMapping('loss', self.widget.reg_algorithm_combo,
+                             self.widget.losses),
             ParameterMapping('learning_rate', self.widget.learning_rate_spin),
-            ParameterMapping('n_estimators', self.widget.n_estimators_spin)]
+            ParameterMapping('n_estimators', self.widget.n_estimators_spin),
+            ParameterMapping.from_attribute(
+                self.widget, 'random_seed', 'random_state')]
 
     def test_input_learner(self):
         """Check if base learner properly changes with learner on the input"""
-        default_base_est = self.widget.base_estimator
-        new_learner = SklTreeLearner()
-        # Check that the default learner is indeed a SklTreeLearner
-        self.assertIsInstance(default_base_est, SklTreeLearner)
-        # Sending a new valid learner should change the learner
-        self.send_signal('Learner', new_learner)
-        self.assertEqual(self.widget.base_estimator, new_learner)
-        # Removing the new learner should change back to the default one
-        self.send_signal('Learner', None)
-        self.assertEqual(self.widget.base_estimator, default_base_est)
+        # Check that the default learner is suitable for AdaBoost
+        self.assertIsNotNone(
+            self.widget.base_estimator,
+            "The default base estimator should not be none")
+        self.assertTrue(
+            self.widget.base_estimator.supports_weights,
+            "The default base estimator should support weights")
+        default_base_estimator_cls = self.widget.base_estimator
+
+        # Try a valid learner
+        self.send_signal("Learner", RandomForestLearner())
+        self.assertIsInstance(
+            self.widget.base_estimator, RandomForestLearner,
+            "The base estimator was not updated when valid learner on input")
+
+        # Reset to none
+        self.send_signal("Learner", None)
+        self.assertIsInstance(
+            self.widget.base_estimator, type(default_base_estimator_cls),
+            "The base estimator was not reset to default when None on input")
 
     def test_input_learner_disconnect(self):
         """Check base learner after disconnecting learner on the input"""
