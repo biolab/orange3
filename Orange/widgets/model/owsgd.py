a23f1afd8d25b2b6b8198efb4c1ee4b27e4b4dd8
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-12-14 15:26:23 +0100

    OWSGD: Create new widget that has correct params

---
+++ Orange/widgets/model/owsgd.py
@@ -0,0 +1,195 @@
+from AnyQt.QtCore import Qt
+
+from Orange.modelling.linear import SGDLearner
+from Orange.widgets import gui
+from Orange.widgets.settings import Setting
+from Orange.widgets.utils.owlearnerwidget import OWBaseLearner
+
+MAXINT = 2 ** 31 - 1
+
+
+class OWSGD(OWBaseLearner):
+    name = 'Stochastic Gradient Descent'
+    description = 'Minimize an objective function using a stochastic ' \
+                  'approximation of gradient descent.'
+    icon = "icons/SGD.svg"
+    priority = 90
+    LEARNER = SGDLearner
+
+    regression_losses = (
+        ('Squared Loss', 'squared_loss'),
+        ('Huber', 'huber'),
+        ('ε insensitive', 'epsilon_insensitive'),
+        ('Squared ε insensitive', 'squared_epsilon_insensitive'))
+    classification_losses = (
+        ('Hinge', 'hinge'),
+        ('Logarithmic', 'log'),
+        ('Modified Huber', 'modified_huber'),
+        ('Squared hinge', 'squared_hinge'),
+        ('Perceptron', 'perceptron')) + regression_losses
+
+    #: Regularization methods
+    penalties = (
+        ('None', 'none'),
+        ('Lasso', 'l1'),
+        ('Ridge', 'l2'),
+        ('Elastic Net', 'elasticnet'))
+
+    learning_rates = (
+        ('Constant', 'constant'),
+        ('Optimal', 'optimal'),
+        ('Inverse scaling', 'invscaling'))
+
+    learner_name = Setting('SGD')
+    cls_loss_function_index = Setting(0)
+    reg_loss_function_index = Setting(0)
+    epsilon = Setting(.15)
+
+    penalty_index = Setting(2)
+    #: Regularization strength
+    alpha = Setting(1e-5)
+    #: Elastic Net mixing parameter
+    l1_ratio = Setting(.15)
+
+    shuffle = Setting(True)
+    use_random_state = Setting(False)
+    random_state = Setting(0)
+    learning_rate_index = Setting(0)
+    eta0 = Setting(.01)
+    power_t = Setting(.25)
+    n_iter = Setting(5)
+
+    def add_main_layout(self):
+        box = gui.widgetBox(self.controlArea, 'Algorithm')
+        self.cls_loss_function_combo = gui.comboBox(
+            box, self, 'cls_loss_function_index', orientation=Qt.Horizontal,
+            label='Classification loss function: ',
+            items=self.classification_losses, callback=self.settings_changed)
+        self.reg_loss_function_combo = gui.comboBox(
+            box, self, 'reg_loss_function_index', orientation=Qt.Horizontal,
+            label='Regression loss function: ', items=self.regression_losses,
+            callback=self.settings_changed)
+        param_box = gui.hBox(box)
+        gui.rubber(param_box)
+        self.epsilon_spin = gui.spin(
+            param_box, self, 'epsilon', 0, 1., 1e-2, spinType=float, label='ε: ',
+            controlWidth=80, alignment=Qt.AlignRight,
+            callback=self.settings_changed)
+
+        box = gui.widgetBox(self.controlArea, 'Regularization')
+        self.penalty_combo = gui.comboBox(
+            box, self, 'penalty_index', label='Regularization method: ',
+            items=self.penalties, orientation=Qt.Horizontal,
+            callback=self._on_regularization_change)
+        self.alpha_spin = gui.spin(
+            box, self, 'alpha', 0, 10., .1e-4, spinType=float, controlWidth=80,
+            label='Regularization strength (α): ', alignment=Qt.AlignRight,
+            callback=self.settings_changed)
+        self.l1_ratio_spin = gui.spin(
+            box, self, 'l1_ratio', 0, 1., 1e-2, spinType=float,
+            label='Mixing parameter: ', controlWidth=80,
+            alignment=Qt.AlignRight, callback=self.settings_changed)
+
+        box = gui.widgetBox(self.controlArea, 'Learning parameters')
+        self.learning_rate_combo = gui.comboBox(
+            box, self, 'learning_rate_index', label='Learning rate: ',
+            items=self.learning_rates, orientation=Qt.Horizontal,
+            callback=self._on_learning_rate_change)
+        # The learning rate settings are aligned right
+        param_box = gui.hBox(box)
+        gui.rubber(param_box)
+        self.eta0_spin = gui.spin(
+            param_box, self, 'eta0', 1e-4, 1., 1e-4, spinType=float,
+            label='Initial learning rate (η<sub>0</sub>): ',
+            alignment=Qt.AlignRight, controlWidth=80,
+            callback=self.settings_changed)
+        param_box = gui.hBox(box)
+        gui.rubber(param_box)
+        gui.separator(box, height=12)
+        self.power_t_spin = gui.spin(
+            param_box, self, 'power_t', 0, 1., 1e-4, spinType=float,
+            label='Inverse scaling exponent (t): ',
+            alignment=Qt.AlignRight, controlWidth=80,
+            callback=self.settings_changed)
+
+        self.n_iter_spin = gui.spin(
+            box, self, 'n_iter', 1, MAXINT - 1, label='Number of iterations: ',
+            controlWidth=80, alignment=Qt.AlignRight,
+            callback=self.settings_changed)
+        self.shuffle_cbx = gui.checkBox(
+            box, self, 'shuffle', 'Shuffle data after each iteration',
+            callback=self.settings_changed)
+        self.random_seed_spin = gui.spin(
+            box, self, 'random_state', 0, MAXINT,
+            label='Fixed seed for random generator: ', controlWidth=80,
+            alignment=Qt.AlignRight, callback=self.settings_changed,
+            checked='use_random_state', checkCallback=self.settings_changed)
+
+        # Enable/disable appropriate controls
+        self._on_regularization_change()
+        self._on_learning_rate_change()
+
+    def _on_regularization_change(self):
+        # Alpha parameter
+        if self.penalties[self.penalty_index][1] in ('l1', 'l2', 'elasticnet'):
+            self.alpha_spin.setEnabled(True)
+        else:
+            self.alpha_spin.setEnabled(False)
+        # Elastic Net mixing parameter
+        if self.penalties[self.penalty_index][1] in ('elasticnet',):
+            self.l1_ratio_spin.setEnabled(True)
+        else:
+            self.l1_ratio_spin.setEnabled(False)
+
+        self.settings_changed()
+
+    def _on_learning_rate_change(self):
+        # Eta_0 parameter
+        if self.learning_rates[self.learning_rate_index][1] in \
+                ('constant', 'invscaling'):
+            self.eta0_spin.setEnabled(True)
+        else:
+            self.eta0_spin.setEnabled(False)
+        # Power t parameter
+        if self.learning_rates[self.learning_rate_index][1] in \
+                ('invscaling',):
+            self.power_t_spin.setEnabled(True)
+        else:
+            self.power_t_spin.setEnabled(False)
+
+        self.settings_changed()
+
+    def create_learner(self):
+        params = {}
+        if self.use_random_state:
+            params['random_state'] = self.random_state
+
+        return self.LEARNER(
+            cls_loss=self.classification_losses[
+                self.cls_loss_function_index][1],
+            reg_loss=self.regression_losses[self.reg_loss_function_index][1],
+            epsilon=self.epsilon,
+            penalty=self.penalties[self.penalty_index][1],
+            alpha=self.alpha,
+            l1_ratio=self.l1_ratio,
+            shuffle=self.shuffle,
+            learning_rate=self.learning_rates[self.learning_rate_index][1],
+            eta0=self.eta0,
+            power_t=self.power_t,
+            n_iter=self.n_iter,
+            **params)
+
+
+if __name__ == '__main__':
+    import sys
+    from AnyQt.QtWidgets import QApplication
+    from Orange.data import Table
+
+    a = QApplication(sys.argv)
+    ow = OWSGD()
+    ow.resetSettings()
+    d = Table(sys.argv[1] if len(sys.argv) > 1 else 'iris')
+    ow.set_data(d)
+    ow.show()
+    a.exec_()
+    ow.saveSettings()
