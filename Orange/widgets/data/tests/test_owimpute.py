2f8bbc3d9a17e1dcdeea48e396d8f8682bbc556e
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2017-01-26 18:21:39 +0100

    owimpute: Add tests for method selection

--- Orange/widgets/data/tests/test_owimpute.py
+++ Orange/widgets/data/tests/test_owimpute.py
@@ -3,13 +3,14 @@
 import numpy as np
 
 from Orange.data import Table
-from Orange.widgets.data.owimpute import OWImpute
+from Orange.preprocess import impute
+from Orange.widgets.data.owimpute import OWImpute, AsDefault
 from Orange.widgets.tests.base import WidgetTest
-
+from Orange.widgets.utils.itemmodels import select_row
 
 class TestOWImpute(WidgetTest):
     def setUp(self):
-        self.widget = self.create_widget(OWImpute)
+        self.widget = self.create_widget(OWImpute)  # type: OWImpute
 
     def test_empty_data(self):
         """No crash on empty data"""
@@ -43,3 +44,49 @@ class TestOWImpute(WidgetTest):
         self.send_signal("Learner", lambda *_: (lambda *_: 1/0))  # Model fails
         widget.unconditional_commit()
         self.assertTrue(widget.Error.imputation_failed.is_shown())
+
+    def test_select_method(self):
+        data = Table("iris")[::5]
+
+        self.send_signal("Data", data)
+        method_types = [type(m) for m in OWImpute.METHODS]
+
+        widget = self.widget
+        model = widget.varmodel
+        view = widget.varview
+        defbg = widget.default_button_group
+        varbg = widget.variable_button_group
+        self.assertSequenceEqual(list(model), data.domain.variables)
+        asdefid = method_types.index(AsDefault)
+        leaveid = method_types.index(impute.DoNotImpute)
+        avgid = method_types.index(impute.Average)
+        defbg.button(avgid).click()
+        self.assertEqual(widget.default_method_index, avgid)
+
+        self.assertTrue(
+            all(isinstance(m, AsDefault) and isinstance(m.method, impute.Average)
+                for m in map(widget.get_method_for_column,
+                             range(len(data.domain.variables)))))
+
+        # change method for first variable
+        select_row(view, 0)
+        varbg.button(avgid).click()
+        met = widget.get_method_for_column(0)
+        self.assertIsInstance(met, impute.Average)
+
+        # select a second var
+        selmodel = view.selectionModel()
+        selmodel.select(model.index(2), selmodel.Select)
+        # the current checked button must unset
+        self.assertEqual(varbg.checkedId(), -1)
+
+        varbg.button(leaveid).click()
+        self.assertIsInstance(widget.get_method_for_column(0),
+                              impute.DoNotImpute)
+        self.assertIsInstance(widget.get_method_for_column(2),
+                              impute.DoNotImpute)
+
+        # reset both back to default
+        varbg.button(asdefid).click()
+        self.assertIsInstance(widget.get_method_for_column(0), AsDefault)
+        self.assertIsInstance(widget.get_method_for_column(2), AsDefault)
