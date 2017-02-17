3280a7043a33738be8ba04fabb3face52a7a70dc
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2017-02-17 18:24:45 +0100

    OWWidget: Mark as explicitly positioned on first show
    Fixes gh-1846

--- Orange/widgets/widget.py
+++ Orange/widgets/widget.py
@@ -387,6 +387,16 @@ class OWWidget(QDialog, OWComponent, Report, ProgressBarMixin,
                     y = max(0, space.height() / 2 - height / 2)
 
                     self.move(x, y)
+
+        # Mark as explicitly moved/resized if not already. QDialog would
+        # otherwise adjust position/size on subsequent hide/show
+        # (move/resize events coming from the window manager do not set
+        # these flags).
+        if not self.testAttribute(Qt.WA_Moved):
+            self.setAttribute(Qt.WA_Moved)
+        if not self.testAttribute(Qt.WA_Resized):
+            self.setAttribute(Qt.WA_Resized)
+
         return restored
 
     def __updateSavedGeometry(self):
@@ -436,7 +446,7 @@ class OWWidget(QDialog, OWComponent, Report, ProgressBarMixin,
         """
         QDialog.showEvent(self, event)
         if self.save_position and not self.__was_restored:
-            # Restore saved geometry on show
+            # Restore saved geometry on (first) show
             self.__restoreWidgetGeometry()
             self.__was_restored = True
         self.__quicktipOnce()
