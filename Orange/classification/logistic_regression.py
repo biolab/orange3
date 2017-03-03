3acf46a9f68f59c7f6ed54c5dd18b0405ac77b2b
Lan Zagar <lan.zagar@fri.uni-lj.si>
2017-03-03 11:33:05 +0100

    logistic_regression: Expand parameter list

--- Orange/classification/logistic_regression.py
+++ Orange/classification/logistic_regression.py
@@ -40,7 +40,8 @@ class LogisticRegressionLearner(SklLearner, _FeatureScorerMixin):
 
     def __init__(self, penalty="l2", dual=False, tol=0.0001, C=1.0,
                  fit_intercept=True, intercept_scaling=1, class_weight=None,
-                 random_state=None, preprocessors=None):
+                 random_state=None, solver='liblinear', max_iter=100,
+                 multi_class='ovr', verbose=0, n_jobs=1, preprocessors=None):
         super().__init__(preprocessors=preprocessors)
         self.params = vars()
 
