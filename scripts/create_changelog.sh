14576f666c73338402f5895d344f59d3702546b5
Lan Zagar <lan.zagar@fri.uni-lj.si>
2017-03-17 10:27:33 +0100

    create_changelog: Filter out already reported items

--- scripts/create_changelog.sh
+++ scripts/create_changelog.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+set -u -e -o pipefail
 
 VERSION=$(grep '^VERSION' setup.py | sed -E "s/.*'([^']*)'/\1/")
 RELEASE_DATE=$(date "+%Y-%m-%d")
+
+# compile a list of already reported PRs
+reported="$(mktemp)"
+grep -E "#[0-9]{4}" -o CHANGELOG.md | sort > "$tempfile"
 
 echo "[$VERSION] - $RELEASE_DATE"
 echo "--------------------"
 echo '##### Enhancements'
 git log stable..master --first-parent --format='%s %b' |
-sed -E 's/.*#([0-9]+).*\[ENH\] *(.*)/\* \2 ([#\1](\.\.\/\.\.\/pull\/\1))/' |
-    grep -E '^\*'
+    sed -E 's/.*#([0-9]+).*\[ENH\] *(.*)/\* \2 ([#\1](\.\.\/\.\.\/pull\/\1))/' |
+    grep -E '^\*' | grep -v -F -f "$reported"
 echo
 echo "##### Bugfixes"
 git log stable..master --first-parent --format='%s %b' |
     sed -E 's/.*#([0-9]+).*\[FIX\] *(.*)/\* \2 ([#\1](\.\.\/\.\.\/pull\/\1))/' |
-    grep -E '^\*'
+    grep -E '^\*' | grep -v -F -f "$reported"
