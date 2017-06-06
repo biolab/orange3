e6f0391886b56df6af48f2044f5d952f2e016435
Jernej Urankar <jerneju@gmail.com>
2017-06-06 12:40:09 +0200

    Preprocess: new signals

--- Orange/widgets/data/owpreprocess.py
+++ Orange/widgets/data/owpreprocess.py
@@ -29,6 +29,7 @@ from Orange.preprocess import Continuize, ProjectPCA, \
 from Orange.widgets import widget, gui, settings
 from Orange.widgets.utils.overlay import OverlayWidget
 from Orange.widgets.utils.sql import check_sql_input
+from Orange.widgets.widget import Input, Output
 
 from Orange.widgets.data.utils.preprocess import (
     BaseEditor, blocked, StandardItemModel, DescriptionRole,
@@ -952,9 +953,12 @@ class OWPreprocess(widget.OWWidget):
     icon = "icons/Preprocess.svg"
     priority = 2105
 
-    inputs = [("Data", Orange.data.Table, "set_data")]
-    outputs = [("Preprocessor", preprocess.preprocess.Preprocess),
-               ("Preprocessed Data", Orange.data.Table)]
+    class Inputs:
+        data = Input("Data", Orange.data.Table)
+
+    class Outputs:
+        preprocessor = Output("Preprocessor", preprocess.preprocess.Preprocess, dynamic=False)
+        preprocessed_data = Output("Preprocessed Data", Orange.data.Table)
 
     storedsettings = settings.Setting({})
     autocommit = settings.Setting(True)
@@ -1134,6 +1138,7 @@ class OWPreprocess(widget.OWWidget):
         self.__update_overlay()
         self.commit()
 
+    @Inputs.data
     @check_sql_input
     def set_data(self, data=None):
         """Set the input data set."""
@@ -1184,8 +1189,8 @@ class OWPreprocess(widget.OWWidget):
         else:
             data = None
 
-        self.send("Preprocessor", preprocessor)
-        self.send("Preprocessed Data", data)
+        self.Outputs.preprocessor.send(preprocessor)
+        self.Outputs.preprocessed_data.send(data)
 
     def commit(self):
         if not self._invalidated:
