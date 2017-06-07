c5f954fa20669d0082aa216a6606d42643ee6ac8
Jernej Urankar <jerneju@gmail.com>
2017-06-07 12:19:32 +0200

    [FIX] Confusion Matrix: cyclomatic complexity refactor

--- Orange/widgets/evaluate/owconfusionmatrix.py
+++ Orange/widgets/evaluate/owconfusionmatrix.py
@@ -354,53 +354,57 @@ class OWConfusionMatrix(widget.OWWidget):
             self.tableview.selectionModel().select(
                 selection, QItemSelectionModel.ClearAndSelect)
 
+    def _prepare_data(self):
+        indices = self.tableview.selectedIndexes()
+        indices = {(ind.row() - 2, ind.column() - 2) for ind in indices}
+        actual = self.results.actual
+        learner_name = self.learners[self.selected_learner[0]]
+        predicted = self.results.predicted[self.selected_learner[0]]
+        selected = [i for i, t in enumerate(zip(actual, predicted))
+                    if t in indices]
+
+        extra = []
+        class_var = self.data.domain.class_var
+        metas = self.data.domain.metas
+
+        if self.append_predictions:
+            extra.append(predicted.reshape(-1, 1))
+            var = Orange.data.DiscreteVariable(
+                "{}({})".format(class_var.name, learner_name),
+                class_var.values
+            )
+            metas = metas + (var,)
+
+        if self.append_probabilities and \
+                        self.results.probabilities is not None:
+            probs = self.results.probabilities[self.selected_learner[0]]
+            extra.append(np.array(probs, dtype=object))
+            pvars = [Orange.data.ContinuousVariable("p({})".format(value))
+                     for value in class_var.values]
+            metas = metas + tuple(pvars)
+
+        domain = Orange.data.Domain(self.data.domain.attributes,
+                                    self.data.domain.class_vars,
+                                    metas)
+        data = self.data.transform(domain)
+        data.metas[:, len(self.data.domain.metas):] = \
+            np.hstack(tuple(extra))
+        data.name = learner_name
+
+        if selected:
+            annotated_data = create_annotated_table(data, selected)
+            data = data[selected]
+        else:
+            annotated_data = create_annotated_table(data, [])
+            data = None
+
+        return data, annotated_data
+
     def commit(self):
         """Output data instances corresponding to selected cells"""
         if self.results is not None and self.data is not None \
                 and self.selected_learner:
-            indices = self.tableview.selectedIndexes()
-            indices = {(ind.row() - 2, ind.column() - 2) for ind in indices}
-            actual = self.results.actual
-            learner_name = self.learners[self.selected_learner[0]]
-            predicted = self.results.predicted[self.selected_learner[0]]
-            selected = [i for i, t in enumerate(zip(actual, predicted))
-                        if t in indices]
-
-            extra = []
-            class_var = self.data.domain.class_var
-            metas = self.data.domain.metas
-
-            if self.append_predictions:
-                extra.append(predicted.reshape(-1, 1))
-                var = Orange.data.DiscreteVariable(
-                    "{}({})".format(class_var.name, learner_name),
-                    class_var.values
-                )
-                metas = metas + (var,)
-
-            if self.append_probabilities and \
-                    self.results.probabilities is not None:
-                probs = self.results.probabilities[self.selected_learner[0]]
-                extra.append(np.array(probs, dtype=object))
-                pvars = [Orange.data.ContinuousVariable("p({})".format(value))
-                         for value in class_var.values]
-                metas = metas + tuple(pvars)
-
-            domain = Orange.data.Domain(self.data.domain.attributes,
-                                        self.data.domain.class_vars,
-                                        metas)
-            data = self.data.transform(domain)
-            data.metas[:, len(self.data.domain.metas):] = \
-                np.hstack(tuple(extra))
-            data.name = learner_name
-
-            if selected:
-                annotated_data = create_annotated_table(data, selected)
-                data = data[selected]
-            else:
-                annotated_data = create_annotated_table(data, [])
-                data = None
-
+            data, annotated_data = self._prepare_data()
         else:
             data = None
             annotated_data = None
