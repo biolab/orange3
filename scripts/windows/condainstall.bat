3629b7e284cfc9a05e9a678f38d17f97678f9654
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2017-06-16 12:13:11 +0200

    Fix condainstall script
    * Use delayed expansion for ERRORLEVEL inside blocks.
    * Use full absolute paths to packages for compatibility with
      conda 4.2.12
    * conda create call compatible with conda <= 4.1.11

--- scripts/windows/condainstall.bat
+++ scripts/windows/condainstall.bat
@@ -9,26 +9,24 @@ set CONDA=%~2
 
 if not exist "%PREFIX%\python.exe" (
     echo Creating a conda env in "%PREFIX%"
-    rem Create an empty initial skeleton to layout the conda, activate.bat and
-    rem other things needed to manage the environment.
-    "%CONDA%" create --yes  --quiet --prefix "%PREFIX%" || exit /b %ERRORLEVEL%
-    set BASEPKGS=
-    for %%f in ( vs*_runtime*.tar.bz2 python-*.tar.bz2 ) do (
-        set BASEPKGS=!BASEPKGS! %%f
+    rem # Create an empty initial skeleton to layout the conda, activate.bat
+    rem # and other things needed to manage the environment. conda 4.1.*
+    rem # requires at least one package name to succeed
+    for %%f in ( vs*_runtime*.tar.bz2 ) do (
+        "%CONDA%" create --yes  --quiet --prefix "%PREFIX%" "%CD%\%%f" ^
+            || exit /b !ERRORLEVEL!
     )
-    "%CONDA%" install --yes --copy --quiet --prefix  "%PREFIX%" !BASEPKGS!
-    if errorlevel 1 (
-        echo "Error creating a conda environment. conda command exited with "
-              "%ERRORLEVEL%"
-        exit /b %ERRORLEVEL%
+
+    rem # Also install python (msvc runtime and python might be required
+    rem # for any post-link scripts).
+    for %%f in ( python-*.tar.bz2 ) do (
+        "%CONDA%" install --yes --copy --quiet --prefix "%PREFIX%" "%CD%\%%f" ^
+            || exit /b !ERRORLEVEL!
     )
 )
 
 for %%f in ( *.tar.bz2 ) do (
     echo Installing: %%f
-    "%CONDA%" install --yes  --copy --quiet --prefix "%PREFIX%" %%f
-    if errorlevel 1 (
-        echo "Error installing %%f. conda command exited with %ERRORLEVEL%"
-        exit /b %ERRORLEVEL%
-    )
+    "%CONDA%" install --yes  --copy --quiet --prefix "%PREFIX%" "%CD%\%%f" ^
+        || exit /b !ERRORLEVEL!
 )
