03985561b55ef925ea000706122e4c6a4a3492f4
Kernc <kerncece@gmail.com>
2017-07-07 10:36:49 +0200

    DOC: fix RTFD via append instead of inserting project root to sys.path
    With insert, `import Orange.data.Table` tried (and failed on RTFD)
    to import Orange from the worktree instead of the installed lib.

--- doc/data-mining-library/source/conf.py
+++ doc/data-mining-library/source/conf.py
@@ -22,7 +22,7 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0, os.path.abspath('../../..'))
+sys.path.append(os.path.abspath('../../..'))
 
 # -- General configuration ------------------------------------------------
 
