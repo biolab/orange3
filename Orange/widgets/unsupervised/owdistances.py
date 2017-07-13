09aca0529c796d10008610a236ef18d6cfcab44d
janezd <janez.demsar@fri.uni-lj.si>
2017-07-13 22:58:47 +0200

    distances: Refactor checks in OWDistances

--- Orange/widgets/unsupervised/owdistances.py
+++ Orange/widgets/unsupervised/owdistances.py
@@ -1,5 +1,6 @@
 from AnyQt.QtCore import Qt
 from scipy.sparse import issparse
+import bottleneck as bn
 
 import Orange.data
 import Orange.misc
@@ -42,15 +43,13 @@ class OWDistances(OWWidget):
 
     class Error(OWWidget.Error):
         no_continuous_features = Msg("No numeric features")
-        dense_metric_sparse_data = Msg("Selected metric does not support sparse data")
-        empty_data = Msg("Empty data set")
-        mahalanobis_error = Msg("{}")
-        distances_memory_error = Msg("Not enough memory.")
-        distances_value_error = Msg("Error occurred while calculating distances\n{}")
+        dense_metric_sparse_data = Msg("{} requires dense data.")
+        distances_memory_error = Msg("Not enough memory")
+        distances_value_error = Msg("Problem in calculation:\n{}")
 
     class Warning(OWWidget.Warning):
         ignoring_discrete = Msg("Ignoring categorical features")
-        imputing_data = Msg("Imputing missing values")
+        imputing_data = Msg("Missing values were imputed")
 
     def __init__(self):
         super().__init__()
@@ -75,78 +74,65 @@ class OWDistances(OWWidget):
     @Inputs.data
     @check_sql_input
     def set_data(self, data):
-        """
-        Set the input data set from which to compute the distances
-        """
         self.data = data
         self.refresh_metrics()
         self.unconditional_commit()
 
     def refresh_metrics(self):
-        """
-        Refresh available metrics depending on the input data's sparsenes
-        """
         sparse = self.data is not None and issparse(self.data.X)
         for i, metric in enumerate(METRICS):
             item = self.metrics_combo.model().item(i)
             item.setEnabled(not sparse or metric[1].supports_sparse)
 
-        self._checksparse()
-
-    def _checksparse(self):
-        # Check the current metric for input data compatibility and set/clear
-        # appropriate informational GUI state
-        self.Error.dense_metric_sparse_data(
-            shown=self.data is not None and issparse(self.data.X) and
-            not METRICS[self.metric_idx][1].supports_sparse)
-
     def commit(self):
+        # pylint: disable=invalid-sequence-index
         metric = METRICS[self.metric_idx][1]
         dist = self.compute_distances(metric, self.data)
         self.Outputs.distances.send(dist)
 
     def compute_distances(self, metric, data):
-        def checks(metric, data):
-            if data is None:
-                return
-            if issparse(data.X):
-                if not metric.supports_sparse:
-                    self.Error.dense_metric_sparse_data()
-                    return
-                remove_discrete = metric.fallback is not None \
-                        and self.data.domain.has_discrete_attributes()
-            else:  # not sparse
-                remove_discrete = \
-                    not metric.supports_discrete or self.axis == 1 \
-                    and self.data.domain.has_discrete_attributes()
-            if remove_discrete:
-                data = distance._preprocess(data, impute=False)
-                if data.X.size:
-                    self.Warning.ignoring_discrete()
-            if not data.X.size:
-                self.Error.empty_data()
-                return
-            return data
+        def _check_sparse():
+            # pylint: disable=invalid-sequence-index
+            if issparse(data.X) and not metric.supports_sparse:
+                self.Error.dense_metric_sparse_data(METRICS[self.metric_idx][0])
+                return False
+
+        def _fix_discrete():
+            nonlocal data
+            if data.domain.has_discrete_attributes() and (
+                    issparse(data.X) and getattr(metric, "fallback", None)
+                    or not metric.supports_discrete
+                    or self.axis == 1):
+                if not data.domain.has_continuous_attributes():
+                    self.Error.no_continuous_features()
+                    return False
+                self.Warning.ignoring_discrete()
+                data = distance.remove_discrete_features(data)
+
+        def _fix_missing():
+            nonlocal data
+            if not metric.supports_missing and bn.anynan(data.X):
+                self.Warning.imputing_data()
+                data = distance.impute(data)
 
         self.clear_messages()
-        data = checks(metric, data)
         if data is None:
             return
+        for check in (_check_sparse, _fix_discrete, _fix_missing):
+            if check() is False:
+                return
         try:
-            met = metric(data, data, 1 - self.axis, impute=True)
+            return metric(data, axis=1 - self.axis, impute=True)
         except ValueError as e:
             self.Error.distances_value_error(e)
-            return
         except MemoryError:
             self.Error.distances_memory_error()
-            return
-        return met
 
     def _invalidate(self):
-        self._checksparse()
         self.commit()
 
     def send_report(self):
+        # pylint: disable=invalid-sequence-index
         self.report_items((
             ("Distances Between", ["Rows", "Columns"][self.axis]),
             ("Metric", METRICS[self.metric_idx][0])
