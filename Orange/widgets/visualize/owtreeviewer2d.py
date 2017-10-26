b0a53549e2704507a3d80c4bf43e7dfbf1d68743
Jernej Urankar <jerneju@gmail.com>
2017-10-26 10:15:42 +0200

    [FIX] Tree Viewer: minimum line width

--- Orange/widgets/visualize/owtreeviewer2d.py
+++ Orange/widgets/visualize/owtreeviewer2d.py
@@ -463,13 +463,14 @@ class OWTreeViewer2D(OWWidget):
         tree_adapter = self.root_node.tree_adapter
         root_instances = tree_adapter.num_samples(self.root_node.node_inst)
         width = 3
+        OFFSET = 0.20
         for edge in self.scene.edges():
             num_inst = tree_adapter.num_samples(edge.node2.node_inst)
             if self.line_width_method == 1:
-                width = 8 * num_inst / root_instances
+                width = 8 * num_inst / root_instances + OFFSET
             elif self.line_width_method == 2:
                 width = 8 * num_inst / tree_adapter.num_samples(
-                    edge.node1.node_inst)
+                    edge.node1.node_inst) + OFFSET
             edge.setPen(QPen(Qt.gray, width, Qt.SolidLine, Qt.RoundCap))
         self.scene.update()
 
