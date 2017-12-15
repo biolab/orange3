e7c80039d528189fb5aefcbece20462a334b2bb4
janezd <janez.demsar@fri.uni-lj.si>
2017-12-15 18:20:03 +0100

    DomainContextSetting: don't skip meta-only domains

--- Orange/widgets/settings.py
+++ Orange/widgets/settings.py
@@ -916,7 +916,7 @@ class DomainContextHandler(ContextHandler):
         return context
 
     def open_context(self, widget, domain):
-        if not domain:
+        if domain is None:
             return None, False
 
         if not isinstance(domain, Domain):
