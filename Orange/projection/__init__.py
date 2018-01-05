1927659c656ac1d718341a5ec1554fdc44e89424
janezd <janez.demsar@fri.uni-lj.si>
2018-01-05 20:20:15 +0100

    Remove wild card imports or disable pylint warnings

--- Orange/projection/__init__.py
+++ Orange/projection/__init__.py
@@ -1,7 +1,10 @@
+# The package is pulling names from modules with defined __all__
+# pylint: disable=wildcard-import
+
 from .base import *
 from .pca import *
 from .cur import *
 from .manifold import *
 from .freeviz import *
-from .radviz import radviz
-from .lda import LDA
+from .radviz import *
+from .lda import *
