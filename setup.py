b2bcf5323a7ede3549fa9a4cb41038ee02d152f6
Lan Zagar <lan.zagar@fri.uni-lj.si>
2018-02-02 13:53:23 +0100

    Release 3.9.1

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ from distutils.command.build_ext import build_ext
 
 NAME = 'Orange3'
 
-VERSION = '3.10.0'
-ISRELEASED = False
+VERSION = '3.9.1'
+ISRELEASED = True
 # full version identifier including a git revision identifier for development
 # build/releases (this is filled/updated in `write_version_py`)
 FULLVERSION = VERSION
