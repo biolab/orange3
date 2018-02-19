a7e49344d0273c9a421704ce44338188a5038d5f
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-02-19 14:11:13 +0100

    OWKmeans: Do not recluster when X does not change

--- Orange/widgets/unsupervised/owkmeans.py
+++ Orange/widgets/unsupervised/owkmeans.py
@@ -469,8 +469,14 @@ class OWKMeans(widget.OWWidget):
     @Inputs.data
     @check_sql_input
     def set_data(self, data):
-        self.data = data
-        self.invalidate()
+        self.data, old_data = data, self.data
+
+        # Do not needlessly recluster the data if X hasn't changed
+        if old_data and self.data and np.array_equal(self.data.X, old_data.X):
+            if self.auto_commit:
+                self.send_data()
+        else:
+            self.invalidate()
 
     def send_report(self):
         # False positives (Setting is not recognized as int)
