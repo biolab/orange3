a65acbfb603e9e9aae7dc6989a87786a87842a1c
astaric <anze.staric@gmail.com>
2018-03-05 15:49:44 +0100

    installer: look for activate.bat
    python might not be installed yet

--- scripts/windows/condainstall.bat
+++ scripts/windows/condainstall.bat
@@ -7,7 +7,7 @@ set PREFIX=%~1
 rem Path to conda executable
 set CONDA=%~2
 
-if not exist "%PREFIX%\python.exe" (
+if not exist "%PREFIX%\Scripts\activate.bat" (
     echo Creating a conda env in "%PREFIX%"
     rem # Create an empty initial skeleton to layout the conda, activate.bat
     rem # and other things needed to manage the environment.
