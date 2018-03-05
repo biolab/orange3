71f2551a5a85dc3ee0cbd606226bb73b3cd64281
astaric <anze.staric@gmail.com>
2018-03-05 15:37:34 +0100

    installer: drop support for conda 4.1
    and all workarounds that go with it. 4.1 is not even available for
    python3.6, 4.2 has been around for a year and a half.

--- scripts/windows/condainstall.bat
+++ scripts/windows/condainstall.bat
@@ -6,25 +6,12 @@ rem Target install prefix
 set PREFIX=%~1
 rem Path to conda executable
 set CONDA=%~2
-rem Path to activate.bat
-set ACTIVATE=%CONDA%\..\activate.bat
 
 if not exist "%PREFIX%\python.exe" (
     echo Creating a conda env in "%PREFIX%"
     rem # Create an empty initial skeleton to layout the conda, activate.bat
-    rem # and other things needed to manage the environment. conda 4.1.*
-    rem # requires at least one package name to succeed
-    for %%f in ( vs*_runtime*.tar.bz2 ) do (
-        "%CONDA%" create --yes  --quiet --prefix "%PREFIX%" "%CD%\%%f" ^
-            || exit /b !ERRORLEVEL!
-    )
-
-    rem # Also install python (msvc runtime and python might be required
-    rem # for any post-link scripts).
-    for %%f in ( python-*.tar.bz2 ) do (
-        "%CONDA%" install --yes --copy --quiet --prefix "%PREFIX%" "%CD%\%%f" ^
-            || exit /b !ERRORLEVEL!
-    )
+    rem # and other things needed to manage the environment.
+    "%CONDA%" create --yes  --quiet --prefix "%PREFIX%"
 )
 
 rem # Create .condarc file that includes conda-forge channel
@@ -39,8 +26,3 @@ for %%f in ( *.tar.bz2 ) do (
     "%CONDA%" install --yes  --copy --quiet --prefix "%PREFIX%" "%CD%\%%f" ^
         || exit /b !ERRORLEVEL!
 )
-
-rem # Activate the environment makes conda create
-rem # activate.bat, conda.bat and other shortcuts.
-echo Activating the environment for the first time"
-"%ACTIVATE%" "%PREFIX%"
