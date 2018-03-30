35cd0cd25794f7a6884ca74c75970e45d0e645b4
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2018-03-30 12:29:08 +0200

    report: Fix test discovery

--- Orange/widgets/report/tests/__init__.py
+++ Orange/widgets/report/tests/__init__.py
@@ -5,9 +5,10 @@ import Orange
 
 
 def suite(loader=None, pattern='test*.py'):
+    top_level_dir = dirname(dirname(Orange.__file__))
     return unittest.TestSuite(loader.discover(dirname(__file__),
                                               pattern or 'test*.py',
-                                              dirname(Orange.__file__)))
+                                              top_level_dir))
 
 
 def load_tests(loader, tests, pattern):
