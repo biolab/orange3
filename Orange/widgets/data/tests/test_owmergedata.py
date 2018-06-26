09e0952b683b4a456a630b9b82ccb6d695f4c597
robert <robert.cvitkovic@gmail.com>
2018-06-26 15:41:41 +0200

    tests: add owmergedata test autoapply button

--- Orange/widgets/data/tests/test_owmergedata.py
+++ Orange/widgets/data/tests/test_owmergedata.py
@@ -450,3 +450,10 @@ class TestOWMergeData(WidgetTest):
 
         output_sparse.X = output_sparse.X.toarray()
         self.assertTablesEqual(output_dense, output_sparse)
+
+    def test_disable_auto_apply(self):
+        """Check if no output on auto apply false"""
+        self.widget.auto_apply = False
+        self.send_signal(self.widget.Inputs.data, self.dataA)
+        self.send_signal(self.widget.Inputs.extra_data, self.dataB)
+        self.assertIsNone(self.get_output(self.widget.Outputs.data))
