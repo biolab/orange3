3e1cba81b5b834e594866e30eca42df097a17e8e
Rafael Irgolic <hello@irgolic.com>
2018-11-04 23:46:32 +0000

    LinksEditWidget: line snaps to center of anchor

--- Orange/canvas/document/editlinksdialog.py
+++ Orange/canvas/document/editlinksdialog.py
@@ -293,7 +293,17 @@ class LinksEditWidget(QGraphicsWidget):
             if self.__tmpLine:
                 # Update the temp line
                 line = self.__tmpLine.line()
-                line.setP2(event.pos())
+
+                maybe_anchor = find_item_at(self.scene(), event.scenePos(),
+                                       type=ChannelAnchor)
+                # If hovering over anchor
+                if maybe_anchor is not None:
+                    target_pos = maybe_anchor.boundingRect().center()
+                    target_pos = self.mapFromItem(maybe_anchor, target_pos)
+                    line.setP2(target_pos)
+                else:
+                    line.setP2(event.pos())
+
                 self.__tmpLine.setLine(line)
 
         QGraphicsWidget.mouseMoveEvent(self, event)
