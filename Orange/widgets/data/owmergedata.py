ae9b5b0b700085f6bcd46f225387d5f56dc81771
Andreja <kovacic.andreja@gmail.com>
2018-12-07 12:39:12 +0100

    append data instead of create new

--- Orange/widgets/data/owmergedata.py
+++ Orange/widgets/data/owmergedata.py
@@ -360,16 +360,16 @@ class OWMergeData(widget.OWWidget):
         of rows given in indices"""
         if not len(indices):
             return None
-        domain = Orange.data.Domain(
+        self.data.domain = Orange.data.Domain(
             *(getattr(self.data.domain, x) + getattr(reduced_extra.domain, x)
               for x in ("attributes", "class_vars", "metas")))
-        X = self._join_array_by_indices(self.data.X, reduced_extra.X, indices)
-        Y = self._join_array_by_indices(
+        self.data.X = self._join_array_by_indices(self.data.X, reduced_extra.X, indices)
+        self.data.Y = self._join_array_by_indices(
             np.c_[self.data.Y], np.c_[reduced_extra.Y], indices)
-        string_cols = [i for i, var in enumerate(domain.metas) if var.is_string]
-        metas = self._join_array_by_indices(
+        string_cols = [i for i, var in enumerate(self.data.domain.metas) if var.is_string]
+        self.data.metas = self._join_array_by_indices(
             self.data.metas, reduced_extra.metas, indices, string_cols)
-        return Orange.data.Table.from_numpy(domain, X, Y, metas)
+        return self.data
 
     @staticmethod
     def _join_array_by_indices(left, right, indices, string_cols=None):
