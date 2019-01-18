bde2cbe7524c18c400af7099b3c4322315ff70d2
Tomaz Hocevar <tomaz.hocevar@fri.uni-lj.si>
2019-01-18 10:27:57 +0100

    Add nanmode as a replacement for scipy(<1.2.0).stats.stats.mode

--- Orange/statistics/util.py
+++ Orange/statistics/util.py
@@ -9,6 +9,8 @@ from warnings import warn
 import bottleneck as bn
 import numpy as np
 from scipy import sparse as sp
+import scipy.stats.stats
+from distutils.version import StrictVersion
 
 
 def _count_nans_per_row_sparse(X, weights, dtype=None):
@@ -460,6 +462,16 @@ def nanmedian(x, axis=None):
             return np.nanmedian(x.toarray())
 
     return _apply_func(x, np.nanmedian, nanmedian_sparse, axis=axis)
+
+
+def nanmode(x, axis=0):
+    """ A temporary replacement for a buggy scipy.stats.stats.mode from scipy < 1.2.0"""
+    if StrictVersion(scipy.__version__) >= StrictVersion("1.2.0"):
+        warn("Use scipy.stats.mode in scipy >= 1.2.0", DeprecationWarning)
+    nans = np.isnan(np.array(x)).sum(axis=axis, keepdims=True) == x.shape[axis]
+    res = scipy.stats.stats.mode(x, axis)
+    return scipy.stats.stats.ModeResult(np.where(nans, np.nan, res.mode),
+                                        np.where(nans, np.nan, res.count))
 
 
 def unique(x, return_counts=False):
