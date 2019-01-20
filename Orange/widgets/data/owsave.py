17efb735a1718c24d566cf374b1ee07f90a6ddbb
janezd <janez.demsar@fri.uni-lj.si>
2019-01-21 00:00:28 +0100

    OWSave: Improve GUI more

--- Orange/widgets/data/owsave.py
+++ Orange/widgets/data/owsave.py
@@ -1,20 +1,14 @@
 import os.path
 
+from AnyQt.QtWidgets import QFileDialog
 from AnyQt.QtCore import Qt
-from AnyQt.QtGui import QBrush
-from AnyQt.QtWidgets import QGridLayout
 
 from Orange.data.table import Table
 from Orange.data.io import TabReader, CSVReader, PickleReader, ExcelReader
 from Orange.widgets import gui, widget
 from Orange.widgets.settings import Setting
-from Orange.widgets.utils import filedialogs
 from Orange.widgets.utils.widgetpreview import WidgetPreview
 from Orange.widgets.widget import Input
-
-
-FILE_WRITERS = (TabReader, CSVReader, PickleReader, ExcelReader)
-FILE_FORMATS = [f"{w.DESCRIPTION} ({w.EXTENSIONS[0]})" for w in FILE_WRITERS]
 
 
 class OWSave(widget.OWWidget):
@@ -24,53 +18,60 @@ class OWSave(widget.OWWidget):
     category = "Data"
     keywords = []
 
+    settings_version = 2
+
+    writers = [TabReader, CSVReader, PickleReader, ExcelReader]
+    filters = [f"{w.DESCRIPTION} ({w.EXTENSIONS[0]})" for w in writers]
+    filt_ext = {filter: w.EXTENSIONS[0] for filter, w in zip(filters, writers)}
+    userhome = os.path.expanduser(f"~{os.sep}")
+
     class Inputs:
         data = Input("Data", Table)
 
     class Error(widget.OWWidget.Error):
-        unsupported_sparse = \
-            widget.Msg("This file format does not support sparse data.")
+        # This message is short to (almost) fit into the widget's width
+        unsupported_sparse = widget.Msg("Format can't store sparse data.")
+        general_error = widget.Msg("{}")
+
+    class Warning(widget.OWWidget.Warning):
+        no_file_name = widget.Msg("Set the file name.")
         general_error = widget.Msg("{}")
 
     want_main_area = False
     resizing_enabled = False
 
+    compress: bool
+    add_type_annotations: bool
+
     filetype = Setting(0)
     last_dir = Setting("")
-    compression = Setting(False)
+    filter = Setting(filters[0])
+    compress = Setting(False)
     add_type_annotations = Setting(True)
     auto_save = Setting(False)
 
     def __init__(self):
         super().__init__()
         self.data = None
-        self.basename = ""
-        self.dirty = False
+        self.filename = ""
+        self.writer = self.writers[0]
 
-        box = gui.vBox(self.controlArea, "Format")
-        gui.comboBox(
-            box, self, "filetype", items=FILE_FORMATS,
-            callback=self._set_dirty, addSpace=2)
-        box2 = gui.indentedBox(box, sep=10)
+        box = gui.vBox(self.controlArea, True)
+        box.layout().setSpacing(8)
+        self.lb_filename = gui.widgetLabel(box)
         gui.checkBox(
-            box2, self, "compression", "Compress file (as gzip)",
-            callback=self._set_dirty, addSpace=2)
+            box, self, "add_type_annotations", "Save with type annotations")
         gui.checkBox(
-            box2, self, "add_type_annotations", label="Add type annotations",
-            callback=self._set_dirty)
+            box, self, "compress", "Compress file (gzip)")
+        self.bt_set_file = gui.button(
+            None, self, "Set File Name", callback=self.set_file_name)
+        box.layout().addWidget(self.bt_set_file, Qt.AlignRight)
 
-        grid = QGridLayout()
-        gui.widgetBox(self.controlArea, box=True, orientation=grid)
-        self.save = gui.button(None, self, "Save", callback=self.save_file)
-        self.save_as = gui.button(
-            None, self, "Save as ...", callback=self.save_file_as)
-        grid.addWidget(self.save, 0, 0)
-        grid.addWidget(self.save_as, 0, 1)
-        grid.setRowMinimumHeight(1, 8)
-        grid.addWidget(
-            gui.checkBox(
-                None, self, "auto_save", "Autosave when receiving new data"),
-            2, 0, 2, 2)
+        box = gui.vBox(self.controlArea, box=True)
+        box.layout().setSpacing(8)
+        gui.checkBox(
+            box, self, "auto_save", "Autosave when receiving new data")
+        self.bt_save = gui.button(box, self, "Save", callback=self.save_file)
         self.adjustSize()
         self._update_controls()
 
@@ -78,95 +79,95 @@ class OWSave(widget.OWWidget):
     def dataset(self, data):
         self.Error.clear()
         self.data = data
-        self.dirty = data is not None
-        self._update_combo_colors()
-        if self.auto_save and self.basename:
+        if self.auto_save and self.filename:
             self.save_file()
-        self._update_controls()
 
-    def save_file_as(self):
-        start_path = self.basename \
-            or os.path.join(self.last_dir or os.path.expanduser("~"),
-                            getattr(self.data, 'name', ''))
-        filename, _, _ = filedialogs.open_filename_dialog_save(
-            start_path, '', [FILE_WRITERS[self.filetype]])
-        if not filename:
+        self._update_controls()
+        if self.data is None:
+            self.info.set_input_summary(self.info.NoInput)
+        else:
+            self.info.set_input_summary(
+                str(len(self.data)),
+                f"Data set {self.data.name or '(no name)'} "
+                f"with {len(self.data)} instances")
+
+    def set_file_name(self):
+        if self.filename:
+            start_dir = self.filename
+        else:
+            data_name = getattr(self.data, 'name', '')
+            if data_name:
+                data_name += self.filt_ext[self.filter]
+            start_dir = os.path.join(self.last_dir or self.userhome, data_name)
+
+        dlg = QFileDialog(None, "Set File", start_dir, ";;".join(self.filters))
+        dlg.setLabelText(dlg.Accept, "Select")
+        dlg.setAcceptMode(dlg.AcceptSave)
+        dlg.setSupportedSchemes(["file"])
+        dlg.selectNameFilter(self.filter)
+        if dlg.exec() == dlg.Rejected:
             return
 
-        self.last_dir, filename = os.path.split(filename)
-        self.basename, _ = os.path.splitext(filename)
-        self.save_file()
+        self.filename = dlg.selectedFiles()[0]
+        self.last_dir = os.path.split(self.filename)[0]
+        self.filter = dlg.selectedNameFilter()
+        self.writer = self.writers[self.filters.index(self.filter)]
         self._update_controls()
 
     def save_file(self):
-        writer = FILE_WRITERS[self.filetype]
-        if self.data is None \
-                or self.data.is_sparse() and not writer.SUPPORT_SPARSE_DATA:
+        self.Error.general_error.clear()
+        if not self._can_save():
             return
-        if not self.basename:
-            self.save_file_as()
-            return
+        name = self.filename \
+            + ".gz" * self.writer.SUPPORT_COMPRESSED * self.compress
         try:
-            filename = os.path.join(self.last_dir, self._fullname)
-            writer.write(filename, self.data, self.add_type_annotations)
-        except Exception as err_value:
+            self.writer.write(name, self.data, self.add_type_annotations)
+        except IOError as err_value:
             self.Error.general_error(str(err_value))
-        else:
-            self.Error.general_error.clear()
-            self.dirty = False
-            self._update_controls()
-
-    @property
-    def _fullname(self):
-        writer = FILE_WRITERS[self.filetype]
-        return self.basename \
-            + writer.EXTENSIONS[0] \
-            + ".gzip" * (self.compression and writer.SUPPORT_COMPRESSED)
-
-    def _update_combo_colors(self):
-        sparse_data = self.data is not None and self.data.is_sparse()
-        combo = self.controls.filetype
-        brushes = QBrush(Qt.gray), QBrush(Qt.black)
-        for i, writer in enumerate(FILE_WRITERS):
-            combo.setItemData(
-                i, brushes[not sparse_data or writer.SUPPORT_SPARSE_DATA],
-                Qt.TextColorRole)
-
-    def _set_dirty(self):
-        self.dirty = True
-        self._update_controls()
 
     def _update_controls(self):
-        ctrl = self.controls
-        writer = FILE_WRITERS[self.filetype]
+        has_file = bool(self.filename)
+        self.lb_filename.setVisible(has_file)
+        self.Warning.no_file_name(shown=not has_file)
+        if self.filename:
+            name = self.filename
+            if name.startswith(self.userhome):
+                name = name[len(self.userhome):]
+            self.lb_filename.setText(f"Save to: {name}")
+
+        self.controls.add_type_annotations.setVisible(
+            has_file and self.writer.OPTIONAL_TYPE_ANNOTATIONS)
+        self.controls.compress.setVisible(
+            has_file and self.writer.SUPPORT_COMPRESSED)
 
         self.Error.unsupported_sparse(
-            shown=self.data is not None
-                  and self.data.is_sparse()
-                  and not writer.SUPPORT_SPARSE_DATA)
-        ctrl.add_type_annotations.setEnabled(writer.OPTIONAL_TYPE_ANNOTATIONS)
-        ctrl.compression.setEnabled(writer.SUPPORT_COMPRESSED)
+            shown=self.data is not None and self.data.is_sparse()
+            and self.filename
+            and not self.writer.SUPPORT_SPARSE_DATA)
+        self.bt_save.setEnabled(self._can_save())
 
-        self.save.setText(
-            f"Save as '{self._fullname}'" if self.basename else "Save")
-        enable_save = self.data is not None \
-            and (not self.data.is_sparse() or writer.SUPPORT_SPARSE_DATA)
-        self.save.setVisible(self.dirty and bool(self.basename))
-        self.save.setEnabled(enable_save)
-        self.save_as.setEnabled(enable_save)
-        ctrl.auto_save.setEnabled(enable_save and bool(self.basename))
+    def _can_save(self):
+        return self.data is not None \
+            and bool(self.filename) \
+            and (not self.data.is_sparse() or self.writer.SUPPORT_SPARSE_DATA)
+
+    def send_report(self):
+        self.report_data_brief(self.data)
+        writer = self.writer
+        noyes = ["No", "Yes"]
+        self.report_items((
+            ("File name", self.filename or "not set"),
+            ("Format", writer.DESCRIPTION),
+            ("Compression", writer.SUPPORT_COMPRESSED and noyes[self.compress]),
+            ("Type annotations",
+             writer.OPTIONAL_TYPE_ANNOTATIONS
+             and noyes[self.add_type_annotations])
+        ))
 
     @classmethod
     def migrate_settings(cls, settings, version=0):
-        return
         if version < 2:
-            for i, (compr, _) in enumerate(COMPRESSIONS):
-                if compr == settings["compression"]:
-                    settings["compression"] = i
-                    break
-            else:
-                settings["compression"] = 0
-            settings["filetype"] = FILE_FORMATS.index(settings["filetype"])
+            settings["filter"] = settings.pop("filetype")
 
 
 if __name__ == "__main__":  # pragma: no cover
