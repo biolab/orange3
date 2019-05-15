a3f6029330109e3efaefbbe0aa2de56c17bf31a7
Vesna Tanko <tankovesna@hotmail.com>
2019-05-15 09:07:05 +0200

    Apply domain: Remove 'Retain all data' option

--- Orange/widgets/data/owtransform.py
+++ Orange/widgets/data/owtransform.py
@@ -1,11 +1,8 @@
 from typing import Optional
 
-import numpy as np
-
 from Orange.data import Table, Domain
 from Orange.widgets import gui
 from Orange.widgets.report.report import describe_data
-from Orange.widgets.settings import Setting
 from Orange.widgets.utils.sql import check_sql_input
 from Orange.widgets.utils.widgetpreview import WidgetPreview
 from Orange.widgets.widget import OWWidget, Input, Output, Msg
@@ -17,8 +14,6 @@ class OWTransform(OWWidget):
     icon = "icons/Transform.svg"
     priority = 2110
     keywords = ["transform"]
-
-    retain_all_data = Setting(False)
 
     class Inputs:
         data = Input("Data", Table, default=True)
@@ -45,10 +40,6 @@ class OWTransform(OWWidget):
         self.output_label = gui.widgetLabel(info_box, "")
         self.set_input_label_text()
         self.set_template_label_text()
-
-        box = gui.widgetBox(self.controlArea, "Output")
-        gui.checkBox(box, self, "retain_all_data", "Retain all data",
-                     callback=self.apply)
 
     def set_input_label_text(self):
         text = "No data on input."
@@ -98,22 +89,10 @@ class OWTransform(OWWidget):
                 self.Error.error(ex)
 
         data = transformed_data
-        if data and self.retain_all_data:
-            data = self.merged_data(data)
         self.transformed_info = describe_data(data)
         self.Outputs.transformed_data.send(data)
         self.set_template_label_text()
         self.set_output_label_text(data)
-
-    def merged_data(self, t_data):
-        domain = self.data.domain
-        t_domain = t_data.domain
-        metas = domain.metas + t_domain.attributes + t_domain.metas
-        domain = Domain(domain.attributes, domain.class_vars, metas)
-        data = self.data.transform(domain)
-        metas = np.hstack((t_data.X, t_data.metas))
-        data.metas[:, -metas.shape[1]:] = metas
-        return data
 
     def send_report(self):
         if self.data:
