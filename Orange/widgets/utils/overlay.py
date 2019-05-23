93cdee3906dc73ee6498bbb569bb697cd749b972
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2019-05-23 14:42:03 +0200

    utils/overlay: Import proxydoc for backward compatibility

--- Orange/widgets/utils/overlay.py
+++ Orange/widgets/utils/overlay.py
@@ -6,7 +6,9 @@ A Widget to display a temporary dismissible message over another widget.
 
 """
 from orangewidget.utils.overlay import (
-    OverlayWidget, MessageWidget, MessageOverlayWidget
+    OverlayWidget, MessageWidget, MessageOverlayWidget,
+    # pylint: disable=unused-import
+    proxydoc
 )
 
 __all__ = [
