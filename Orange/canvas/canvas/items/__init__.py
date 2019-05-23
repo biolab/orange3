27c629e77b59d2cf0e6eda04c741ac35bf98467a
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2019-05-23 16:04:10 +0200

    Remove canvas package

--- Orange/canvas/canvas/items/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Orange Canvas Graphics Items
-
-"""
-
-from .nodeitem import NodeItem, NodeAnchorItem, NodeBodyItem, SHADOW_COLOR
-from .nodeitem import SourceAnchorItem, SinkAnchorItem, AnchorPoint
-from .linkitem import LinkItem, LinkCurveItem
-from .annotationitem import TextAnnotation, ArrowAnnotation
