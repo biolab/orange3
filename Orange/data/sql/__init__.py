f5cfaefca4338eb801255abc03d3e446d3d57a12
Marcelo Viana <mviana@users.noreply.github.com>
2019-07-31 10:38:29 -0300

    Update __init__.py
    Restore to original (uploaded in wrong directory)

--- Orange/data/sql/__init__.py
+++ Orange/data/sql/__init__.py
@@ -1,16 +1,1 @@
-from .base import Backend
 
-try:
-    from .postgres import Psycopg2Backend
-except ImportError:
-    pass
-
-try:
-    from .mssql import PymssqlBackend
-except ImportError:
-    pass
-
-try:
-    from .mysql import PymysqlBackend
-except ImportError:
-    pass
