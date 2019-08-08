59636f8220a0a0f1793038483d16216f7ac7ca41
Brylie Christopher Oxley <brylie@gnumedia.org>
2019-08-08 16:20:23 +0300

    Improve formatting (using black)

--- doc/development/source/orange-demo/orangedemo/OWDataSamplerA.py
+++ doc/development/source/orange-demo/orangedemo/OWDataSamplerA.py
@@ -1,10 +1,10 @@
-# [start-snippet-1]
 import numpy
 
 import Orange.data
 from Orange.widgets.widget import OWWidget, Input, Output
 from Orange.widgets.utils.widgetpreview import WidgetPreview
 from Orange.widgets import gui
+
 
 class OWDataSamplerA(OWWidget):
     name = "Data Sampler"
@@ -25,28 +25,7 @@ class OWDataSamplerA(OWWidget):
 
         # GUI
         box = gui.widgetBox(self.controlArea, "Info")
-        self.infoa = gui.widgetLabel(box, 'No data on input yet, waiting to get something.')
-        self.infob = gui.widgetLabel(box, '')
-# [end-snippet-1]
-
-# [start-snippet-2]
-    @Inputs.data
-    def set_data(self, dataset):
-        if dataset is not None:
-            self.infoa.setText('%d instances in input dataset' % len(dataset))
-            indices = numpy.random.permutation(len(dataset))
-            indices = indices[:int(numpy.ceil(len(dataset) * 0.1))]
-            sample = dataset[indices]
-            self.infob.setText('%d sampled instances' % len(sample))
-            self.Outputs.sample.send(sample)
-        else:
-            self.infoa.setText('No data on input yet, waiting to get something.')
-            self.infob.setText('')
-            self.Outputs.sample.send("Sampled Data")
-# [end-snippet-2]
-
-
-# [start-snippet-3]
-if __name__ == "__main__":
-    WidgetPreview(OWDataSamplerA).run(Orange.data.Table("iris"))
-# [end-snippet-3]
+        self.infoa = gui.widgetLabel(
+            box, "No data on input yet, waiting to get something."
+        )
+        self.infob = gui.widgetLabel(box, "")
