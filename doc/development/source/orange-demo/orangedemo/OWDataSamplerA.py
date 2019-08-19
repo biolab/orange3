871d7d04db3067b3625c0305750578af00e45e9d
Brylie Christopher Oxley <brylie@amble.fi>
2019-08-19 13:38:02 +0300

    Restore inadvertent deletions

--- doc/development/source/orange-demo/orangedemo/OWDataSamplerA.py
+++ doc/development/source/orange-demo/orangedemo/OWDataSamplerA.py
@@ -1,22 +1,19 @@
+# [start-snippet-1]
+import sys
 import numpy
 
 import Orange.data
-from Orange.widgets.widget import OWWidget, Input, Output
-from Orange.widgets.utils.widgetpreview import WidgetPreview
-from Orange.widgets import gui
+from Orange.widgets import widget, gui
 
 
-class OWDataSamplerA(OWWidget):
+class OWDataSamplerA(widget.OWWidget):
     name = "Data Sampler"
-    description = "Randomly selects a subset of instances from the dataset"
+    description = "Randomly selects a subset of instances from the data set"
     icon = "icons/DataSamplerA.svg"
     priority = 10
 
-    class Inputs:
-        data = Input("Data", Orange.data.Table)
-
-    class Outputs:
-        sample = Output("Sampled Data", Orange.data.Table)
+    inputs = [("Data", Orange.data.Table, "set_data")]
+    outputs = [("Sampled Data", Orange.data.Table)]
 
     want_main_area = False
 
@@ -26,6 +23,52 @@ class OWDataSamplerA(OWWidget):
         # GUI
         box = gui.widgetBox(self.controlArea, "Info")
         self.infoa = gui.widgetLabel(
-            box, "No data on input yet, waiting to get something."
-        )
-        self.infob = gui.widgetLabel(box, "")
+            box, 'No data on input yet, waiting to get something.')
+        self.infob = gui.widgetLabel(box, '')
+# [end-snippet-1]
+
+# [start-snippet-2]
+    def set_data(self, dataset):
+        if dataset is not None:
+            self.infoa.setText('%d instances in input data set' % len(dataset))
+            indices = numpy.random.permutation(len(dataset))
+            indices = indices[:int(numpy.ceil(len(dataset) * 0.1))]
+            sample = dataset[indices]
+            self.infob.setText('%d sampled instances' % len(sample))
+            self.send("Sampled Data", sample)
+        else:
+            self.infoa.setText(
+                'No data on input yet, waiting to get something.')
+            self.infob.setText('')
+            self.send("Sampled Data", None)
+# [end-snippet-2]
+
+# [start-snippet-3]
+
+
+def main(argv=sys.argv):
+    from PyQt4.QtGui import QApplication
+    app = QApplication(list(argv))
+    args = app.argv()
+    if len(argv) > 1:
+        filename = argv[1]
+    else:
+        filename = "iris"
+
+    ow = OWDataSamplerA()
+    ow.show()
+    ow.raise_()
+
+    dataset = Orange.data.Table(filename)
+    ow.set_data(dataset)
+    ow.handleNewSignals()
+    app.exec_()
+    ow.set_data(None)
+    ow.handleNewSignals()
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+# [end-snippet-3]
