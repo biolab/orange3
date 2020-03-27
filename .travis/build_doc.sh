ec0361db2db412111ce39fc06ea4991cb80d160e
Marko Toplak <marko.toplak@gmail.com>
2020-03-27 12:35:44 +0100

    tox add

--- .travis/build_doc.sh
+++ .travis/build_doc.sh
@@ -11,6 +11,9 @@ images="$(git diff --name-only origin/master..HEAD |
 echo "Checking if images are indexed:"
 while read image; do
     [ -f "$image" ] || continue
+    if  [[ "$image" == *"_unindexed"* ]]; then
+      continue
+    fi
     imtype=$(identify -verbose "$image" | awk '/^ *Type: /{ print $2 }')
     echo "$image  $imtype"
     if ! echo "$imtype" | grep -Eq '(Palette|Grayscale)'; then
