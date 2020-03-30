62612dfa557ab824f929a1d1e811795ab6e4c11b
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2020-03-30 13:17:47 +0200

    GradientLegendWidget: Add setRange setter

--- Orange/widgets/visualize/utils/heatmap.py
+++ Orange/widgets/visualize/utils/heatmap.py
@@ -1159,6 +1159,12 @@ class GradientLegendWidget(QGraphicsWidget):
         if parent is not None:
             self.setParentItem(parent)
 
+    def setRange(self, low, high):
+        if self.low != low or self.high != high:
+            self.low = low
+            self.high = high
+            self.__update()
+
     def setColorMap(self, colormap: ColorMap) -> None:
         """Set the color map"""
         self.colormap = colormap
@@ -1180,6 +1186,7 @@ class GradientLegendWidget(QGraphicsWidget):
             tick_values = [low, high]
         tickformat = "{:.6g}".format
         ticks = [(val, tickformat(val)) for val in tick_values]
+        self.__axis.setRange(low, high)
         self.__axis.setTicks([ticks])
 
         self.updateGeometry()
