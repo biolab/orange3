f597dd85d9ad728d169335c65f03c865f4da7991
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2020-05-05 16:22:51 +0200

    owheatmap: Handle all N/A column for color annotation

--- Orange/widgets/visualize/owheatmap.py
+++ Orange/widgets/visualize/owheatmap.py
@@ -990,12 +990,14 @@ class OWHeatMap(widget.OWWidget):
         if var is None:
             return None
         column_data = column_data_from_table(self.input_data, var)
-        span = (np.nanmin(column_data), np.nanmax(column_data))
         merges = self._merge_row_indices()
         if merges is not None:
             column_data = aggregate(var, column_data, merges)
         data, colormap = self._colorize(var, column_data)
         if var.is_continuous:
+            span = (np.nanmin(column_data), np.nanmax(column_data))
+            if np.any(np.isnan(span)):
+                span = 0., 1.
             colormap.span = span
         return data, colormap, var
 
