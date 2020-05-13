0ec2c80b80ddbef7b066510033876f060c1bb32c
Andreja Kovacic <kovacic.andreja@gmail.com>
2020-05-13 13:13:31 +0200

    Owmanifoldlearning: rename new vars if necessary

--- Orange/widgets/tests/utils.py
+++ Orange/widgets/tests/utils.py
@@ -348,16 +348,21 @@ def table_dense_sparse(test_case):
     return _wrapper
 
 
-def possible_duplicate_table(name, table_name='iris'):
+def possible_duplicate_table(name, table_name='iris', class_var=False):
     """
     Used for checking whether widget resolves possible domain duplicates.
     If the programmer inputs name that will create duplicates and it later fails,
     that's on them.
     """
     data = Table(table_name)
-    new_attributes = list(data.domain.attributes[1:])
-    new_attributes.append(ContinuousVariable(name))
-    domain = Domain(new_attributes,
-                    data.domain.class_vars,
+    attributes = data.domain.attributes
+    class_vars = list(data.domain.class_vars)
+    if class_var:
+        class_vars[0] = ContinuousVariable(name)
+    else:
+        attributes = list(data.domain.attributes[1:])
+        attributes.append(ContinuousVariable(name))
+    domain = Domain(attributes,
+                    class_vars,
                     data.domain.metas)
     return Table.from_numpy(domain, data.X, data.Y, data.metas)
