2136d92e5e339e0606b1babe7afc30f8efd7966f
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2020-06-03 13:30:16 +0200

    combobox: Add an ItemStyledComboBox utility

--- Orange/widgets/utils/combobox.py
+++ Orange/widgets/utils/combobox.py
@@ -1,5 +1,39 @@
+from AnyQt.QtCore import Qt
+from AnyQt.QtGui import QBrush, QColor, QPalette, QPen, QFont, QFontMetrics
+from AnyQt.QtWidgets import QStylePainter, QStyleOptionComboBox, QStyle
+
 from orangewidget.utils.combobox import ComboBoxSearch, ComboBox
 
 __all__ = [
-    "ComboBoxSearch", "ComboBox"
+    "ComboBoxSearch", "ComboBox", "ItemStyledComboBox"
 ]
+
+
+class ItemStyledComboBox(ComboBox):
+    """
+    A QComboBox that draws its text using current item's foreground and font
+    role.
+
+    Note
+    ----
+    Stylesheets etc. can completely ignore this.
+    """
+    def paintEvent(self, _event) -> None:
+        painter = QStylePainter(self)
+        option = QStyleOptionComboBox()
+        self.initStyleOption(option)
+        painter.drawComplexControl(QStyle.CC_ComboBox, option)
+        foreground = self.currentData(Qt.ForegroundRole)
+        if isinstance(foreground, (QBrush, QColor)):
+            foreground = QBrush(foreground)
+            if foreground.style() != Qt.NoBrush:
+                # some styles take WindowText some just use current pen?
+                option.palette.setBrush(QPalette.WindowText, foreground)
+                option.palette.setBrush(QPalette.ButtonText, foreground)
+                option.palette.setBrush(QPalette.Text, foreground)
+                painter.setPen(QPen(foreground, painter.pen().widthF()))
+        font = self.currentData(Qt.FontRole)
+        if isinstance(font, QFont):
+            option.fontMetrics = QFontMetrics(font)
+            painter.setFont(font)
+        painter.drawControl(QStyle.CE_ComboBoxLabel, option)
