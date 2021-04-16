25531e2288e500b5c038f160ad0888373b21ae5c
Ajda Pretnar <ajdapretnar@gmail.com>
2021-04-16 15:00:15 +0200

    [DOC] Small fixes

--- Orange/widgets/data/owmelt.py
+++ Orange/widgets/data/owmelt.py
@@ -60,7 +60,7 @@ class MeltContextHandler(ContextHandler):
 class OWMelt(widget.OWWidget):
     name = "Melt"
     description = "Convert wide data to narrow data, a list of item-value pairs"
-    icon = "icons/ToShoppingList.svg"
+    icon = "icons/Melt.svg"
     keywords = ["shopping list", "wide", "narrow"]
 
     class Inputs:
