b86c9c1b3d1650c768de09989d4e777c06007410
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2021-05-06 14:15:45 +0200

    benchmark: Add TableModel/View benchmark

---
+++ benchmark/bench_datadelegate.py
@@ -0,0 +1,41 @@
+from AnyQt.QtWidgets import QStyledItemDelegate
+
+from orangewidget.tests.base import GuiTest
+
+from Orange.data import Table
+from Orange.widgets.data.owtable import RichTableModel
+from Orange.widgets.utils.tableview import TableView
+
+from .base import benchmark, Benchmark
+
+
+class BenchDataDelegate(GuiTest, Benchmark):
+    def setUp(self) -> None:
+        super().setUp()
+        data = Table("brown-selected")
+        self.view = TableView()
+        self.delegate = QStyledItemDelegate()
+        self.view.setItemDelegate(self.delegate)
+        self.model = RichTableModel(data)
+        self.view.setModel(self.model)
+        self.view.resize(1024, 760)
+        self.option = self.view.viewOptions()
+        self.index_00 = self.model.index(0, 0)
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.view.setModel(None)
+        del self.view
+        del self.model
+        del self.delegate
+        del self.option
+        del self.index_00
+
+    @benchmark(number=3, warmup=1, repeat=10)
+    def bench_paint(self):
+        _ = self.view.grab()
+
+    @benchmark(number=3, warmup=1, repeat=10)
+    def bench_init_style_option(self):
+        self.delegate.initStyleOption(self.option, self.index_00)
+
