caeed29d20c8329b631792cc923b187cda9c795d
Andrii Oriekhov <andriyorehov@gmail.com>
2022-03-05 12:28:43 +0200

    add GitHub URL for PyPi

--- setup.py
+++ setup.py
@@ -509,6 +509,7 @@ def setup_package():
         author=AUTHOR,
         author_email=AUTHOR_EMAIL,
         url=URL,
+        project_urls=PROJECT_URLS,
         license=LICENSE,
         keywords=KEYWORDS,
         classifiers=CLASSIFIERS,
