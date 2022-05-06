d25d38e01b0f3ac626f7961cf248aa0c1967b260
janezd <janez.demsar@fri.uni-lj.si>
2022-05-06 21:38:05 +0200

    Silhouette: Abandon PerfectDomainContextHandler, use vars instead of indices

--- Orange/widgets/visualize/tests/test_owsilhouetteplot.py
+++ Orange/widgets/visualize/tests/test_owsilhouetteplot.py
@@ -3,8 +3,11 @@
 # pylint: disable=missing-docstring
 import random
 import unittest
+from unittest.mock import Mock
 
 import numpy as np
+
+from orangewidget.settings import Context
 
 import Orange.distance
 from Orange.data import (
@@ -225,6 +228,69 @@ class TestOWSilhouettePlot(WidgetTest, WidgetOutputsTestMixin):
 
         output = self.get_output(self.widget.Outputs.annotated_data)
         self.assertEqual(output.domain.metas[0].name, 'Silhouette (iris) (1)')
+
+    def test_report(self):
+        widget = self.widget
+        widget.report_plot = Mock()
+        widget.report_caption = Mock()
+
+        widget.send_report()
+        widget.report_plot.assert_not_called()
+        widget.report_caption.assert_not_called()
+
+        data = Table("zoo")
+        self.send_signal(widget.Inputs.data, data)
+
+        widget.annotation_var = None
+        widget.send_report()
+        widget.report_plot.assert_called()
+        widget.report_caption.assert_called()
+        text = widget.report_caption.call_args[0][0]
+        self.assertIn(data.domain.class_var.name, text)
+        self.assertNotIn("nnotated", text)
+
+        widget.annotation_var = data.domain.metas[0]
+        widget._silplot.rowNamesVisible = lambda: True
+        widget.send_report()
+        text = widget.report_caption.call_args[0][0]
+        self.assertIn(data.domain.class_var.name, text)
+        self.assertIn("nnotated", text)
+        self.assertIn(data.domain.metas[0].name, text)
+
+    def test_migration(self):
+        enc_domain = dict(
+            attributes=(('foo', 1), ('bar', 1), ('baz', 2), ('bax', 1)),
+            class_vars=(('cfoo', 1), ),
+            metas=(('mbar', 3), ('mbaz', 1)))
+
+        context = Context(
+            values=dict(cluster_var_idx=(0, -2), annotation_var_idx=(0, -2)),
+            **enc_domain
+        )
+        OWSilhouettePlot.migrate_context(context, 1)
+        values = context.values
+        self.assertNotIn("cluster_var_idx", values)
+        self.assertNotIn("annotation_var_idx", values)
+        self.assertEqual(values["cluster_var"], ("foo", 101))
+        self.assertEqual(values["annotation_var"], None)
+
+        context = Context(
+            values=dict(cluster_var_idx=(4, -2), annotation_var_idx=(4, -2)),
+            **enc_domain
+        )
+        OWSilhouettePlot.migrate_context(context, 1)
+        self.assertNotIn("cluster_var_idx", values)
+        self.assertNotIn("annotation_var_idx", values)
+        self.assertEqual(context.values["cluster_var"], ("mbaz", 101))
+        self.assertEqual(context.values["annotation_var"], ("cfoo", 101))
+
+        context = Context(
+            values=dict(cluster_var_idx=(4, -2), annotation_var_idx=(5, -2)),
+            **enc_domain
+        )
+        OWSilhouettePlot.migrate_context(context, 1)
+        self.assertEqual(context.values["cluster_var"], ("mbaz", 101))
+        self.assertEqual(context.values["annotation_var"], ("mbar", 103))
 
 
 if __name__ == "__main__":
