15cba33dfd40bfbbb36000efa5e661418dcf6887
Marko Toplak <marko.toplak@gmail.com>
2022-05-16 13:40:29 +0200

    Rewrite table summaries to avoid bottleneck's anynan

--- Orange/widgets/data/owfile.py
+++ Orange/widgets/data/owfile.py
@@ -525,10 +525,8 @@ class OWFile(widget.OWWidget, RecentPathsWComboMixin):
 
         text += f"<p>{len(table)} {pl(len(table), 'instance')}"
 
-        missing_in_attr = missing_prop(table.has_missing_attribute()
-                                       and table.get_nan_frequency_attribute())
-        missing_in_class = missing_prop(table.has_missing_class()
-                                        and table.get_nan_frequency_class())
+        missing_in_attr = missing_prop(table.get_nan_frequency_attribute())
+        missing_in_class = missing_prop(table.get_nan_frequency_class())
         nattrs = len(domain.attributes)
         text += f"<br/>{nattrs} {pl(nattrs, 'feature')} {missing_in_attr}"
         if domain.has_continuous_class:
