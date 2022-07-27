a42ed63f3593c11a61efa6a79c3de7b1fa4156b2
Marko Toplak <marko.toplak@gmail.com>
2022-07-27 21:48:27 +0200

    Select by Data Index: tests for Dask

--- Orange/widgets/data/tests/test_owselectbydataindex.py
+++ Orange/widgets/data/tests/test_owselectbydataindex.py
@@ -1,12 +1,20 @@
 # pylint: disable=protected-access
 
 from Orange.data import Table, Domain
+from Orange.data.dask import DaskTable
+from Orange.tests.test_dasktable import temp_dasktable
 from Orange.widgets.data.owselectbydataindex import OWSelectByDataIndex
 from Orange.widgets.tests.base import WidgetTest
 from Orange.widgets.utils.annotated_data import ANNOTATED_DATA_FEATURE_NAME
 
 
 class TestOWSelectSubset(WidgetTest):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.iris = Table("iris")
+        cls.titanic = Table("titanic")
 
     def setUp(self):
         self.widget = self.create_widget(OWSelectByDataIndex)
@@ -17,27 +25,27 @@ class TestOWSelectSubset(WidgetTest):
         self.send_signal(self.widget.Inputs.data_subset, data_subset)
 
     def test_subset(self):
-        data = Table("iris")
+        data = self.iris
         self.apply_subset_20_40(data)
         out = self.get_output(self.widget.Outputs.matching_data)
         self.assertEqual(list(data[20:40]), list(out))
 
     def test_non_matching(self):
-        data = Table("iris")
+        data = self.iris
         self.apply_subset_20_40(data)
         out = self.get_output(self.widget.Outputs.non_matching_data)
         self.assertEqual(list(data[:20]) + list(data[40:]), list(out))
 
     def test_annotated(self):
-        data = Table("iris")
+        data = self.iris
         self.apply_subset_20_40(data)
         out = self.get_output(self.widget.Outputs.annotated_data)
         vals = [a[ANNOTATED_DATA_FEATURE_NAME].value for a in out]
         self.assertEqual(['No']*20 + ['Yes']*20 + ['No']*(len(data) - 40), vals)
 
     def test_subset_nosubset(self):
-        data = Table("iris")
-        data_subset = Table("titanic")
+        data = self.iris
+        data_subset = self.titanic
         self.send_signal(self.widget.Inputs.data, data)
         self.send_signal(self.widget.Inputs.data_subset, data_subset)
         matching = self.get_output(self.widget.Outputs.matching_data)
@@ -45,3 +53,20 @@ class TestOWSelectSubset(WidgetTest):
         self.assertTrue(self.widget.Warning.instances_not_matching.is_shown())
         self.assertEqual([], list(matching))
         self.assertEqual(list(data), list(non_matching))
+
+
+class TestOWSelectSubsetDask(TestOWSelectSubset):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.iris = temp_dasktable("iris")
+        cls.titanic = temp_dasktable("titanic")
+
+    def test_dask_outputs(self):
+        w = self.widget
+        self.apply_subset_20_40(self.iris)
+        m = self.get_output(w.Outputs.matching_data)
+        self.assertIsInstance(m, DaskTable)
+        n = self.get_output(w.Outputs.non_matching_data)
+        self.assertIsInstance(n, DaskTable)
+        self.assertEqual(len(self.iris), len(m) + len(n))
