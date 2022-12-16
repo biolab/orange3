3048a473b62051d2b3748f8657710d0d3270d41c
Marko Toplak <marko.toplak@gmail.com>
2022-12-16 10:05:09 +0100

    DaskTable.__getitem__ returns DaskRowInstances

--- Orange/data/dask.py
+++ Orange/data/dask.py
@@ -1,12 +1,28 @@
 import contextlib
 import pickle
+import warnings
+from numbers import Integral
 
 import h5py
 import dask.array as da
 import numpy as np
 import pandas
 
-from Orange.data import Table
+from Orange.data import Table, RowInstance
+
+
+class DaskRowInstance(RowInstance):
+
+    def __init__(self, table, row_index):
+        """
+        Construct a data instance representing the given row of the table.
+        """
+        warnings.warn("Creating Instances from DaskTables is inefficient", stacklevel=2)
+        super().__init__(table, row_index)
+        if isinstance(self._x, dask.array.Array):
+            self._x = self._x.compute()
+        if isinstance(self._y, dask.array.Array):
+            self._y = self._y.compute()
 
 
 class DaskTable(Table):
@@ -74,6 +90,12 @@ class DaskTable(Table):
 
     def set_weights(self, weight=1):
         raise NotImplementedError()
+
+    def __getitem__(self, key):
+        if isinstance(key, Integral):
+            return DaskRowInstance(self, key)
+        else:
+            return super().__getitem__(key)
 
     def save(self, filename):
         # the following code works with both Table and DaskTable
