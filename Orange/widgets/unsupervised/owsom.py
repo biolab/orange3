0cb2c5e6dcd75cd3c2413fe21be54a6386c79beb
janezd <janez.demsar@fri.uni-lj.si>
2023-01-05 17:56:14 +0100

    owsom: Plural form-friendly warnings

--- Orange/widgets/unsupervised/owsom.py
+++ Orange/widgets/unsupervised/owsom.py
@@ -20,6 +20,7 @@ from Orange.preprocess import decimal_binnings, time_binnings
 from Orange.projection.som import SOM
 
 from Orange.widgets import gui
+from Orange.widgets.utils.localization import pl
 from Orange.widgets.widget import OWWidget, Msg, Input, Output
 from Orange.widgets.settings import \
     DomainContextHandler, ContextSetting, Setting
@@ -214,8 +215,7 @@ class OWSOM(OWWidget):
             Msg("Some data instances have undefined value of '{}'.")
         no_defined_colors = \
             Msg("'{}' has no defined values.")
-        missing_values = \
-            Msg("{} data instance{} with undefined value(s) {} not shown.")
+        missing_values = Msg("{}")
         single_attribute = Msg("Data contains a single numeric column.")
 
     class Error(OWWidget.Error):
@@ -334,10 +334,9 @@ class OWSOM(OWWidget):
 
         def set_warnings():
             missing = len(data) - len(self.data)
-            if missing == 1:
-                self.Warning.missing_values(1, "", "is")
-            elif missing > 1:
-                self.Warning.missing_values(missing, "s", "are")
+            if missing:
+                self.Warning.missing_values(
+                    f'{missing} data {pl(missing, "instance")} with undefined value(s) {pl(missing, "is|are")} not shown.')
 
         cont_x = self.cont_x.copy() if self.cont_x is not None else None
         self.data = self.cont_x = None
