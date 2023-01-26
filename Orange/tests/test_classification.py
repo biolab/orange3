d67cc598d6b1948363202f631a94700423956dd2
Marko Toplak <marko.toplak@gmail.com>
2023-01-26 16:10:20 +0100

    Test unpickling learners after PCA

--- Orange/tests/test_classification.py
+++ Orange/tests/test_classification.py
@@ -432,6 +432,31 @@ class LearnerAccessibility(unittest.TestCase):
                         err_msg='%s does not return same values when unpickled %s'
                         % (learner.__class__.__name__, ds.name))
 
+    def test_all_models_work_after_unpickling_pca(self):
+        datasets = [Table('iris'), Table('titanic')]
+        for learner in list(all_learners()):
+            # calibration, threshold learners' __init__ require arguments
+            if learner in (ThresholdLearner, CalibratedLearner):
+                continue
+            # Skip slow tests
+            if isinstance(learner, _RuleLearner):
+                continue
+            with self.subTest(learner.__name__):
+                learner = learner()
+                for ds in datasets:
+                    pca_ds = Orange.projection.PCA()(ds)(ds)
+                    model = learner(pca_ds)
+                    s = pickle.dumps(model, 0)
+                    model2 = pickle.loads(s)
+
+                    np.testing.assert_almost_equal(
+                        Table.from_table(model.domain, ds).X,
+                        Table.from_table(model2.domain, ds).X)
+                    np.testing.assert_almost_equal(
+                        model(ds), model2(ds),
+                        err_msg='%s does not return same values when unpickled %s'
+                                % (learner.__class__.__name__, ds.name))
+
     def test_adequacy_all_learners(self):
         for learner in all_learners():
             # calibration, threshold learners' __init__ requires arguments
