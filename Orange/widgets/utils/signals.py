84a486d9cbc87079ca39be67e249dafc8a024555
janezd <janez.demsar@fri.uni-lj.si>
2023-08-20 16:27:45 +0200

    Implement Orange.widget.utils.signals.lazy_table_transform

--- Orange/widgets/utils/signals.py
+++ Orange/widgets/utils/signals.py
@@ -1,7 +1,11 @@
+from typing import Optional
+
 from orangewidget.utils.signals import (
     Input, Output, Single, Multiple, Default, NonDefault, Explicit, Dynamic,
-    InputSignal, OutputSignal, WidgetSignalsMixin
+    InputSignal, OutputSignal, WidgetSignalsMixin, LazyValue
 )
+
+from Orange.data import Table, Domain
 
 __all__ = [
     "Input", "Output", "InputSignal", "OutputSignal",
@@ -12,3 +16,11 @@ __all__ = [
 
 class AttributeList(list):
     """Signal type for lists of attributes (variables)"""
+
+
+def lazy_table_transform(domain: Domain,
+                         data: Optional[Table]) -> LazyValue[Table]:
+    if data is None:
+        return None
+    return LazyValue[Table](lambda: data.transform(domain),
+                            domain=domain, length=len(data))
