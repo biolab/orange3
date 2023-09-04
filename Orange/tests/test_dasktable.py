b2e61a15b9c6ed98d5a5d52e8864e75fba14ab9c
Marko Toplak <marko.toplak@gmail.com>
2023-09-04 11:33:17 +0200

    push back instance warning

--- Orange/tests/test_dasktable.py
+++ Orange/tests/test_dasktable.py
@@ -214,7 +214,7 @@ class TableTestCase(unittest.TestCase):
 
     def test_remind_us_of_instance_warning(self):
         now = datetime.now()
-        if (now.year, now.month) >= (2023, 9):
+        if (now.year, now.month) >= (2024, 1):
             iris = temp_dasktable("iris")
             with self.assertWarns(expected_warning=UserWarning):
                 iris[1]
