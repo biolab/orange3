8e16a61775e8ea8ab5f7b9e249e71923160fc227
Jaka Kokošar <jaka.kokosar@gmail.com>
2023-09-29 10:10:14 +0200

    owscatterplotgraph: properly check if the array is empty

--- Orange/widgets/visualize/owscatterplotgraph.py
+++ Orange/widgets/visualize/owscatterplotgraph.py
@@ -662,7 +662,7 @@ class OWScatterPlotBase(gui.OWComponent, QObject):
                 if len(mask_new) < self.sample_size:
                     return
 
-                if not diff:
+                if diff.size == 0:
                     # do nothing
                     if len(intersect_sampled_points) == self.sample_size:
                         return
