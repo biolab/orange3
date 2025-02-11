92d177842c6a85da3f780f8236291440967e8ec1
ZanMervic <zan.mervic@gmail.com>
2025-02-11 23:09:13 +0100

    Added unittest to test ScoringSheet reproducibility

--- Orange/widgets/model/tests/test_owscoringsheet.py
+++ Orange/widgets/model/tests/test_owscoringsheet.py
@@ -1,25 +1,19 @@
 import unittest
+import numpy as np
 
 from orangewidget.tests.base import WidgetTest
 
 from Orange.data import Table
 from Orange.preprocess import Impute
 
-from Orange.classification.scoringsheet import ScoringSheetLearner
 from Orange.widgets.model.owscoringsheet import OWScoringSheet
 
 
 class TestOWScoringSheet(WidgetTest):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.heart = Table("heart_disease")
-        cls.housing = Table("housing")
-        cls.scoring_sheet_learner = ScoringSheetLearner(20, 5, 5, None)
-        cls.scoring_sheet_model = cls.scoring_sheet_learner(cls.heart)
-
     def setUp(self):
         self.widget = self.create_widget(OWScoringSheet)
+        self.heart = Table("heart_disease")
+        self.housing = Table("housing")
 
     def test_no_data_input(self):
         self.assertIsNotNone(self.get_output(self.widget.Outputs.learner))
@@ -70,6 +64,42 @@ class TestOWScoringSheet(WidgetTest):
             max(non_zero_coefficients, key=lambda x: abs(x)),
             self.widget.max_points_per_param,
         )
+
+    def test_model_reproducibility(self):
+        self.widget = self.create_widget(OWScoringSheet)
+        self.widget.num_attr_after_selection = 20
+        self.widget.num_decision_params = 7
+        self.widget.max_points_per_param = 8
+        self.widget.custom_features_checkbox = True
+        self.widget.num_input_features = 4
+
+        self.widget.apply()
+
+        self.send_signal(self.widget.Inputs.data, self.heart)
+        self.wait_until_finished()
+        model = self.get_output(self.widget.Outputs.model)
+
+        coefficients = np.array(
+            [
+                -8.0, 6.0, 0.0, 0.0, -3.0, 4.0, 0.0, -2.0, -1.0,
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -6.0, 0.0, 0.0, 0.0, 0.0,
+            ]
+        )
+        feature_names = [
+            "major vessels colored=< 1", "chest pain=asymptomatic", "gender=female",
+            "gender=male", "thal=normal", "thal=reversable defect", "rest SBP=125 - 150",
+            "chest pain=non-anginal", "major vessels colored=1 - 2", "major vessels colored=2 - 3",
+            "chest pain=atypical ang", "chest pain=typical ang", "rest SBP=150 - 175",
+            "rest ECG=left vent hypertrophy", "rest ECG=normal", "ST by exercise=< 2",
+            "rest SBP=100 - 125", "exerc ind ang=0", "exerc ind ang=1", "age=40 - 60",
+        ]
+        intercept = 7.0
+        multiplier = 3.4567159
+
+        np.testing.assert_equal(model.model.coefficients, coefficients)
+        self.assertEqual(model.model.featureNames, feature_names)
+        self.assertEqual(model.model.intercept, intercept)
+        self.assertAlmostEqual(model.model.multiplier, multiplier, places=5)
 
     def test_custom_number_input_features_information(self):
         self.widget.custom_features_checkbox = True
