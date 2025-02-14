8583dd2a3027d6ca621f4204cf4cb876ab3ae2d4
Vesna Tanko <tankovesna@hotmail.com>
2025-02-14 11:59:27 +0100

    Distributions: Always show bars when no fitted distributions

--- Orange/widgets/visualize/owdistributions.py
+++ Orange/widgets/visualize/owdistributions.py
@@ -698,7 +698,7 @@ class OWDistributions(OWWidget):
                 x0 + xoff, bar_width, 0,
                 [tot_freq if self.cumulative_distr else freq],
                 colors, stacked=False, expanded=False, tooltip=tooltip,
-                desc=desc, hidden=self.hide_bars)
+                desc=desc, hidden=self.hide_bars and self.fitted_distribution)
 
         if self.fitted_distribution:
             self._plot_approximations(
@@ -744,7 +744,7 @@ class OWDistributions(OWWidget):
                 x0 + xoff, bar_width, 0 if self.stacked_columns else 0.1,
                 plotfreqs,
                 gcolors, stacked=self.stacked_columns, expanded=self.show_probs,
-                hidden=self.hide_bars,
+                hidden=self.hide_bars and self.fitted_distribution,
                 tooltip=self._split_tooltip(
                     desc, np.sum(plotfreqs), total, gvalues, plotfreqs),
                 desc=desc)
