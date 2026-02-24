f44d748d7c8a2917a32a281eb4cf17d9cc0808a2
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2026-02-24 14:13:09 +0100

    Remove PyQt imports in top level Orange module

--- Orange/__init__.py
+++ Orange/__init__.py
@@ -19,24 +19,6 @@ datasets = _DatasetInfo()
 
 del mod_name
 
-# If Qt is available (GUI) and Qt5, install backport for PyQt4 imports
-try:
-    import AnyQt.importhooks
-except ImportError:
-    pass
-else:
-    if AnyQt.USED_API == "pyqt5":
-        # Make the chosen PyQt version pinned
-        from AnyQt.QtCore import QObject
-        del QObject
-
-        import pyqtgraph  # import pyqtgraph first so that it can detect Qt5
-        del pyqtgraph
-
-        AnyQt.importhooks.install_backport_hook('pyqt4')
-    del AnyQt
-
-
 # A hack that prevents segmentation fault with Nvidia drives on Linux if Qt's browser window
 # is shown (seen in https://github.com/spyder-ide/spyder/pull/7029/files)
 try:
