95326e0c891cf6a3ea12cd59451553eef3c4657e
Guillermo Molleda Jimena <gmolleda@us.es>
2026-07-12 16:02:46 +0200

    Add optional L2 instance normalization for Euclidean kNN

--- Orange/widgets/model/owknn.py
+++ Orange/widgets/model/owknn.py
@@ -1,12 +1,32 @@
 from AnyQt.QtCore import Qt
+from sklearn.preprocessing import normalize
 
 from Orange.data import Table
 from Orange.modelling import KNNLearner
+from Orange.preprocess import PreprocessorList
 from Orange.widgets import gui
 from Orange.widgets.settings import Setting
 from Orange.widgets.utils.owlearnerwidget import OWBaseLearner
 from Orange.widgets.utils.widgetpreview import WidgetPreview
 
+class NormalizeInstancesL2:
+    """Normalize each instance to unit L2 norm."""
+    _reprable_module = True
+
+    def __call__(self, data):
+        if data is None or data.X is None or data.X.shape[0] == 0:
+            return data
+
+        return Table.from_numpy(
+            data.domain,
+            normalize(data.X, norm="l2", axis=1),
+            Y=data.Y,
+            metas=data.metas,
+            W=data.W if data.has_weights() else None,
+        )
+
+    def __repr__(self):
+        return "Normalize instances (L2)"
 
 class OWKNNLearner(OWBaseLearner):
     name = "kNN"
@@ -30,6 +50,7 @@ class OWKNNLearner(OWBaseLearner):
     n_neighbors = Setting(5)
     metric_index = Setting(0)
     weight_index = Setting(0)
+    normalize_instances_l2 = Setting(False)
 
     def add_main_layout(self):
         # this is part of init, pylint: disable=attribute-defined-outside-init
@@ -41,23 +62,66 @@ class OWKNNLearner(OWBaseLearner):
         self.metrics_combo = gui.comboBox(
             box, self, "metric_index", orientation=Qt.Horizontal,
             label="Metric:", items=self.metrics_options,
+            callback=self._on_metric_changed)
+        self.normalize_l2_checkbox = gui.checkBox(
+            box, self, "normalize_instances_l2",
+            label="Normalize instances (L2)",
             callback=self.settings_changed)
+        self.normalize_l2_checkbox.setToolTip(
+            "Normalize each instance to unit L2 norm before learning. "
+            "Recommended for embeddings when using Euclidean distance.")
         self.weights_combo = gui.comboBox(
             box, self, "weight_index", orientation=Qt.Horizontal,
             label="Weight:", items=self.weights_options,
             callback=self.settings_changed)
+        self._update_l2_checkbox()
+
+    def _on_metric_changed(self):
+        self._update_l2_checkbox()
+        self.settings_changed()
+
+    def _update_l2_checkbox(self):
+        # false positive, pylint: disable=invalid-sequence-index
+        enabled = self.metrics[self.metric_index] == "euclidean"
+        self.normalize_l2_checkbox.setEnabled(enabled)
+        if not enabled and self.normalize_instances_l2:
+            self.normalize_instances_l2 = False
+            self.normalize_l2_checkbox.setChecked(False)
 
     def create_learner(self):
+        preprocessors = self.preprocessors
+
+        if self.normalize_instances_l2:
+            l2_normalizer = NormalizeInstancesL2()
+
+            if preprocessors is None:
+                preprocessors = l2_normalizer
+            elif isinstance(preprocessors, PreprocessorList):
+                preprocessors = PreprocessorList(
+                    preprocessors.preprocessors + [l2_normalizer]
+                )
+            else:
+                preprocessors = PreprocessorList(
+                    [preprocessors, l2_normalizer]
+                )
+
         return self.LEARNER(
             n_neighbors=self.n_neighbors,
+            # false positive, pylint: disable=invalid-sequence-index
             metric=self.metrics[self.metric_index],
             weights=self.weights[self.weight_index],
-            preprocessors=self.preprocessors)
+            preprocessors=preprocessors,
+        )
 
     def get_learner_parameters(self):
         return (("Number of neighbours", self.n_neighbors),
+                # false positive, pylint: disable=invalid-sequence-index
                 ("Metric", self.metrics_options[self.metric_index]),
-                ("Weight", self.weights_options[self.weight_index]))
+                ("Normalize instances", 
+                    "L2" if self.normalize_instances_l2 else "No",
+                ),
+                # false positive, pylint: disable=invalid-sequence-index
+                ("Weight", self.weights_options[self.weight_index]),)
 
 
 if __name__ == "__main__":  # pragma: no cover
--- Orange/widgets/model/tests/test_owknn.py
+++ Orange/widgets/model/tests/test_owknn.py
@@ -1,10 +1,16 @@
 # pylint: disable=missing-docstring
-from Orange.widgets.model.owknn import OWKNNLearner
+
+import numpy as np
+from scipy import sparse
+
+from Orange.data import Table
+from Orange.widgets.model.owknn import OWKNNLearner, NormalizeInstancesL2
 from Orange.widgets.tests.base import (
     WidgetTest,
     WidgetLearnerTestMixin,
-    ParameterMapping
+    ParameterMapping,
 )
+from Orange.preprocess import Randomize, PreprocessorList
 
 
 class TestOWKNNLearner(WidgetTest, WidgetLearnerTestMixin):
@@ -17,4 +23,125 @@ class TestOWKNNLearner(WidgetTest, WidgetLearnerTestMixin):
                              self.widget.metrics),
             ParameterMapping('weights', self.widget.weights_combo,
                              self.widget.weights),
-            ParameterMapping('n_neighbors', self.widget.n_neighbors_spin)]
+            ParameterMapping('n_neighbors', self.widget.n_neighbors_spin),
+        ]
+
+    def test_l2_preprocessor_added_only_for_euclidean(self):
+        self.assertEqual(self.widget.metrics[self.widget.metric_index], "euclidean")
+        self.assertTrue(self.widget.normalize_l2_checkbox.isEnabled())
+        self.assertFalse(self.widget.normalize_instances_l2)
+
+        learner = self.widget.create_learner()
+        self.assertFalse(any(isinstance(pp, NormalizeInstancesL2)
+                             for pp in learner.preprocessors))
+
+        self.widget.normalize_l2_checkbox.click()
+        self.assertTrue(self.widget.normalize_instances_l2)
+        learner = self.widget.create_learner()
+        self.assertTrue(any(isinstance(pp, NormalizeInstancesL2)
+                            for pp in learner.preprocessors))
+
+        self.widget.metric_index = 1
+        self.widget._on_metric_changed()
+        self.assertFalse(self.widget.normalize_l2_checkbox.isEnabled())
+        self.assertFalse(self.widget.normalize_instances_l2)
+        learner = self.widget.create_learner()
+        self.assertFalse(any(isinstance(pp, NormalizeInstancesL2)
+                             for pp in learner.preprocessors))
+
+    def test_l2_checkbox_reenabled_when_returning_to_euclidean(self):
+        self.widget.metric_index = 1
+        self.widget._on_metric_changed()
+        self.assertFalse(self.widget.normalize_l2_checkbox.isEnabled())
+
+        self.widget.metric_index = 0
+        self.widget._on_metric_changed()
+        self.assertTrue(self.widget.normalize_l2_checkbox.isEnabled())
+        self.assertFalse(self.widget.normalize_instances_l2)
+
+    def test_l2_normalizes_dense_instances(self):
+        data = Table.from_numpy(
+            None,
+            np.array([
+                [3.0, 4.0],
+                [0.0, 0.0],
+                [1.0, 0.0],
+            ])
+        )
+
+        normalized = NormalizeInstancesL2()(data)
+
+        np.testing.assert_allclose(
+            normalized.X,
+            np.array([
+                [0.6, 0.8],
+                [0.0, 0.0],
+                [1.0, 0.0],
+            ])
+        )
+
+    def test_l2_normalizes_sparse_instances(self):
+        data = Table.from_numpy(
+            None,
+            sparse.csr_matrix([
+                [3.0, 4.0],
+                [0.0, 0.0],
+            ])
+        )
+
+        normalized = NormalizeInstancesL2()(data)
+
+        self.assertTrue(sparse.issparse(normalized.X))
+        np.testing.assert_allclose(
+            normalized.X.toarray(),
+            np.array([
+                [0.6, 0.8],
+                [0.0, 0.0],
+            ])
+        )
+
+    def test_l2_does_not_modify_input_data(self):
+        data = Table.from_numpy(
+            None,
+            np.array([[3.0, 4.0]])
+        )
+        original = data.X.copy()
+
+        NormalizeInstancesL2()(data)
+
+        np.testing.assert_array_equal(data.X, original)
+
+    def test_l2_is_appended_to_input_preprocessor(self):
+        randomize = Randomize()
+        self.widget.preprocessors = randomize
+        self.widget.normalize_instances_l2 = True
+
+        learner = self.widget.create_learner()
+
+        self.assertEqual(len(learner.preprocessors), 1)
+        preprocessor_list = learner.preprocessors[0]
+
+        self.assertIsInstance(preprocessor_list, PreprocessorList)
+        self.assertEqual(preprocessor_list.preprocessors[0], randomize)
+        self.assertIsInstance(
+            preprocessor_list.preprocessors[1],
+            NormalizeInstancesL2,
+        )
+
+    def test_l2_is_appended_to_preprocessor_list(self):
+        randomize = Randomize()
+        preprocessors = PreprocessorList([randomize])
+        self.widget.preprocessors = preprocessors
+        self.widget.normalize_instances_l2 = True
+
+        learner = self.widget.create_learner()
+
+        self.assertEqual(len(learner.preprocessors), 1)
+        preprocessor_list = learner.preprocessors[0]
+
+        self.assertIsInstance(preprocessor_list, PreprocessorList)
+        self.assertEqual(preprocessor_list.preprocessors[0], randomize)
+        self.assertIsInstance(
+            preprocessor_list.preprocessors[1],
+            NormalizeInstancesL2,
+        )
